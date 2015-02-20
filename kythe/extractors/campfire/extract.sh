c91541004e20372e2562c8e076ded45d7f9346a5
Cody Schroeder <schroederc@google.com>
2015-02-20 08:52:03 -0800

    Support indexpacks in Docker release container

--- kythe/extractors/campfire/extract.sh
+++ kythe/extractors/campfire/extract.sh
@@ -1,18 +1,38 @@
 #!/bin/bash -e
+
+# Copyright 2014 Google Inc. All rights reserved.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Script to extract compilations from a campfire-based repository.
 #
 # Usage:
-#   extract.sh [--docker] [campfire-root] <output-dir>
+#   extract.sh [--docker] [--index_pack] [campfire-root] <output-dir>
 
 usage() {
-  echo "Usage: $(basename "$0") [--docker] [campfire-root] <output-dir>" >&2
+  echo "Usage: $(basename "$0") [--docker] [--index_pack] [campfire-root] <output-dir>" >&2
   exit 1
 }
 
 CAMPFIRE=./campfire
 if [[ "$1" == "--docker" ]]; then
   CAMPFIRE=./campfire-docker
+  shift
+fi
+
+INDEX_PACK=
+if [[ "$1" == "--index_pack" ]]; then
+  INDEX_PACK=1
   shift
 fi
 
@@ -37,7 +57,12 @@ for idx in $(find campfire-out/gen -name '*.kindex'); do
   name="$(basename "$idx" .kindex)"
   lang="${name##*.}"
   dir="$OUTPUT/$lang"
-  mkdir -p "$dir"
-  dest="$dir/$(tr '/' '_' <<<"${idx#campfire-out/gen/}")"
-  cp "$idx" "$dest" # TODO(schroederc): put into indexpack instead
+
+  if [[ -n "$INDEX_PACK" ]]; then
+      ./campfire-out/bin/kythe/go/platform/tools/indexpack --to_archive "$dir" "$idx"
+  else
+    mkdir -p "$dir"
+    dest="$dir/$(tr '/' '_' <<<"${idx#campfire-out/gen/}")"
+    cp "$idx" "$dest"
+  fi
 done
