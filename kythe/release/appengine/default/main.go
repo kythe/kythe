6b28b44f7ef2f125fdd19bc16f0026210658edd5
Cody Schroeder <schroederc@google.com>
2015-03-12 13:35:16 -0700

    Ensure Go vanity URL tags exist for every page requested with ?go-get=1
    Reviewers: fromberger
    
    Reviewed By: fromberger
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D132

--- kythe/release/appengine/default/main.go
+++ kythe/release/appengine/default/main.go
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package server
 
 import (
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -9,6 +26,17 @@ const (
 	phabricatorURL = "https://phabricator-dot-kythe-repo.appspot.com"
 	repoURL        = phabricatorURL + "/diffusion/K/browse/master"
 	staticRoot     = "site"
+
+	goGetHTML = `<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="go-import" content="kythe.io git https://github.com/google/kythe">
+    <meta name="go-source" content="kythe.io http://kythe.io http://kythe.io/repo{/dir} http://kythe.io/repo{/dir}/{file}${line}">
+  </head>
+  <body>
+    <a href="http://kythe.io">kythe.io</a>
+  </body>
+</html>`
 )
 
 func init() {
@@ -19,6 +47,12 @@ func init() {
 		http.Redirect(w, r, repoURL+r.URL.Path, http.StatusTemporaryRedirect)
 	})))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join(staticRoot, r.URL.Path))
+		if _, ok := r.URL.Query()["go-get"]; ok {
+			if _, err := w.Write([]byte(goGetHTML)); err != nil {
+				log.Println("Error writing ?go-get=1 response:", err)
+			}
+		} else {
+			http.ServeFile(w, r, filepath.Join(staticRoot, r.URL.Path))
+		}
 	})
 }
