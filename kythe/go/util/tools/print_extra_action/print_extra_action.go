bea3251536d5c5dcfb964bd960d6c5e4d58f4c72
Cody Schroeder <schroederc@google.com>
2015-04-15 15:28:31 -0700

    Add print_extra_action utility for printing Bazel ExtraActionInfo protos
    Test Plan:
    ```
    bazel build --experimental_action_listener=//kythe/go/util/tools:print_extra_actions //... | jq .
    ```
    
    Reviewers: zarko
    
    Reviewed By: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D189

---
+++ kythe/go/util/tools/print_extra_action/print_extra_action.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Binary print_extra_action takes a single argument that is a path to file with
+// an protobuf wire-encoding of a Bazel ExtraActionInfo and prints it as JSON to
+// stdout.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/golang/protobuf/proto"
+
+	xapb "third_party/bazel/extra_actions_base_proto"
+)
+
+var knownExtensions = []*proto.ExtensionDesc{
+	xapb.E_SpawnInfo_SpawnInfo,
+	xapb.E_CppCompileInfo_CppCompileInfo,
+	xapb.E_CppLinkInfo_CppLinkInfo,
+	xapb.E_JavaCompileInfo_JavaCompileInfo,
+	xapb.E_PythonInfo_PythonInfo,
+}
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(f)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var xa xapb.ExtraActionInfo
+	if err := proto.Unmarshal(data, &xa); err != nil {
+		log.Fatal(err)
+	}
+
+	obj := make(map[string]interface{})
+	obj["extra_action_info"] = &xa
+
+	xs, err := proto.GetExtensions(&xa, knownExtensions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var extensions []interface{}
+	for _, e := range xs {
+		if e != nil {
+			extensions = append(extensions, e)
+		}
+	}
+
+	if len(extensions) > 0 {
+		obj["extensions"] = extensions
+	}
+
+	if err := json.NewEncoder(os.Stdout).Encode(obj); err != nil {
+		log.Fatal(err)
+	}
+
+}
