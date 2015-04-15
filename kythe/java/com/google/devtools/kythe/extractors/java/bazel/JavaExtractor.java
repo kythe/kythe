aa80110f19ed6981143aa7f9367c57f17ae7bf16
Cody Schroeder <schroederc@google.com>
2015-04-15 15:54:50 -0700

    Bazel support: implement extra_action to extract Java CompilationUnits as kindex files
    Test Plan:
    ```
    bazel clean
    bazel build --experimental_action_listener=//kythe/java/com/google/devtools/kythe/extractors/java/bazel:extract_kindex //...
    find -L bazel-out -name '*.kindex'
    ```
    
    Reviewers: zarko
    
    Reviewed By: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D190

---
+++ kythe/java/com/google/devtools/kythe/extractors/java/bazel/JavaExtractor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.kythe.extractors.java.bazel;
+
+import com.google.devtools.build.lib.actions.extra.ExtraActionInfo;
+import com.google.devtools.build.lib.actions.extra.ExtraActionsBase;
+import com.google.devtools.build.lib.actions.extra.JavaCompileInfo;
+import com.google.devtools.kythe.extractors.java.JavaCompilationUnitExtractor;
+import com.google.devtools.kythe.extractors.shared.CompilationDescription;
+import com.google.devtools.kythe.extractors.shared.ExtractionException;
+import com.google.devtools.kythe.extractors.shared.IndexInfoUtils;
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.ExtensionRegistry;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/** Java CompilationUnit extractor using Bazel's extra_action feature. */
+public class JavaExtractor {
+  private static final String CORPUS = "kythe";
+
+  public static void main(String[] args) throws IOException, ExtractionException {
+    String extraActionPath = args[0];
+    String outputPath = args[1];
+
+    ExtensionRegistry registry = ExtensionRegistry.newInstance();
+    ExtraActionsBase.registerAllExtensions(registry);
+
+    ExtraActionInfo info;
+    try (InputStream stream = Files.newInputStream(Paths.get(args[0]))) {
+      CodedInputStream coded = CodedInputStream.newInstance(stream);
+      info = ExtraActionInfo.parseFrom(coded, registry);
+    }
+
+    if (!info.hasExtension(JavaCompileInfo.javaCompileInfo)) {
+      throw new IllegalArgumentException("Given ExtraActionInfo without JavaCompileInfo");
+    }
+
+    JavaCompileInfo jInfo = info.getExtension(JavaCompileInfo.javaCompileInfo);
+    // TODO(schroederc): implement corpus/FileVNames configuration support
+    CompilationDescription description = new JavaCompilationUnitExtractor(CORPUS).extract(
+        info.getOwner(),
+        jInfo.getSourceFileList(),
+        jInfo.getClasspathList(),
+        jInfo.getSourcepathList(),
+        jInfo.getProcessorpathList(),
+        jInfo.getProcessorList(),
+        jInfo.getJavacOptList(),
+        jInfo.getOutputjar());
+
+    IndexInfoUtils.writeIndexInfoToFile(description, outputPath);
+  }
+}
