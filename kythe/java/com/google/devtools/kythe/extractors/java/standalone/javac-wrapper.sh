7c2f56a563f906fa5bd06924bad4061b968f3fb1
Cody Schroeder <schroederc@google.com>
2015-05-19 15:03:23 -0700

    Release README: add basic usage information
    Reviewers: zarko, jdennett, fromberger
    
    Reviewed By: fromberger
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D264

--- kythe/java/com/google/devtools/kythe/extractors/java/standalone/javac-wrapper.sh
+++ kythe/java/com/google/devtools/kythe/extractors/java/standalone/javac-wrapper.sh
@@ -20,18 +20,27 @@
 # KYTHE_OUTPUT_DIRECTORY set to understand where the root of the compiled source
 # repository is and where to put the resulting .kindex files, respectively.
 #
+# This script assumes a usable java binary is on $PATH.
+#
 # This script is meant as a replacement for $JAVA_HOME/bin/javac.  It assumes
 # the true javac binary is in the same directory as itself and named
-# "javac.real".  The default path for the javac extractor jar is
-# /opt/kythe/javac_extractor.jar but can be set with the JAVAC_EXTRACTOR_JAR
-# environment variable.
+# "javac.real".  The location of the real javac binary can be configured with
+# the REAL_JAVAC environment variable.  The default path for the javac extractor
+# jar is /opt/kythe/javac_extractor.jar but can be set with the
+# JAVAC_EXTRACTOR_JAR environment variable.
 #
 # If used in a Docker environment where KYTHE_ROOT_DIRECTORY and
 # KYTHE_OUTPUT_DIRECTORY are volumes, it can be useful to set the DOCKER_CLEANUP
 # environment variable so that files modified/created in either volume have
 # their owner/group set to the volume's root directory's owner/group.
 
-readonly BIN="$(dirname "$(readlink -e "$0")")"
+echo "$@" >> /tmp/javac-wrapper.log
+exec 1> /tmp/javac-wrapper.out
+exec 2> /tmp/javac-wrapper.err
+
+if [[ -z "$REAL_JAVAC" ]]; then
+  readonly REAL_JAVAC="$(dirname "$(readlink -e "$0")")/javac.real"
+fi
 if [[ -z "$JAVAC_EXTRACTOR_JAR" ]]; then
   readonly JAVAC_EXTRACTOR_JAR="/opt/kythe/javac_extractor.jar"
 fi
@@ -48,5 +57,5 @@ if [[ -n "$DOCKER_CLEANUP" ]]; then
   trap cleanup EXIT ERR INT
 fi
 
-"$BIN/java" -jar "$JAVAC_EXTRACTOR_JAR" "$@"
-"$BIN/javac.real" "$@"
+java -jar "$JAVAC_EXTRACTOR_JAR" "$@"
+"$REAL_JAVAC" "$@"
