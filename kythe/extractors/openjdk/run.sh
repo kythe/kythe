fa4ad8aab8a2719848125c65d2ab145402401902
Cody Schroeder <schroederc@google.com>
2015-05-22 14:28:05 -0700

    Update OpenJDK extractor to use Bazel
    Reviewers: zarko
    
    Reviewed By: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D274

--- kythe/extractors/openjdk/run.sh
+++ kythe/extractors/openjdk/run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -e
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Script to build/run the Docker openjdk-extractor container.
 #
@@ -20,6 +33,6 @@ mkdir -p "$OUTPUT"
 OUTPUT="$(readlink -e "$OUTPUT")"
 
 cd "$(dirname "$0")"/../../..
-./campfire package --start_registry=false //kythe/extractors/openjdk
+bazel build //kythe/extractors/openjdk
 
 docker run -ti --rm -v $OUTPUT:/idx openjdk-extractor
