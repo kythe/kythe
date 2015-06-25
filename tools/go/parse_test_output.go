398422f13aef51484bf0717d7fe170251b9e97c9
Cody Schroeder <schroederc@google.com>
2015-06-25 16:23:38 -0700

    parse_test_output: add xml output format
    Reviewers: zarko, shahms
    
    Reviewed By: shahms
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D365

--- tools/go/parse_test_output.go
+++ tools/go/parse_test_output.go
@@ -5,6 +5,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"encoding/xml"
 	"flag"
 	"io"
 	"log"
@@ -12,6 +13,8 @@ import (
 	"regexp"
 	"strings"
 	"time"
+
+	"tools/go/testsuite"
 
 	"golang.org/x/tools/benchmark/parse"
 )
@@ -37,8 +40,16 @@ const logPrefix = "\t"
 
 var testCaseRE = regexp.MustCompile(`^--- (PASS|FAIL|SKIP): ([^ ]+) \((.+)\)$`)
 
+var (
+	outputFile = flag.String("out", "/dev/stdout", "Path of file to write test results as XML/JSON")
+	format     = flag.String("format", "json", "Format to write results (supported: json or xml)")
+)
+
 func main() {
 	flag.Parse()
+	if *format != "xml" && *format != "json" {
+		log.Fatalf("Unsupported output format: %q", *format)
+	}
 
 	r, w, err := os.Pipe()
 	if err != nil {
@@ -96,8 +107,55 @@ func main() {
 		}
 	}
 
-	json.NewEncoder(os.Stdout).Encode(struct {
-		Tests      []*testCase
-		Benchmarks []*parse.Benchmark `json:",omitempty"`
-	}{results, benchmarks})
+	f, err := os.Create(*outputFile)
+	if err != nil {
+		log.Fatalf("Failure to create output file %q: %v", *outputFile, err)
+	}
+
+	switch *format {
+	case "xml":
+		if err := xml.NewEncoder(f).EncodeElement(makeTestSuite(results, benchmarks), xml.StartElement{
+			Name: xml.Name{Local: "testsuite"},
+		}); err != nil {
+			log.Fatalf("Error encoding XML: %v", err)
+		}
+	case "json":
+		if err := json.NewEncoder(f).Encode(struct {
+			Tests      []*testCase
+			Benchmarks []*parse.Benchmark `json:",omitempty"`
+		}{results, benchmarks}); err != nil {
+			log.Fatalf("Error encoding JSON: %v", err)
+		}
+	default:
+		panic("unsupported output format: " + *format)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatalf("Error closing output file %q: %v", *outputFile, err)
+	}
+}
+
+func makeTestSuite(tests []*testCase, benchmarks []*parse.Benchmark) *testsuite.Suite {
+	s := &testsuite.Suite{}
+	for _, t := range tests {
+		c := testsuite.TestCase{
+			Name: t.Name,
+			Time: float64(t.Duration.Nanoseconds()) / float64(time.Millisecond),
+		}
+		switch t.Status {
+		case Skip:
+			c.Status = testsuite.NotRun
+		case Pass:
+			c.Status = testsuite.Run
+		case Fail:
+			c.Status = testsuite.Run
+			c.Errors = append(c.Errors, testsuite.TestError{
+				Content: strings.Join(t.Log, "\n"),
+			})
+		default:
+			log.Printf("Unknown test case status: %q", t.Status)
+		}
+		s.TestCase = append(s.TestCase, c)
+	}
+	return s
 }
