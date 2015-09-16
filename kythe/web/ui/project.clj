f6da9212e8e4f53fdd1c3c06bade00957a4900b7
Cody Schroeder <schroederc@google.com>
2015-09-16 13:40:02 -0700

    Sample web UI: downgrade to known working ClojureScript version
    Reviewers: zarko, shahms
    
    Reviewed By: shahms
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D517

--- kythe/web/ui/project.clj
+++ kythe/web/ui/project.clj
@@ -7,12 +7,12 @@
                  :subscribe "http://groups.google.com/group/kythe/boxsubscribe"
                  :unsubscribe "http://groups.google.com/group/kythe/boxsubscribe"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"
+                 [org.clojure/clojurescript "0.0-2371"
                   :exclusions [org.apache.ant/ant]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.8.0-alpha2"]
                  [cljs-ajax "0.3.3"]]
-  :plugins [[lein-cljsbuild "1.0.3"]
+  :plugins [[lein-cljsbuild "1.1.0"]
             [lein-licenses "0.2.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {
