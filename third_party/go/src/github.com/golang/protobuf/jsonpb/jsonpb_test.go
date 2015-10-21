d316469fc27249752435a98e0095a1b881734215
Cody Schroeder <schroederc@google.com>
2015-10-21 12:26:53 -0700

    Update third_party Go protobuf library
    Reviewers: shahms, zarko
    
    Reviewed By: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D569

--- third_party/go/src/github.com/golang/protobuf/jsonpb/jsonpb_test.go
+++ third_party/go/src/github.com/golang/protobuf/jsonpb/jsonpb_test.go
@@ -37,14 +37,14 @@ import (
 
 	pb "github.com/golang/protobuf/jsonpb/jsonpb_test_proto"
 	"github.com/golang/protobuf/proto"
+	proto3pb "github.com/golang/protobuf/proto/proto3_proto"
 )
 
 var (
 	marshaler = Marshaler{}
 
 	marshalerAllOptions = Marshaler{
-		EnumsAsString: true,
-		Indent:        "  ",
+		Indent: "  ",
 	}
 
 	simpleObject = &pb.Simple{
@@ -181,8 +181,8 @@ var (
 		RRepeats: []*pb.Repeats{innerRepeats, innerRepeats2},
 	}
 
-	complexObjectJSON = `{"color":1,` +
-		`"r_color":[0,1,2],` +
+	complexObjectJSON = `{"color":"GREEN",` +
+		`"r_color":["RED","GREEN","BLUE"],` +
 		`"simple":{"o_int32":-32},` +
 		`"r_simple":[{"o_int32":-32},{"o_int64":"25"}],` +
 		`"repeats":{"r_string":["roses","red"]},` +
@@ -268,9 +268,9 @@ var marshalingTests = []struct {
 	{"repeated fields pretty object", marshalerAllOptions, repeatsObject, repeatsObjectPrettyJSON},
 	{"nested message/enum flat object", marshaler, complexObject, complexObjectJSON},
 	{"nested message/enum pretty object", marshalerAllOptions, complexObject, complexObjectPrettyJSON},
-	{"enum-string flat object", Marshaler{EnumsAsString: true},
+	{"enum-string flat object", Marshaler{},
 		&pb.Widget{Color: pb.Widget_BLUE.Enum()}, `{"color":"BLUE"}`},
-	{"enum-value pretty object", Marshaler{Indent: " "},
+	{"enum-value pretty object", Marshaler{EnumsAsInts: true, Indent: " "},
 		&pb.Widget{Color: pb.Widget_BLUE.Enum()}, colorPrettyJSON},
 	{"unknown enum value object", marshalerAllOptions,
 		&pb.Widget{Color: pb.Widget_Color(1000).Enum(), RColor: []pb.Widget_Color{pb.Widget_RED}}, colorListPrettyJSON},
@@ -320,6 +320,8 @@ var unmarshalingTests = []struct {
 	{"nested message/enum pretty object", complexObjectPrettyJSON, complexObject},
 	{"enum-string object", `{"color":"BLUE"}`, &pb.Widget{Color: pb.Widget_BLUE.Enum()}},
 	{"enum-value object", "{\n \"color\": 2\n}", &pb.Widget{Color: pb.Widget_BLUE.Enum()}},
+	{"proto3 enum string", `{"hilarity":"PUNS"}`, &proto3pb.Message{Hilarity: proto3pb.Message_PUNS}},
+	{"proto3 enum value", `{"hilarity":1}`, &proto3pb.Message{Hilarity: proto3pb.Message_PUNS}},
 	{"unknown enum value object",
 		"{\n  \"color\": 1000,\n  \"r_color\": [\n    \"RED\"\n  ]\n}",
 		&pb.Widget{Color: pb.Widget_Color(1000).Enum(), RColor: []pb.Widget_Color{pb.Widget_RED}}},
@@ -354,15 +356,16 @@ func TestUnmarshaling(t *testing.T) {
 var unmarshalingShouldError = []struct {
 	desc string
 	in   string
+	pb   proto.Message
 }{
-	{"a value", "666"},
-	{"gibberish", "{adskja123;l23=-="},
+	{"a value", "666", new(pb.Simple)},
+	{"gibberish", "{adskja123;l23=-=", new(pb.Simple)},
+	{"unknown enum name", `{"hilarity":"DAVE"}`, new(proto3pb.Message)},
 }
 
 func TestUnmarshalingBadInput(t *testing.T) {
 	for _, tt := range unmarshalingShouldError {
-		obj := &pb.Simple{}
-		err := UnmarshalString(tt.in, obj)
+		err := UnmarshalString(tt.in, tt.pb)
 		if err == nil {
 			t.Errorf("an error was expected when parsing %q instead of an object", tt.desc)
 		}
