ae8e2fc1f07412d85fea52e15b2764d9146d2550
Cody Schroeder <schroederc@google.com>
2016-05-16 12:12:52 -0700

    Add lower-level disksort.Iterator
    Summary:
    An Iterator allows for easier integration into some other components
    (like the post-processing pipeline); the existing internal iteration
    pattern can simply be implemented as a wrapper.
    
    Reviewers: shahms, zarko
    
    Reviewed By: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D892

--- kythe/go/util/disksort/disksort.go
+++ kythe/go/util/disksort/disksort.go
@@ -45,11 +45,29 @@ type Interface interface {
 	// Add adds a new element to the set of data to be sorted.
 	Add(i interface{}) error
 
+	// Iterator returns an Iterator to read each of the elements previously added
+	// to the set of data to be sorted.  Once Iterator is called, no more data may
+	// be added to the sorter.  This is a lower-level version of Read.  Iterator
+	// and Read may only be called once.
+	Iterator() (Iterator, error)
+
 	// Read calls f on each elements previously added the set of data to be
 	// sorted.  If f returns an error, it is returned immediately and f is no
 	// longer called.  Once Read is called, no more data may be added to the
-	// sorter.  Read may only be called once.
+	// sorter.  Iterator and Read may only be called once.
 	Read(f func(interface{}) error) error
+}
+
+// An Iterator reads each element, in order, in a sorted dataset.
+type Iterator interface {
+	// Next returns the next ordered element.  If none exist, an io.EOF error is
+	// returned.
+	Next() (interface{}, error)
+
+	// Close releases all of the Iterator's used resources.  Each Iterator must be
+	// closed after the client's last call to Next or stray temporary files may be
+	// left on disk.
+	Close() error
 }
 
 // Marshaler is an interface to functions that can binary encode/decode
@@ -151,63 +169,59 @@ func (m *mergeSorter) Add(i interface{}) error {
 	return nil
 }
 
-// Read implements part of the Interface interface.
-func (m *mergeSorter) Read(f func(i interface{}) error) (err error) {
+type mergeIterator struct {
+	buffer []interface{}
+
+	merger    *sortutil.ByLesser
+	marshaler Marshaler
+	workDir   string
+}
+
+// Iterator implements part of the Interface interface.
+func (m *mergeSorter) Iterator() (iter Iterator, err error) {
 	if m.finalized {
-		return ErrAlreadyFinalized
+		return nil, ErrAlreadyFinalized
 	}
 	m.finalized = true // signal that further operations should fail
 
-	// Ensure that the working directory is always cleaned up.
-	defer func() {
-		cleanupErr := os.RemoveAll(m.workDir)
-		if err == nil {
-			err = cleanupErr
-		} else {
-			log.Println("WARNING: error removing temporary directory:", m.workDir)
-		}
-	}()
+	it := &mergeIterator{workDir: m.workDir, marshaler: m.opts.Marshaler}
 
 	if len(m.shards) == 0 {
 		// Fast path for a single, in-memory shard
-		defer func() { m.buffer = nil }()
-		sortutil.Sort(m.opts.Lesser, m.buffer)
-		for len(m.buffer) > 0 {
-			if err := f(m.buffer[0]); err != nil {
-				return err
-			}
-			m.buffer = m.buffer[1:]
-		}
-		return nil
+		it.buffer, m.buffer = m.buffer, nil
+		sortutil.Sort(m.opts.Lesser, it.buffer)
+		return it, nil
 	}
-
-	if len(m.buffer) != 0 {
-		// To make the merging algorithm simpler, dump the last shard to disk.
-		if err := m.dumpShard(); err != nil {
-			m.buffer = nil
-			return fmt.Errorf("error dumping final shard: %v", err)
-		}
-	}
-	m.buffer = nil
 
 	// This is a heap storing the head of each shard.
 	merger := &sortutil.ByLesser{
 		Lesser: &mergeElementLesser{Lesser: m.opts.Lesser},
 	}
+	it.merger = merger
 
 	defer func() {
 		// Try to cleanup on errors
-		for merger.Len() != 0 {
-			x := heap.Pop(merger).(*mergeElement)
-			_ = x.f.Close() // ignore errors (file is only open for reading)
+		if err != nil {
+			if cErr := it.Close(); cErr != nil {
+				log.Printf("WARNING: error closing Iterator after error: %v", cErr)
+			}
 		}
 	}()
+
+	if len(m.buffer) != 0 {
+		// To make the merging algorithm simpler, dump the last shard to disk.
+		if err := m.dumpShard(); err != nil {
+			m.buffer = nil
+			return nil, fmt.Errorf("error dumping final shard: %v", err)
+		}
+	}
+	m.buffer = nil
 
 	// Initialize the merger heap by reading the first element of each shard.
 	for _, shard := range m.shards {
 		f, err := os.OpenFile(shard, os.O_RDONLY, shardFileMode)
 		if err != nil {
-			return fmt.Errorf("error opening shard %q: %v", shard, err)
+			return nil, fmt.Errorf("error opening shard %q: %v", shard, err)
 		}
 
 		r := io.Reader(f)
@@ -219,51 +233,106 @@ func (m *mergeSorter) Read(f func(i interface{}) error) (err error) {
 		first, err := rd.Next()
 		if err != nil {
 			f.Close()
-			return fmt.Errorf("error reading beginning of shard %q: %v", shard, err)
+			return nil, fmt.Errorf("error reading beginning of shard %q: %v", shard, err)
 		}
 		el, err := m.opts.Marshaler.Unmarshal(first)
 		if err != nil {
 			f.Close()
-			return fmt.Errorf("error unmarshaling beginning of shard %q: %v", shard, err)
+			return nil, fmt.Errorf("error unmarshaling beginning of shard %q: %v", shard, err)
 		}
 
 		heap.Push(merger, &mergeElement{el: el, rd: rd, f: f})
+	}
+
+	return it, nil
+}
+
+// Next implements part of the Iterator interface.
+func (i *mergeIterator) Next() (interface{}, error) {
+	if i.merger == nil {
+		// Fast path for a single, in-memory shard
+		if len(i.buffer) == 0 {
+			return nil, io.EOF
+		}
+		val := i.buffer[0]
+		i.buffer = i.buffer[1:]
+		return val, nil
+	}
+
+	if i.merger.Len() == 0 {
+		return nil, io.EOF
 	}
 
 	// While the merger heap is non-empty:
 	//   el := pop the head of the heap
 	//   pass it to the user-specific function
 	//   push the next element el.rd to the merger heap
-	for merger.Len() != 0 {
-		x := heap.Pop(merger).(*mergeElement)
+	x := heap.Pop(i.merger).(*mergeElement)
+	el := x.el
 
-		// Give the value to the user-supplied function
-		if err := f(x.el); err != nil {
-			return err
+	// Read and parse the next value on the same shard
+	rec, err := x.rd.Next()
+	if err != nil {
+		_ = x.f.Close()           // ignore errors (file is only open for reading)
+		_ = os.Remove(x.f.Name()) // ignore errors (os.RemoveAll used in Close)
+		if err != io.EOF {
+			return nil, fmt.Errorf("error reading shard: %v", err)
 		}
-
-		// Read and parse the next value on the same shard
-		rec, err := x.rd.Next()
+	} else {
+		next, err := i.marshaler.Unmarshal(rec)
 		if err != nil {
-			_ = x.f.Close()           // ignore errors (file is only open for reading)
-			_ = os.Remove(x.f.Name()) // ignore errors (os.RemoveAll used in defer)
-			if err == io.EOF {
-				continue
-			} else {
-				return fmt.Errorf("error reading shard: %v", err)
-			}
-		}
-		next, err := m.opts.Marshaler.Unmarshal(rec)
-		if err != nil {
-			return fmt.Errorf("error unmarshaling element: %v", err)
+			return nil, fmt.Errorf("error unmarshaling element: %v", err)
 		}
 
 		// Reuse mergeElement, push it back onto the merger heap with the next value
 		x.el = next
-		heap.Push(merger, x)
+		heap.Push(i.merger, x)
 	}
 
+	return el, nil
+}
+
+// Close implements part of the Iterator interface.
+func (i *mergeIterator) Close() error {
+	i.buffer = nil
+	if i.merger != nil {
+		for i.merger.Len() != 0 {
+			x := heap.Pop(i.merger).(*mergeElement)
+			_ = x.f.Close() // ignore errors (file is only open for reading)
+		}
+	}
+	if rmErr := os.RemoveAll(i.workDir); rmErr != nil {
+		return fmt.Errorf("error removing temporary directory %q: %v", i.workDir, rmErr)
+	}
 	return nil
+}
+
+// Read implements part of the Interface interface.
+func (m *mergeSorter) Read(f func(i interface{}) error) (err error) {
+	it, err := m.Iterator()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if cErr := it.Close(); cErr != nil {
+			if err == nil {
+				err = cErr
+			} else {
+				log.Println("WARNING: error closing Iterator:", cErr)
+			}
+		}
+	}()
+	for {
+		val, err := it.Next()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+		if err := f(val); err != nil {
+			return err
+		}
+	}
 }
 
 const shardFileMode = 0600 | os.ModeExclusive | os.ModeAppend | os.ModeTemporary | os.ModeSticky
