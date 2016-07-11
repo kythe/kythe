5ae4412f39aa1a6759ce6112ab24c79ea0204180
Luke Zarko <zarko@google.com>
2016-07-11 13:13:35 -0700

    Fix analyzer_driver_test
    Reviewers: schroederc
    
    Reviewed By: schroederc
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D957

--- kythe/go/platform/tools/testdata/analyzer_driver_test.sh
+++ kythe/go/platform/tools/testdata/analyzer_driver_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-analyzer_driver() { "$PWD/kythe/go/platform/tools/analyzer_driver" "$@"; }
-entrystream() { "$PWD/kythe/go/platform/tools/entrystream" "$@"; }
+analyzer_driver() { "$PWD/kythe/go/platform/tools/analyzer_driver/analyzer_driver" "$@"; }
+entrystream() { "$PWD/kythe/go/platform/tools/entrystream/entrystream" "$@"; }
 
 JAVA_INDEXER_SERVER="$PWD/kythe/java/com/google/devtools/kythe/analyzers/java/indexer_server"
 KINDEX="$PWD/kythe/testdata/test.kindex"
