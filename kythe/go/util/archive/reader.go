ceb39765bc621f2b05e0c2e0202893687d9ad09c
M. J. Fromberger <fromberger@google.com>
2016-08-23 07:53:42 -0700

    Add package "archive" for scanning archive files.
    Summary:
    This package provides a Scan function to sequentially scan the contents
    of a ZIP or tar archive and pass each plain file to a callback. It has
    built-in support for some common tar compression schemes.
    
    The main use of this package is for scanning archives of .kindex files.
    
    Reviewers: salguarnieri, zarko, #core_team
    
    Reviewed By: zarko, #core_team
    
    Subscribers: zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1019

---
+++ kythe/go/util/archive/reader.go
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package archive provides support for reading the contents of archives such
+// as .zip and .tar files.
+package archive
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"compress/bzip2"
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+)
+
+// File defines the input capabilities needed to scan an archive file.
+type File interface {
+	io.Closer
+	io.Reader
+	io.ReaderAt
+	io.Seeker
+}
+
+// ErrNotArchive is returned by Scan when passed a file it does not recognize
+// as a readable archive.
+var ErrNotArchive = errors.New("not a supported archive file")
+
+// A ScanFunc is invoked by the Scan function for each file found in the
+// specified archive. The arguments are the filename as encoded in the archive,
+// and either an error or a reader positioned at the beginning of the file's
+// contents.
+//
+// Any error returned by the ScanFunc is propagated to the caller of Scan,
+// terminating the traversal of the archive.  The callback may choose to ignore
+// err, in which case the error is ignored and scanning continues.
+type ScanFunc func(filename string, err error, r io.Reader) error
+
+// Scan sequentially scans the contents of an archive and invokes f for each
+// file found. If f returns an error, scanning stops and that error is returned
+// to the caller of Scan. The path is used to determine what type of archive is
+// referred to by file. If the type is not known, it returns ErrNotArchive.
+//
+// The supported archive formats are:
+//
+//    .zip     -- ZIP archive (also .ZIP, .jar)
+//    .tar     -- uncompressed tar
+//    .tar.gz  -- gzip-compressed tar (also .tgz)
+//    .tar.bz2 -- bzip2-compressed tar
+//
+// Scan only invokes f for file entries; directories are not included.
+func Scan(file File, path string, f ScanFunc) error {
+	format, compression := parsePath(path)
+	switch format {
+	case ".zip":
+		size, err := file.Seek(0, io.SeekEnd)
+		if err != nil {
+			return fmt.Errorf("archive: finding ZIP file size: %v", err)
+		}
+		archive, err := zip.NewReader(file, size)
+		if err != nil {
+			return fmt.Errorf("archive: opening ZIP reader: %v", err)
+		}
+
+		for _, entry := range archive.File {
+			rc, err := entry.Open()
+			err = f(entry.Name, err, rc)
+			rc.Close()
+			if err != nil {
+				return err
+			}
+		}
+
+	case ".tar":
+		r := io.Reader(file)
+		switch compression {
+		case ".gz":
+			gz, err := gzip.NewReader(file)
+			if err != nil {
+				return fmt.Errorf("archive: opening gzip reader: %v", err)
+			}
+			r = gz
+		case ".bz2":
+			r = bzip2.NewReader(file)
+		case "":
+		default:
+		}
+		archive := tar.NewReader(r)
+
+		for {
+			entry, err := archive.Next()
+			if err == io.EOF {
+				break
+			}
+			isFile := entry != nil && entry.FileInfo().Mode().IsRegular()
+
+			// If we got an entry of any kind, invoke the callback whether or
+			// not we have an error. If we didn't get an entry, treat an error
+			// here as fatal.
+			if err == nil {
+				err = f(entry.Name, nil, archive)
+			} else if isFile {
+				err = f(entry.Name, err, nil)
+			}
+			if err != nil {
+				return err
+			}
+		}
+
+	default:
+		return ErrNotArchive
+	}
+	return nil
+}
+
+// parsePath determines which file format is represented by path, returning the
+// base file format (.zip or .tar) and the additional compression format
+// extension (.gz or .bz2), or "" if there is no additional compression.
+// Returns "", "" if the format could not be determined.
+func parsePath(path string) (format, compression string) {
+	switch ext := filepath.Ext(path); ext {
+	case ".zip", ".ZIP", ".jar":
+		return ".zip", ""
+	case ".tar":
+		return ext, ""
+	case ".tgz":
+		return ".tar", ".gz"
+	case ".gz", ".bz2":
+		base := filepath.Ext(strings.TrimSuffix(path, ext))
+		if base == ".tar" {
+			return base, ext
+		}
+	}
+	return "", "" // format unknown
+}
---
+++ kythe/go/util/archive/reader_test.go
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package archive
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"bytes"
+	"compress/gzip"
+	"errors"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func newTar(files map[string]string) *bytes.Buffer {
+	buf := bytes.NewBuffer(nil)
+	w := tar.NewWriter(buf)
+	for name, data := range files {
+		if err := w.WriteHeader(&tar.Header{
+			Name: name,
+			Size: int64(len(data)),
+		}); err != nil {
+			log.Fatal("NewHeader: ", err)
+		}
+		if _, err := w.Write([]byte(data)); err != nil {
+			log.Fatal("Write: ", err)
+		}
+	}
+	if err := w.Close(); err != nil {
+		log.Fatal("Close: ", err)
+	}
+	return buf
+}
+
+func newZIP(files map[string]string) *bytes.Buffer {
+	buf := bytes.NewBuffer(nil)
+	w := zip.NewWriter(buf)
+	for name, data := range files {
+		f, err := w.Create(name)
+		if err != nil {
+			log.Fatal("Create: ", err)
+		} else if _, err := f.Write([]byte(data)); err != nil {
+			log.Fatal("Write: ", err)
+		}
+	}
+	if err := w.Close(); err != nil {
+		log.Fatal("Close: ", err)
+	}
+	return buf
+}
+
+func TestScanNothing(t *testing.T) {
+	var ignore *os.File
+
+	// These paths should not be recognized as archives.
+	for _, path := range []string{"foo", "foo/bar", "foo.gz", "foo.bz2", "a/b/foo.zippy"} {
+		err := Scan(ignore, path, func(name string, err error, rc io.Reader) error {
+			t.Errorf("ScanFunc was called with path=%q, err=%v, rc=%v", name, err, rc)
+			return nil
+		})
+		if err != ErrNotArchive {
+			t.Errorf("Scan(%q): got error %v, want %v", err, ErrNotArchive)
+		}
+	}
+}
+
+// byteFile implements the File interface around a *bytes.Reader.  It adds a
+// no-op Close method to complete the interface.
+type byteFile struct{ *bytes.Reader }
+
+func (b byteFile) Close() error { return nil }
+
+func TestScanErrors(t *testing.T) {
+	file := byteFile{
+		Reader: bytes.NewReader(newZIP(map[string]string{
+			"this is not my hat": "♡",
+		}).Bytes()),
+	}
+
+	// Verify that an error returned by the callback is propagated.
+	want := errors.New("everything you know is wrong")
+	var got string
+	err := Scan(file, "fail.zip", func(name string, err error, r io.Reader) error {
+		got = name
+		return want
+	})
+	if err != want {
+		t.Errorf("Scan(fail.zip): got error %v, want %v", err, want)
+	}
+	if want := "this is not my hat"; got != want {
+		t.Errorf("Scan(fail.zip): got name %q, want %q", got, want)
+	}
+}
+
+func TestScanZIP(t *testing.T) {
+	files := map[string]string{
+		"devtools/grok/OWNERS": "stevey",
+		"file/base/file.h":     "#include <stdio.h>",
+		"pyglib.app.py":        "from __future__ import golang",
+	}
+	data := newZIP(files).Bytes()
+
+	// Each of these paths should be recognized as a ZIP file.
+	for _, path := range []string{"foo.zip", "bar.ZIP", "foo/bar.zip", "foo/bar/baz.plotz.zip", "ack/bar.jar"} {
+		file := byteFile{bytes.NewReader(data)}
+		got := make(map[string]string)
+
+		err := Scan(file, path, func(name string, err error, r io.Reader) error {
+			data, err := ioutil.ReadAll(r)
+			if err != nil {
+				return err
+			}
+			got[name] = string(data)
+			return nil
+		})
+		if err != nil {
+			t.Errorf("Scan(%q): unexpected error: %v", path, err)
+		}
+
+		if !reflect.DeepEqual(got, files) {
+			t.Errorf("Scan(%q):\n got %+v\nwant %+v", path, got, files)
+		}
+	}
+}
+
+func TestScanTar(t *testing.T) {
+	files := map[string]string{
+		"mapreduce/public/mapreduce.protodevel": `syntax = "proto3";`,
+		"file/base/BUILD":                       `cc_library(name = "recordio", ...)`,
+		"README.txt":                            "Lasciate ogni speranza, voi ch'entrate",
+		"third_party/README.md":                 `This directory is where your gross hacks go`,
+	}
+	data := newTar(files).Bytes()
+	gzdata := gzipData(data)
+
+	// Each of these paths should be recognized as a tar file.
+	tests := []struct {
+		path string
+		data []byte
+	}{
+		{"plain.tar", data},
+		{"foo/bar/plain.old.tar", data},
+		{"eat-a-tar.tar.gz", gzdata},
+		{"doom/and/chaos.tgz", gzdata},
+	}
+	for _, test := range tests {
+		file := byteFile{bytes.NewReader(test.data)}
+		got := make(map[string]string)
+
+		err := Scan(file, test.path, func(name string, err error, r io.Reader) error {
+			data, err := ioutil.ReadAll(r)
+			if err != nil {
+				return err
+			}
+			got[name] = string(data)
+			return nil
+		})
+		if err != nil {
+			t.Errorf("Scan(%q): unexpected error: %v", test.path, err)
+		}
+
+		if !reflect.DeepEqual(got, files) {
+			t.Errorf("Scan(%q):\n got %+v\nwant %+v", test.path, got, files)
+		}
+	}
+}
+
+func gzipData(data []byte) []byte {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	if _, err := w.Write(data); err != nil {
+		log.Fatal("Write: ", err)
+	} else if err := w.Close(); err != nil {
+		log.Fatal("Close: ", err)
+	}
+	return buf.Bytes()
+}
