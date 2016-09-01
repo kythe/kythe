16c1feea482575d3b8b8b47bff81245b332a7cae
M. J. Fromberger <fromberger@google.com>
2016-08-31 19:41:24 -0700

    Add a locking wrapper for a kcd.ReadWriter.
    Summary:
    This is a convenience to provide locking for implementations of the
    ReadWriter interface that are not safe for concurrency.
    
    Reviewers: #core_team, zarko
    
    Reviewed By: #core_team, zarko
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1034

---
+++ kythe/go/platform/kcd/locked/locked.go
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package locked implements a delegating wrapper that protects each method of
+// the resulting kcd.Reader or kcd.ReadWriter with a mutex, so that the result
+// is safe for concurrent use by multiple goroutines.
+package locked
+
+import (
+	"errors"
+	"io"
+	"sync"
+
+	"kythe.io/kythe/go/platform/kcd"
+
+	"golang.org/x/net/context"
+)
+
+type locker struct {
+	μ  sync.Mutex
+	rd kcd.Reader
+	wr kcd.Writer
+}
+
+// Reader returns a kcd.Reader that delegates to r with method calls protected
+// by a mutex.
+func Reader(r kcd.Reader) kcd.Reader { return &locker{rd: r} }
+
+// ReadWriter returns a kcd.ReadWriter that delegates to r with method calls
+// protected by a mutex.
+func ReadWriter(rw kcd.ReadWriter) kcd.ReadWriter { return &locker{rd: rw, wr: rw} }
+
+// ErrNotSupported is returned by the write methods if no Writer is available.
+var ErrNotSupported = errors.New("write operation not supported")
+
+// Revisions implements a method of kcd.Reader.
+func (db *locker) Revisions(ctx context.Context, want *kcd.RevisionsFilter, f func(kcd.Revision) error) error {
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.rd.Revisions(ctx, want, f)
+}
+
+// Find implements a method of kcd.Reader.
+func (db *locker) Find(ctx context.Context, filter *kcd.FindFilter, f func(string) error) error {
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.rd.Find(ctx, filter, f)
+}
+
+// Units implements a method of kcd.Reader.
+func (db *locker) Units(ctx context.Context, unitDigests []string, f func(digest, key string, data []byte) error) error {
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.rd.Units(ctx, unitDigests, f)
+}
+
+// Files implements a method of kcd.Reader.
+func (db *locker) Files(ctx context.Context, fileDigests []string, f func(string, []byte) error) error {
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.rd.Files(ctx, fileDigests, f)
+}
+
+// FilesExist implements a method of kcd.Reader.
+func (db *locker) FilesExist(ctx context.Context, fileDigests []string, f func(string) error) error {
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.rd.FilesExist(ctx, fileDigests, f)
+}
+
+// WriteRevision implements a method of kcd.Writer.
+func (db *locker) WriteRevision(ctx context.Context, rev kcd.Revision, replace bool) error {
+	if db.wr == nil {
+		return ErrNotSupported
+	}
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.wr.WriteRevision(ctx, rev, replace)
+}
+
+// WriteUnit implements a method of kcd.Writer.
+func (db *locker) WriteUnit(ctx context.Context, revision, corpus, formatKey string, unit kcd.Unit) (string, error) {
+	if db.wr == nil {
+		return "", ErrNotSupported
+	}
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.wr.WriteUnit(ctx, revision, corpus, formatKey, unit)
+}
+
+// WriteFile implements a method of kcd.Writer.
+func (db *locker) WriteFile(ctx context.Context, r io.Reader) (string, error) {
+	if db.wr == nil {
+		return "", ErrNotSupported
+	}
+	db.μ.Lock()
+	defer db.μ.Unlock()
+	return db.wr.WriteFile(ctx, r)
+}
