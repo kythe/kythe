15934ede17197f2972b9b449cd0a3ae255e94576
M. J. Fromberger <fromberger@google.com>
2017-02-27 09:49:36 -0800

    Specify an ordering rule for VNames.
    Summary:
    This affects the definition of Entry order, which is defined in terms of the
    order of source/target fields, themselves vnames. When we first wrote this, we
    had those fields as tickets; now that they're vnames it's underspecified.
    
    Add vnameutil.Compare, a library to compare vnames according to this rule.
    
    Reviewers: #core_team, shahms
    
    Reviewed By: #core_team, shahms
    
    Subscribers: shahms, #core_team
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1383

---
+++ kythe/go/util/vnameutil/order.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vnameutil
+
+import (
+	"strings"
+
+	spb "kythe.io/kythe/proto/storage_proto"
+)
+
+// Compare reports the relative order of two vnames, returning -1 if a < b, 0
+// if a == b, and 1 if a > b.
+func Compare(a, b *spb.VName) int {
+	for _, v := range [...]struct{ ap, bp *string }{
+		{&a.Corpus, &b.Corpus},
+		{&a.Language, &b.Language},
+		{&a.Path, &b.Path},
+		{&a.Root, &b.Root},
+		{&a.Signature, &b.Signature},
+	} {
+		if n := strings.Compare(*v.ap, *v.bp); n != 0 {
+			return n
+		}
+	}
+	return 0
+}
---
+++ kythe/go/util/vnameutil/order_test.go
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vnameutil
+
+import (
+	"testing"
+
+	spb "kythe.io/kythe/proto/storage_proto"
+)
+
+func TestOrder(t *testing.T) {
+	tests := []struct {
+		a, b V
+		want int
+	}{
+		{"_____", "_____", 0},
+		{"AB_DE", "AB_DE", 0},
+		{"_____", "A____", -1},
+		{"ABCDE", "ABCDF", -1},
+		{"ABCDE", "ABCDD", 1},
+		{"B____", "A____", 1},
+		{"AAAA_", "AAAAA", -1},
+		{"_ABCD", "_ACCD", -1},
+	}
+
+	for _, test := range tests {
+		got := Compare(test.a.vname(), test.b.vname())
+		if got != test.want {
+			t.Errorf("Compare(%q, %q): got %d, want %d", test.a, test.b, got, test.want)
+		}
+	}
+}
+
+// V is a compact representation of a vname for test construction.
+//
+// Valid values have 1 character per vname field: corpus, lang, path, root,
+// sig.  The field value is that character, except underscore (_) which is
+// turned into an empty string. Missing letters on the right are blanks.
+type V string
+
+func (v V) vname() *spb.VName {
+	var out spb.VName
+	ptrs := [...]*string{&out.Corpus, &out.Language, &out.Path, &out.Root, &out.Signature}
+
+	for i, ch := range v {
+		ptr := ptrs[i]
+		if ch == '_' {
+			*ptr = ""
+		} else {
+			*ptr = string(ch)
+		}
+	}
+	return &out
+}
