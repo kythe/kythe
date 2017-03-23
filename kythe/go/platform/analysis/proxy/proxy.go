21d5a40356e91a46534c1fa47f0fac3e7ba857d1
M. J. Fromberger <fromberger@google.com>
2017-03-23 12:02:47 -0700

    Add a JSON proxy wrapper for proto-incapable indexers.
    Summary:
    Some languages have difficulty handling protocol buffers, or at least find it
    much easier to work in JSON. To permit a driver to work with such languages,
    this change adds a "proxy" library that converts the interaction into a simple
    JSON protocol conducted over pipes.
    
    This hasn't yet been integrated into the existing driver tool, but I'd like to
    do that as a separate step. The typical mode of operation where we run the
    indexer as a subprocess and pass work to it will continue to work in this new
    regime, only the communication channel changes.
    
    Reviewers: #core_team, zarko, evmar
    
    Reviewed By: #core_team, zarko, evmar
    
    Subscribers: #core_team
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1443

---
+++ kythe/go/platform/analysis/proxy/proxy.go
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package proxy implements a wrapper that proxies an analysis request to a
+// Kythe indexer that does not speak protocol buffers or RPCs.
+//
+// Communication with the underlying indexer is via JSON over pipes, with
+// messages from the proxy to the indexer on one side and replies on the other.
+// Each JSON message is sent as a single complete line terminated by a newline
+// (LF) character.
+//
+// Communication between proxy and indexer is in lock-step; there is only one
+// transaction active at a time in either direction and the indexer initiates
+// all requests.
+//
+// The protocol between indexer (X) and proxy (P) is:
+//
+//   X → P: {"req":"analysis"}<LF>
+//   P → X: {"rsp":"ok","args":{"unit":<unit>,"rev":<revision>}}<LF>
+//          {"rsp":"error","args":<error>}<LF>
+//
+//   X → P: {"req":"output","args":[<entry>...]}<LF>
+//   P → X: {"rsp":"ok"}<LF>
+//          {"rsp":"error","args":<error>}<LF>
+//
+//   X → P: {"req":"done","args":{"ok":true,"msg":<error>}}<LF>
+//   P → X: {"rsp":"ok"}<LF>
+//          {"rsp":"error","args":<error>}<LF>
+//
+//   X → P: {"req":"file","args":{"path":<path>,"digest":<digest>}}<LF>
+//   P → X: {"rsp":"ok","args":{"path":<path>,"digest":<digest>,"content":<bytes>}}<LF>
+//          {"rsp":"error","args":<error>}<LF>
+//
+// Where:
+//
+//    <bytes>    -- BASE-64 encoded bytes (string)
+//    <digest>   -- file content digest (string)
+//    <entry>    -- JSON encoded kythe.proto.Entry message
+//    <error>    -- error diagnostic (string)
+//    <path>     -- file path (string)
+//    <revision> -- revision marker (string)
+//    <unit>     -- JSON encoded kythe.proto.CompilationUnit message
+//    <LF>       -- line feed character (decimal code 10)
+//
+// When rsp="error" in a reply, the args are an error string. The ordinary flow
+// for the indexer is:
+//
+//     {"req":"analysis"}   -- to start a new analysis task
+//     {"req":"output",...} -- to send outputs for the task
+//     {"req":"file",...}   -- to fetch required input files.
+//         ... (as many times as needed) ...
+//     {"req":"done",...}   -- to mark the analysis as complete
+//                             and to report success/failure
+//
+// In case of an indexing error, the indexer is free to terminate the analysis
+// early and report {"req":"done","args":{"ok":false}} to the driver.
+//
+// In case of an error writing output data, the driver will report an error in
+// response to the "output" call. Once this happens, the analysis is abandoned
+// as if the indexer had called "done". Subsequent calls to "output" or "done"
+// will behave as if no analysis is in progress. The indexer is free at that
+// point to start a new analysis.
+//
+package proxy
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+
+	apb "kythe.io/kythe/proto/analysis_proto"
+	spb "kythe.io/kythe/proto/storage_proto"
+)
+
+// New returns a proxy that reads requests from in and writes responses to out.
+// The caller must invoke Run to start processing requests.
+func New(in io.Reader, out io.Writer) *Proxy {
+	return &Proxy{
+		in:  json.NewDecoder(in),
+		out: json.NewEncoder(out),
+	}
+}
+
+// A request represents a request from the indexer to the proxy.
+type request struct {
+	Type string          `json:"req"` // the type of the request
+	Args json.RawMessage `json:"args,omitempty"`
+}
+
+// A response represents a response from the proxy to the indexer.
+type response struct {
+	Status string      `json:"rsp"` // status message
+	Args   interface{} `json:"args,omitempty"`
+}
+
+// A unit represents a compilation unit to be analyzed.
+type unit struct {
+	Unit     *apb.CompilationUnit `json:"unit"`
+	Revision string               `json:"rev,omitempty"`
+}
+
+// A file represents a file request or content reply.
+type file struct {
+	Path    string `json:"path,omitempty"`
+	Digest  string `json:"digest,omitempty"`
+	Content []byte `json:"content,omitempty"`
+}
+
+// A status represents a status message from a "done" request.
+type status struct {
+	OK      bool   `json:"ok,omitempty"`
+	Message string `json:"msg,omitempty"`
+}
+
+// A Proxy processes requests from an indexer subprocess on the other end of a
+// pipe by dispatching them to a callback handler.  The caller must invoke Run
+// to start processing requests.
+type Proxy struct {
+	in  *json.Decoder // input from the indexer
+	out *json.Encoder // output to the indexer
+	err error
+}
+
+// Run blocks serving requests from the indexer until communication with the
+// indexer reports an error. The resulting error is returned unless it is
+// io.EOF, in which case Run returns nil.
+func (p *Proxy) Run(h Handler) error {
+	var hasReq bool // true when a request is being processed
+	for {
+		var req request
+		if err := p.in.Decode(&req); err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+
+		switch req.Type {
+		case "analysis":
+			// Prerequisite: There is no analysis request already in progress.
+			if hasReq {
+				p.reply("error", "an analysis is already in progress")
+			} else if req, err := h.Analysis(); err != nil {
+				p.reply("error", err.Error())
+			} else {
+				hasReq = true
+				p.reply("ok", &unit{
+					Unit:     req.Compilation,
+					Revision: req.Revision,
+				})
+			}
+
+		case "output":
+			// Prerequisite: There is an analysis request in progress.
+			if !hasReq {
+				p.reply("error", "no analysis is in progress")
+				break
+			}
+			var entries []*spb.Entry
+			err := json.Unmarshal(req.Args, &entries)
+			if err != nil {
+				p.reply("error", "invalid entries: "+err.Error())
+			} else if err := h.Output(entries...); err != nil {
+				p.reply("error", err.Error())
+			} else {
+				p.reply("ok", nil)
+				break
+			}
+
+			// Analysis is now abandoned; report the error back to the
+			// handler and give up.
+			h.Done(err)
+			hasReq = false
+
+		case "done":
+			// Prerequisite: There is an analysis request in progress.
+			if !hasReq {
+				p.reply("error", "no analysis is in progress")
+				break
+			}
+			hasReq = false
+			if req.Args == nil {
+				h.Done(nil)
+			} else {
+				var stat status
+				if err := json.Unmarshal(req.Args, &stat); err != nil {
+					h.Done(err)
+					p.reply("error", "invalid status: "+err.Error())
+					break
+				} else if !stat.OK {
+					h.Done(errors.New("analysis failed: " + stat.Message))
+				} else {
+					h.Done(nil)
+				}
+			}
+			p.reply("ok", nil)
+
+		case "file":
+			// A file request can be issued at any time, though in practice it
+			// may fail when no request is present since different requests may
+			// come from different file sources.
+			var info file
+			if err := json.Unmarshal(req.Args, &info); err != nil {
+				p.reply("error", "invalid file request: "+err.Error())
+				break
+			} else if info.Path == "" && info.Digest == "" {
+				p.reply("error", "empty file request")
+				break
+			}
+			bits, err := h.File(info.Path, info.Digest)
+			if err != nil {
+				p.reply("error", err.Error())
+			} else {
+				p.reply("ok", &file{
+					Path:    info.Path,
+					Digest:  info.Digest,
+					Content: bits,
+				})
+			}
+
+		default:
+			p.reply("error", "invalid request: "+req.Type)
+		}
+
+		if p.err != nil {
+			return p.err // deferred from above
+		}
+	}
+}
+
+func (p *Proxy) reply(status string, args interface{}) {
+	p.err = p.out.Encode(&response{
+		Status: status,
+		Args:   args,
+	})
+}
+
+// A Handler provides callbacks to handle requests issued by the indexer.
+type Handler interface {
+	// Analysis fetches a new analysis request. It is an error if there is an
+	// unfinished request already in flight.
+	Analysis() (*apb.AnalysisRequest, error)
+
+	// Output delivers output from an ongoing analysis.
+	Output(...*spb.Entry) error
+
+	// Done reports an ongoing analysis as complete, with the error indicating
+	// success or failure.
+	Done(error)
+
+	// File requests the contents of a file given a path and/or digest, at
+	// least one of which must be nonempty.
+	File(path, digest string) ([]byte, error)
+}
---
+++ kythe/go/platform/analysis/proxy/proxy_test.go
@@ -0,0 +1,302 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package proxy
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"reflect"
+	"testing"
+
+	apb "kythe.io/kythe/proto/analysis_proto"
+	spb "kythe.io/kythe/proto/storage_proto"
+)
+
+// A testreq is the equivalent of a request, pre-encoding.
+type testreq struct {
+	Type string      `json:"req,omitempty"`
+	Args interface{} `json:"args,omitempty"`
+}
+
+// An indexer simulates an indexer process. It sends each of the requests in
+// its queue in turn, and records the responses.
+type indexer struct {
+	in  *json.Decoder
+	out *json.Encoder
+
+	reqs []testreq
+	rsps []string // JSON format
+	errc chan error
+}
+
+func (ix *indexer) run() {
+	defer close(ix.errc)
+	for _, req := range ix.reqs {
+		if err := ix.out.Encode(req); err != nil {
+			ix.errc <- err
+			return
+		}
+
+		var rsp response
+		if err := ix.in.Decode(&rsp); err != nil {
+			ix.errc <- err
+			return
+		}
+
+		jrsp := mustMarshal(&rsp)
+		ix.rsps = append(ix.rsps, jrsp)
+	}
+}
+
+func newIndexer(in io.Reader, out io.Writer, reqs ...testreq) *indexer {
+	return &indexer{
+		in:   json.NewDecoder(in),
+		out:  json.NewEncoder(out),
+		reqs: reqs,
+		errc: make(chan error, 1),
+	}
+}
+
+func (ix *indexer) wait() error { return <-ix.errc }
+
+type handler struct {
+	analysis func() (*apb.AnalysisRequest, error)
+	output   func(...*spb.Entry) error
+	done     func(error)
+	file     func(path, digest string) ([]byte, error)
+}
+
+// Analysis implements part of the Handler interface.  If no function is set,
+// testReq is returned without error.
+func (h handler) Analysis() (*apb.AnalysisRequest, error) {
+	if f := h.analysis; f != nil {
+		return f()
+	}
+	return testReq, nil
+}
+
+// Output implements part of the Handler interface. If no function is set, the
+// output is discarded without error.
+func (h handler) Output(entries ...*spb.Entry) error {
+	if f := h.output; f != nil {
+		return f(entries...)
+	}
+	return nil
+}
+
+// Done implements part of the Handler interface. If no function is set, this
+// is silently a no-op.
+func (h handler) Done(err error) {
+	if f := h.done; f != nil {
+		f(err)
+	}
+}
+
+// File implements part of the Handler interface. If no function is set, an
+// error with string "notfound" is returned.
+func (h handler) File(path, digest string) ([]byte, error) {
+	if f := h.file; f != nil {
+		return f(path, digest)
+	}
+	return nil, errors.New("notfound")
+}
+
+// runProxy runs an indexer with the given requests and a proxy delegating to
+// h.  The responses collected by the indexer are returned.
+func runProxy(h Handler, reqs ...testreq) ([]string, error) {
+	pin, pout := io.Pipe() // proxy to indexer
+	xin, xout := io.Pipe() // indexer to proxy
+	ix := newIndexer(xin, pout, reqs...)
+	go func() {
+		defer pout.Close() // signal EOF to the driver
+		ix.run()
+	}()
+	err := New(pin, xout).Run(h)
+	if err != nil {
+		xout.Close()
+	}
+	if xerr := ix.wait(); err == nil {
+		err = xerr
+	}
+	return ix.rsps, err
+}
+
+// Dummy values for testing.
+var (
+	testReq = &apb.AnalysisRequest{
+		Compilation: &apb.CompilationUnit{
+			VName: &spb.VName{Signature: "test"},
+		},
+	}
+
+	testEntries = []*spb.Entry{
+		{Source: &spb.VName{Signature: "A"}, EdgeKind: "loves", Target: &spb.VName{Signature: "B"}},
+		{Source: &spb.VName{Signature: "C"}, FactName: "versus", FactValue: []byte("D")},
+	}
+)
+
+const analysisReply = `{"rsp":"ok","args":{"unit":{"v_name":{"signature":"test"}}}}`
+
+func mustMarshal(v interface{}) string {
+	bits, err := json.Marshal(v)
+	if err != nil {
+		log.Fatalf("Error marshaling JSON: %v", err)
+	}
+	return string(bits)
+}
+
+func TestNOOP(t *testing.T) {
+	// Verify that startup and shutdown are clean.
+	if rsps, err := runProxy(handler{}); err != nil {
+		t.Errorf("Proxy failed on empty input: %v", err)
+	} else if len(rsps) != 0 {
+		t.Errorf("Empty input returned responses: %+v", rsps)
+	}
+}
+
+func TestErrors(t *testing.T) {
+	tests := []struct {
+		desc string
+		h    Handler
+		reqs []testreq
+		want []string
+	}{{
+		desc: "Error getting analysis",
+		h:    handler{analysis: func() (*apb.AnalysisRequest, error) { return nil, errors.New("bad") }},
+		reqs: []testreq{{Type: "analysis"}},
+		want: []string{`{"rsp":"error","args":"bad"}`},
+	}, {
+		desc: "Error sending outputs",
+		h:    handler{output: func(...*spb.Entry) error { return errors.New("bad") }},
+		reqs: []testreq{{Type: "analysis"}, {Type: "output", Args: testEntries}},
+		want: []string{
+			analysisReply,
+			`{"rsp":"error","args":"bad"}`,
+		},
+	}, {
+		desc: "Requests sent out of order provoke an error",
+		h:    handler{},
+		reqs: []testreq{
+			{Type: "done", Args: status{OK: true}},
+			{Type: "output", Args: testEntries},
+		},
+		want: []string{
+			`{"rsp":"error","args":"no analysis is in progress"}`,
+			`{"rsp":"error","args":"no analysis is in progress"}`,
+		},
+	}, {
+		desc: "File not found",
+		h:    handler{},
+		reqs: []testreq{{Type: "file", Args: file{Path: "foo", Digest: "bar"}}},
+		want: []string{`{"rsp":"error","args":"notfound"}`},
+	}, {
+		desc: "An error in output terminates the analysis",
+		h: handler{
+			output: func(es ...*spb.Entry) error {
+				if es[0].EdgeKind == "fail" {
+					return errors.New("bad")
+				}
+				return nil
+			},
+		},
+		reqs: []testreq{
+			{Type: "analysis"},                                            // succeeds
+			{Type: "output", Args: []*spb.Entry{{EdgeKind: "ok"}}},        // succeeds
+			{Type: "output", Args: []*spb.Entry{{EdgeKind: "fail"}}},      // fails
+			{Type: "output", Args: []*spb.Entry{{EdgeKind: "wah"}}},       // fails
+			{Type: "done", Args: status{OK: false, Message: "cat abuse"}}, // fails
+			{Type: "analysis"},                                            // succeeds
+		},
+		want: []string{
+			analysisReply,
+			`{"rsp":"ok"}`,
+			`{"rsp":"error","args":"bad"}`,
+			`{"rsp":"error","args":"no analysis is in progress"}`,
+			`{"rsp":"error","args":"no analysis is in progress"}`,
+			analysisReply,
+		},
+	}}
+
+	for _, test := range tests {
+		t.Log("Testing:", test.desc)
+		t.Logf(" - requests: %#q", test.reqs)
+		rsps, err := runProxy(test.h, test.reqs...)
+		if err != nil {
+			t.Errorf("Unexpected error from proxy: %v", err)
+		}
+		t.Logf(" - responses: %+v", rsps)
+		if !reflect.DeepEqual(rsps, test.want) {
+			t.Errorf("Incorrect responses; wanted %+v", test.want)
+		}
+	}
+}
+
+func TestAnalysisWorks(t *testing.T) {
+	// Enact a standard analyzer transaction, and verify that the responses work.
+	var doneCalled bool
+	var gotEntries []*spb.Entry
+
+	rsps, err := runProxy(handler{
+		done: func(err error) {
+			if err != nil {
+				t.Errorf("Done was called with an error: %v", err)
+			}
+			doneCalled = true
+		},
+		output: func(es ...*spb.Entry) error {
+			gotEntries = append(gotEntries, es...)
+			return nil
+		},
+		file: func(path, digest string) ([]byte, error) {
+			if path == "exists" {
+				return []byte("data"), nil
+			}
+			return nil, errors.New("notfound")
+		},
+	},
+		testreq{Type: "analysis"},
+		testreq{Type: "file", Args: file{Path: "exists"}},
+		testreq{Type: "output", Args: testEntries},
+		testreq{Type: "file", Args: file{Path: "does not exist"}},
+		testreq{Type: "done"},
+	)
+	if err != nil {
+		t.Errorf("Unexpected error from proxy: %v", err)
+	}
+
+	// Verify that the proxy followed the protocol reasonably.
+	if !doneCalled {
+		t.Error("The handler's Done method was never called")
+	}
+	if !reflect.DeepEqual(gotEntries, testEntries) {
+		t.Errorf("Incorrect entries:\n got: %+v\nwant: %+v", gotEntries, testEntries)
+	}
+
+	// Verify that we got the expected replies back from the proxy.
+	want := []string{
+		analysisReply, // from Analyze
+		`{"rsp":"ok","args":{"content":"ZGF0YQ==","path":"exists"}}`, // from File (1)
+		`{"rsp":"ok"}`,                      // from Output
+		`{"rsp":"error","args":"notfound"}`, // from File (2)
+		`{"rsp":"ok"}`,                      // from Done
+	}
+	if !reflect.DeepEqual(rsps, want) {
+		t.Errorf("Wrong incorrect responses:\n got: %+v\nwant: %+v", rsps, want)
+	}
+}
