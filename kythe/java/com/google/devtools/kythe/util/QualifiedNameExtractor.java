69485d3895f56d3736823f36e9c658ae500eef4f
Craig Barber <craigbarber@google.com>
2017-07-17 10:05:05 -0700

    Added utility library for resolving qualified names in java.
    Reviewers: schroederc, zarko, #core_team
    
    Reviewed By: zarko, #core_team
    
    Subscribers: jrtom, zarko, #core_team
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1774

---
+++ kythe/java/com/google/devtools/kythe/util/QualifiedNameExtractor.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.kythe.util;
+
+import com.google.devtools.kythe.proto.MarkedSource;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Provides a library of functions for extracting qualified names from java record/class marked
+ * source nodes.
+ */
+public class QualifiedNameExtractor {
+  /**
+   * Extracts a qualified name from the specified {@link MarkedSource} tree.
+   *
+   * @param markedSource The {@link MarkedSource} tree to be evaluated.
+   * @return The resolved qualified name if found.
+   */
+  public static Optional<String> extractNameFromMarkedSource(MarkedSource markedSource) {
+    // extract the class name
+    Optional<MarkedSource> identifier =
+        retrieveFirstMarkedSourceByKind(
+            markedSource, MarkedSource.Kind.IDENTIFIER, /* isPretextRequired */ true);
+    if (!identifier.isPresent()) {
+      // if the current class node's marked source does not contain an identifier, skip it
+      return Optional.empty();
+    }
+    String className = identifier.get().getPreText();
+
+    // extract the class's package name
+    Optional<MarkedSource> context =
+        retrieveFirstMarkedSourceByKind(
+            markedSource, MarkedSource.Kind.CONTEXT, /* isPretextRequired */ false);
+    if (!context.isPresent()) {
+      // if the current class node's marked source does not contain a context, skip it
+      return Optional.empty();
+    }
+    String packageDelim =
+        !context.get().getPostChildText().isEmpty() ? context.get().getPostChildText() : ".";
+    String packageName =
+        String.join(
+            packageDelim,
+            context
+                .get()
+                .getChildList()
+                .stream()
+                .filter(
+                    child ->
+                        child.getKind().equals(MarkedSource.Kind.IDENTIFIER)
+                            && !child.getPreText().isEmpty())
+                .map(ident -> ident.getPreText())
+                .collect(Collectors.toList()));
+
+    // emit the qualified class name
+    return Optional.of(String.format("%s%s%s", packageName, packageDelim, className));
+  }
+
+  /**
+   * Traverse the specified {@link MarkedSource} tree, returning the first node that matches the
+   * specified kind.
+   *
+   * @param markedSource The root of the {@link MarkedSource} tree to be traversed.
+   * @param kind The kind of the requested node.
+   * @param isPretextRequired Whether or not the existence of a pretext string is required.
+   */
+  private static Optional<MarkedSource> retrieveFirstMarkedSourceByKind(
+      MarkedSource markedSource, MarkedSource.Kind kind, boolean isPretextRequired) {
+    Optional<MarkedSource> result =
+        markedSource
+            .getChildList()
+            .stream()
+            .filter(
+                child ->
+                    child.getKind().equals(kind)
+                        && (isPretextRequired ? !child.getPreText().isEmpty() : true))
+            .findFirst();
+    if (result.isPresent()) {
+      return result;
+    }
+
+    for (MarkedSource child : markedSource.getChildList()) {
+      result = retrieveFirstMarkedSourceByKind(child, kind, isPretextRequired);
+      if (result.isPresent()) {
+        return result;
+      }
+    }
+
+    return Optional.empty();
+  }
+}
---
+++ kythe/javatests/com/google/devtools/kythe/util/QualifiedNameExtractorTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devtools.kythe.util;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.devtools.kythe.proto.MarkedSource;
+import com.google.protobuf.TextFormat;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests {@link QualifiedNameExtractor}. */
+@RunWith(JUnit4.class)
+public class QualifiedNameExtractorTest {
+  @Test
+  public void testExtractNameFromMarkedSourceReturnsProperly() throws Exception {
+    MarkedSource.Builder builder = MarkedSource.newBuilder();
+    TextFormat.merge(
+        "child {\nkind: CONTEXT\nchild {\nkind: IDENTIFIER\npre_text: \"java\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"com\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"google\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"devtools\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"kythe\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"analyzers\"\n} \nchild {\nkind: IDENTIFIER\npre_text: \"java\"\n} \npost_child_text: \".\"\nadd_final_list_token: true\n} \nchild {\nkind: IDENTIFIER\npre_text: \"JavaEntrySets\"\n}",
+        builder);
+    MarkedSource testInput = builder.build();
+    Optional<String> resolvedName = QualifiedNameExtractor.extractNameFromMarkedSource(testInput);
+    assertThat(resolvedName.isPresent()).isTrue();
+    assertThat(resolvedName.get())
+        .isEqualTo("java.com.google.devtools.kythe.analyzers.java.JavaEntrySets");
+  }
+
+  @Test
+  public void testExtractNameFromMarkedSourceProperlyHandlesMissingContext() throws Exception {
+    MarkedSource.Builder builder = MarkedSource.newBuilder();
+    TextFormat.merge(
+        "child {\nchild {\nkind: IDENTIFIER\npre_text: \"JavaEntrySets\"\n}\n}", builder);
+    MarkedSource testInput = builder.build();
+    Optional<String> resolvedName = QualifiedNameExtractor.extractNameFromMarkedSource(testInput);
+    assertThat(resolvedName.isPresent()).isFalse();
+  }
+
+  @Test
+  public void testExtractNameFromMarkedSourceProperlyHandlesMissingData() throws Exception {
+    MarkedSource.Builder builder = MarkedSource.newBuilder();
+    TextFormat.merge("child {}", builder);
+    MarkedSource testInput = builder.build();
+    Optional<String> resolvedName = QualifiedNameExtractor.extractNameFromMarkedSource(testInput);
+    assertThat(resolvedName.isPresent()).isFalse();
+  }
+}
