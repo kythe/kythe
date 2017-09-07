dcb4cf505c33956649190e9d4daa2ae7cee42122
Evan Martin <evan.martin@gmail.com>
2017-09-07 12:12:17 -0700

    typescript: upgrade to typescript 2.4
    Summary:
    - Adjust the version dependency in package.json.  Note: using
    "^2.4" brought in 2.5, but in my experience the TypeScript compiler
    APIs do not follow semver, so switch to "~2.4" for a tighter bound
    on the version.  (See yarn.lock for the version yarn thinks is
    appropriate.)
    
    - Delete the shim code that made us build with 2.3 or 2.4.
    
    Reviewers: #core_team, shahms
    
    Reviewed By: #core_team, shahms
    
    Subscribers: #core_team
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1925

--- kythe/typescript/indexer.ts
+++ kythe/typescript/indexer.ts
@@ -21,14 +21,6 @@ import * as ts from 'typescript';
 
 import * as utf8 from './utf8';
 
-// Forward-declare a type found in TypeScript 2.4 to make this
-// code work both in TS2.3 and TS2.4.
-declare module 'typescript' {
-  interface NamedDeclaration extends ts.Declaration {
-    name?: ts.DeclarationName;
-  }
-}
-
 /** VName is the type of Kythe node identities. */
 export interface VName {
   signature: string;
