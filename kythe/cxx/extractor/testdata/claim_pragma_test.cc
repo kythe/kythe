811b9e780018a94658e97aa373df78db1fbfd82e
Shahms King <shahms@google.com>
2017-10-18 09:58:16 -0700

    Add and use hash-canonicalizing field comparator
    Summary: Next step in cleaning up shell-based extractor tests
    
    Reviewers: #core_team, zarko
    
    Reviewed By: #core_team, zarko
    
    Subscribers: jrtom, schroederc
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D1212

--- kythe/cxx/extractor/testdata/claim_pragma_test.cc
+++ kythe/cxx/extractor/testdata/claim_pragma_test.cc
@@ -8,6 +8,7 @@
 #include "absl/memory/memory.h"
 #include "gmock/gmock.h"
 #include "google/protobuf/text_format.h"
+#include "google/protobuf/util/field_comparator.h"
 #include "google/protobuf/util/message_differencer.h"
 #include "gtest/gtest.h"
 #include "kythe/cxx/common/language.h"
@@ -15,11 +16,15 @@
 namespace kythe {
 namespace {
 
+using ::google::protobuf::FieldDescriptor;
+using ::google::protobuf::Message;
 using ::google::protobuf::TextFormat;
+using ::google::protobuf::util::DefaultFieldComparator;
 using ::google::protobuf::util::MessageDifferencer;
 using ::testing::ElementsAre;
 
-// TODO(shahms): Compare canonicalized hashes again.
+using pbstring = ::google::protobuf::string;
+
 constexpr char kExpectedContents[] = R"(
 v_name {
   language: "c++"
@@ -34,14 +39,14 @@ required_input {
   }
   context {
     row {
-      #source_context: "hash0"
+      source_context: "hash0"
       column {
         offset: 12
-        #linked_context: "hash1"
+        linked_context: "hash1"
       }
       column {
         offset: 33
-        #linked_context: "hash1"
+        linked_context: "hash1"
       }
       always_process: true
     }
@@ -57,7 +62,7 @@ required_input {
   }
   context {
     row {
-      #source_context: "hash1"
+      source_context: "hash1"
       always_process: true
     }
   }
@@ -72,7 +77,7 @@ required_input {
   }
   context {
     row {
-      #source_context: "hash1"
+      source_context: "hash1"
     }
   }
 }
@@ -87,8 +92,110 @@ argument: "-I./kythe/cxx/extractor/testdata"
 argument: "./kythe/cxx/extractor/testdata/claim_main.cc"
 argument: "-fsyntax-only"
 source_file: "./kythe/cxx/extractor/testdata/claim_main.cc"
-#entry_context: "hash0"
+entry_context: "hash0"
 )";
+
+// Returns the `FieldDescriptor*` for the nested field found
+// by following the provided path components or null, e.g.
+//   auto pb = Parse(R"(
+//     message {
+//       inner {
+//         field: 10
+//       }
+//     })");
+//   const FieldDescriptor* message_inner_field =
+//       FindFieldByLowercasePath(pb.GetDescriptor(),
+//                                {"message", "inner", "field"});
+const FieldDescriptor* FindNestedFieldByLowercasePath(
+    const google::protobuf::Descriptor* descriptor,
+    const std::vector<pbstring>& field_names) {
+  const FieldDescriptor* field = nullptr;
+  for (const auto& name : field_names) {
+    if (descriptor == nullptr) return nullptr;
+    field = descriptor->FindFieldByLowercaseName(name);
+    if (field == nullptr) return nullptr;
+    descriptor = field->message_type();
+  }
+  return field;
+}
+
+std::vector<const FieldDescriptor*> CanonicalizedHashFields(
+    const kythe::proto::CompilationUnit& unit) {
+  const auto* desc = unit.GetDescriptor();
+  auto field = [desc](const std::vector<pbstring>& field_names) {
+    return FindNestedFieldByLowercasePath(desc, field_names);
+  };
+  return {
+      field({"entry_context"}),
+      field({"required_input", "context", "row", "source_context"}),
+      field({"required_input", "context", "row", "column", "linked_context"}),
+  };
+}
+
+class CanonicalHashComparator : public DefaultFieldComparator {
+ public:
+  bool CanonicalizeHashField(const FieldDescriptor* field) {
+    CHECK(field->cpp_type() == FieldDescriptor::CPPTYPE_STRING)
+        << "Field must be bytes or string.";
+    auto result = canonical_fields_.insert(field);
+    return result.second;
+  }
+
+ private:
+  using HashMap = std::unordered_map<pbstring, size_t>;
+
+  ComparisonResult Compare(
+      const Message& message_1, const Message& message_2,
+      const FieldDescriptor* field, int index_1, int index_2,
+      const google::protobuf::util::FieldContext* field_context) override {
+    // Fall back to the default if this isn't a canonicalized field.
+    if (canonical_fields_.find(field) == canonical_fields_.end()) {
+      return DefaultFieldComparator::Compare(message_1, message_2, field,
+                                             index_1, index_2, field_context);
+    }
+    const auto* reflection_1 = message_1.GetReflection();
+    const auto* reflection_2 = message_2.GetReflection();
+    if (field->is_repeated()) {
+      // Allocate scratch strings to store the result if a conversion is
+      // needed.
+      pbstring scratch1;
+      pbstring scratch2;
+      return CompareCanonicalHash(reflection_1->GetRepeatedStringReference(
+                                      message_1, field, index_1, &scratch1),
+                                  reflection_2->GetRepeatedStringReference(
+                                      message_2, field, index_2, &scratch2));
+    } else {
+      // Allocate scratch strings to store the result if a conversion is
+      // needed.
+      pbstring scratch1;
+      pbstring scratch2;
+      return CompareCanonicalHash(
+          reflection_1->GetStringReference(message_1, field, &scratch1),
+          reflection_2->GetStringReference(message_2, field, &scratch2));
+    }
+  }
+
+  ComparisonResult CompareCanonicalHash(const pbstring& string_1,
+                                        const pbstring& string_2) {
+    return HashIndex(&left_message_hashes_, string_1) ==
+                   HashIndex(&right_message_hashes_, string_2)
+               ? SAME
+               : DIFFERENT;
+  }
+
+  static size_t HashIndex(HashMap* canonical_map, const pbstring& hash) {
+    size_t index = canonical_map->size();
+    // We use an index equivalent to the visitation order of the hashes.
+    // This is potentially fragile as we really only care if a protocol buffer
+    // is self-consistent, but will suffice for unit tests.
+    auto result = canonical_map->insert({hash, index});
+    return result.first->second;
+  }
+
+  std::unordered_set<const FieldDescriptor*> canonical_fields_;
+  HashMap left_message_hashes_;
+  HashMap right_message_hashes_;
+};
 
 class FakeIndexWriterSink : public kythe::IndexWriterSink {
  public:
@@ -102,9 +209,14 @@ class FakeIndexWriterSink : public kythe::IndexWriterSink {
 
     const auto& desc = *unit.GetDescriptor();
 
+    CanonicalHashComparator hash_compare;
+    for (const auto* field : CanonicalizedHashFields(unit)) {
+      hash_compare.CanonicalizeHashField(field);
+    }
     MessageDifferencer diff;
     diff.set_message_field_comparison(MessageDifferencer::EQUIVALENT);
     diff.set_scope(MessageDifferencer::PARTIAL);
+    diff.set_field_comparator(&hash_compare);
     diff.TreatAsSet(desc.FindFieldByLowercaseName("required_input"));
     // We need to ignore the value of the '-target' argument, so
     // do the comparison separately.
