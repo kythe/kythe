72bc2b065dfd942ecaf8263b40519cb219a15670
Daniel Moy <danielmoy@google.com>
2018-05-04 13:17:04 -0700

    Fix golint error only caught downstream.
    Reviewers: zarko, #core_team
    
    Reviewed By: zarko, #core_team
    
    Subscribers: #core_team
    
    Differential Revision: https://phabricator-dot-kythe-repo.appspot.com/D2280

--- kythe/go/extractors/config/extractor.go
+++ kythe/go/extractors/config/extractor.go
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// Package extractor provides a library for running kythe extraction on a single
-// repository.
 package config
 
 import (
