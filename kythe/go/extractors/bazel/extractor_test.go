a3fa17b77ed19b73989f1557d76c23548744cabb
M. J. Fromberger <fromberger@google.com>
2018-06-01 10:06:29 -0700

    Make FindSourceArgs return a new-style fixup.
    As part of migrating to a fixup interface that uses the compilation unit
    directly, update the FindSourceArgs constructor to return the new style.

--- kythe/go/extractors/bazel/extractor_test.go
+++ kythe/go/extractors/bazel/extractor_test.go
@@ -304,26 +304,24 @@ func TestFetchInputs(t *testing.T) {
 }
 
 func TestFindSourceArgs(t *testing.T) {
-	unit := &kindex.Compilation{
-		Proto: &apb.CompilationUnit{
-			RequiredInput: []*apb.CompilationUnit_FileInput{{
-				Info: &apb.FileInfo{Path: "a/b/c.go"},
-			}, {
-				Info: &apb.FileInfo{Path: "d/e/f.cc"},
-			}, {
-				Info: &apb.FileInfo{Path: "old"},
-			}},
-			SourceFile: []string{"old"},
-			// Matches:        no      yes, keep   yes, skip   no
-			Argument: []string{"blah", "a/b/c.go", "p/d/q.go", "quux"},
-		},
+	unit := &apb.CompilationUnit{
+		RequiredInput: []*apb.CompilationUnit_FileInput{{
+			Info: &apb.FileInfo{Path: "a/b/c.go"},
+		}, {
+			Info: &apb.FileInfo{Path: "d/e/f.cc"},
+		}, {
+			Info: &apb.FileInfo{Path: "old"},
+		}},
+		SourceFile: []string{"old"},
+		// Matches:        no      yes, keep   yes, skip   no
+		Argument: []string{"blah", "a/b/c.go", "p/d/q.go", "quux"},
 	}
 	// Results:      new         extant
 	want := []string{"a/b/c.go", "old"}
 
 	r := regexp.MustCompile(`\.go$`)
 	FindSourceArgs(r)(unit)
-	if got := unit.Proto.SourceFile; !reflect.DeepEqual(got, want) {
+	if got := unit.SourceFile; !reflect.DeepEqual(got, want) {
 		t.Errorf("FindSourceArgs: got %+q, want %+q", got, want)
 	}
 }
