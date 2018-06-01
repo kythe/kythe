9497f7aa7255fc364c6f6d38ea2b64d49c3f83eb
Salvatore Guarnieri <salguarnieri@google.com>
2018-06-01 11:21:32 -0700

    use special ctor

--- kythe/java/com/google/devtools/kythe/analyzers/base/EdgeKind.java
+++ kythe/java/com/google/devtools/kythe/analyzers/base/EdgeKind.java
@@ -24,7 +24,7 @@ public enum EdgeKind {
   TAGGED(true, "tagged"),
 
   // Edges from an anchor.
-  IMPUTES("imputes"),
+  IMPUTES(true, "imputes"),
   REF(true, "ref"),
   REF_CALL(true, "ref/call"),
   REF_DOC(true, "ref/doc"),
