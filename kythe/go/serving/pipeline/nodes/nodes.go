abaca070f735c69bf97b85349bfaae355651ed5f
Cody Schroeder <schroederc@google.com>
2018-06-04 14:24:32 -0700

    Better nodes.Filter documentation

--- kythe/go/serving/pipeline/nodes/nodes.go
+++ kythe/go/serving/pipeline/nodes/nodes.go
@@ -212,10 +212,51 @@ func embedSourceKey(src *spb.VName, n *ppb.Node) *ppb.Node {
 // Filter is a beam DoFn that emits *ppb.Nodes matching a set of kinds/subkinds.
 // Optionally, each processed node's facts/edges will also be filtered to the
 // desired set.
+//
+// The semantics of the Filter are such that a "zero"-value Filter will pass all
+// Nodes through unaltered.  Part of the filter only applies if set to a non-nil
+// value and all parts are applied independently.
+//
+// Examples:
+//
+//   Emit only "record" nodes with the "class" subkind with all their facts/edges:
+//     &Filter {
+//       FilterByKind:    []string{"record"},
+//       FilterBySubkind: []string{"class"},
+//     }
+//
+//   Emit only "anchor" nodes (any subkind) with all their facts/edges:
+//     &Filter {FilterByKind: []string{"anchor"}}
+//
+//   Emit only "anchor" nodes with only the loc/{start,end} facts and no edges:
+//     &Filter {
+//       FilterByKind: []string{"anchor"},
+//       IncludeFacts: []string{"/kythe/loc/start", "/kythe/loc/end"},
+//       IncludeEdges: []string{},
+//     }
+//
+//   Emit only "anchor" nodes with their "childof" edges (but all their facts):
+//     &Filter {
+//       FilterByKind: []string{"anchor"},
+//       IncludeEdges: []string{"/kythe/edge/childof"},
+//     }
+//
+//   Emit all nodes without any of their edges (but all their facts):
+//     &Filter {IncludeEdges: []string{}}
 type Filter struct {
-	FilterByKind, FilterBySubkind []string
+	// FilterByKind, if non-nil, configures the filter to only pass through nodes
+	// that match one of the given kinds.
+	FilterByKind []string
+	// FilterBySubkind, if non-nil, configures the filter to only pass through
+	// nodes that match one of the given subkinds.
+	FilterBySubkind []string
 
-	IncludeFacts, IncludeEdges []string
+	// IncludeFacts, if non-nil, configures the filter remove all facts not
+	// explicitly contained with the slice.
+	IncludeFacts []string
+	// IncludeEdges, if non-nil, configures the filter remove all edges with a
+	// kind not explicitly contained with the slice.
+	IncludeEdges []string
 }
 
 // ProcessElement emits the given Node if it matches the given Filter.
