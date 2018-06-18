cbf4c8ab0801441921d99d76890731a281572dc8
Cody Schroeder <schroederc@google.com>
2018-06-18 13:41:25 -0700

    Remove unused test file

--- kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/AnnotationComments.java
+++
@@ -1,8 +0,0 @@
-package pkg;
-import javax.annotation.Generated;
-//- @+4AnnotationComments defines/binding AnnotationComments
-//- CommentNode documents AnnotationComments
-/** ac */
-@Generated("test")
-public class AnnotationComments {
-}
