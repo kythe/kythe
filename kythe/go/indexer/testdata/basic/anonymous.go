12abcb66512681a7fd7be7d33543d555c1d973b9
M. J. Fromberger <fromberger@google.com>
2018-07-13 13:53:59 -0700

    Go: Fix declarations of anonymous struct type fields.
    Addresses #2827. Previously, the code to do this depended on there being a
    composite literal to initialize the value. But that isn't always present.  When
    it isn't, the expression type itself will have the information we need.
    
    The interesting cases are:
    
       var v struct { X int }
    
    and
    
       var w = struct { X int }{X: 25}
    
    Both are possible, but in that case we want the first case to dominate.
    
    I updated the existing tests to cover this better, and added a test to catch
    the specific case that motivated #2827.

--- kythe/go/indexer/testdata/basic/anonymous.go
+++ kythe/go/indexer/testdata/basic/anonymous.go
@@ -28,7 +28,7 @@ func f(planx struct {
 	return planx.T
 }
 
-var _ = struct {
+var v = struct {
 	//- @V defines/binding V
 	//- V.node/kind variable
 	//- V.subkind field
@@ -37,6 +37,19 @@ var _ = struct {
 	//- @V ref V
 	V: 25,
 }
+
+//- @V ref V
+var _ = v.V
+
+var w struct {
+	//- @X defines/binding X
+	//- X.node/kind variable
+	//- X.subkind field
+	X uint32
+}
+
+//- @X ref X
+var _ = w.X
 
 //- @elt defines/binding Elt
 //- Elt.node/kind variable
