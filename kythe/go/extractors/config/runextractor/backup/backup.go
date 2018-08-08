31a150c126638e73e293c6b2120ee3d1ca66a292
Daniel Moy <danielmoy@google.com>
2018-08-07 21:22:17 -0700

    doc comments are good (#2907)
    * doc comments are good

--- kythe/go/extractors/config/runextractor/backup/backup.go
+++ kythe/go/extractors/config/runextractor/backup/backup.go
@@ -36,10 +36,16 @@ import (
 	"path/filepath"
 )
 
+// A File records the locations of an original file and a backup copy of that
+// file.  The Restore method replaces the contents of the original with the
+// backup, overwriting any changes that were made since the backup was created.
 type File struct {
 	orig, tmp string // file paths
 }
 
+// New creates a backup copy of the specified file, located in the same
+// directory. The caller should ensure the Release method is called when the
+// backup is no longer needed, to clean up.
 func New(orig string) (*File, error) {
 	f, err := os.Open(orig)
 	if err != nil {
