87bb587be45c01d8b0fe52661f5144315b794eec
Cody Schroeder <schroederc@google.com>
2018-12-06 09:20:26 -0800

    fix(java_indexer): ensure static import refs are static

--- kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/StaticMethods.java
+++ kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/StaticMethods.java
@@ -2,6 +2,12 @@ package pkg;
 
 public class StaticMethods {
 
+  //- @member defines/binding PrivateMember
+  private int member;
+
+  //- @member defines/binding MemberFunc
+  public static int member() { return 0; }
+
   //- @staticMethod defines/binding StaticBool
   public static void staticMethod(boolean b) {}
 
