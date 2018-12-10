7655c354742a70b373247f8842324c85eb750d30
Daniel Moy <danielmoy@google.com>
2018-12-10 15:08:52 -0800

    fix: change tools/modules/update.sh to use an archive instead of clone
    One way of fixing #3307.
    
    This will make the initial download of llvm and clang faster, by not
    requiring a clone of the entire ~3GB repo.  It also save on the ongoing
    disk used.  However, it does make updates marginally slower because you
    have to re-download a new archive.  Given that updates are vastly
    dominated by the cost of building llvm&clang, this is probably ok.
    
    Also this has the side effect of making images for Google Cloud Build +
    bazel significantly smaller, which will be nice for #3210.

--- tools/modules/update.sh
+++ tools/modules/update.sh
@@ -16,28 +16,30 @@
 # update_modules.sh updates the repositories for external code (to the precise
 # revisions used for testing).
 #
-# If the single argument `--git_only` is passed, the repositories will be
+# If the single argument `--fetch_only` is passed, the repositories will be
 # updated to the pinned versions without configuring or building them.
 # If the argument `--build_only` is passed, the script will assume that the
 # repositories are at the correct version and will configure and build them.
 
-git_maybe_clone() {
-  local repo="$1"
+wget_copy_archive() {
+  local target="$1"
   local dir="$2"
-  if [[ ! -d "$dir/.git" ]]; then
-    git clone "$repo" "$dir"
-  fi
-}
+  local sha="$3"
 
-git_checkout_sha() {
-  local repo="$1"
-  local sha="$2"
-  cd "$repo"
-  if [[ "$sha" != "$(git rev-parse HEAD)" ]]; then
-    git fetch origin master
-    git checkout -f "$sha"
+  if [[ ! -f "$dir/$sha.sentinel" ]]; then
+    wget "https://github.com/llvm-mirror/$target/archive/$sha.zip"
+    rm -rf "$dir"
+    mkdir -p "$dir"
+    local tmpdir=$(mktemp -d)
+    unzip "$sha.zip" -d "$tmpdir/"
+    # This relies on the behavior of github to always produce a zip archive with
+    # a subdirectory named "repo-###sha###" that contains the repo inside it.
+    mv -T "$tmpdir/$target-$sha" "$dir"
+    rmdir "$tmpdir"
+    rm "$sha.zip"
+    # Leave an empty file so that we know what version we have checked out.
+    touch "$dir/$sha.sentinel"
   fi
-  cd - >/dev/null
 }
 
 cd "$(dirname $0)/../.."
@@ -58,15 +60,12 @@ if [[ -d "$LLVM_REPO/build" && ! -h "$LLVM_REPO/build" ]]; then
 fi
 
 . ./tools/modules/versions.sh
-if [[ -z "$1" || "$1" == "--git_only" ]]; then
+if [[ -z "$1" || "$1" == "--fetch_only" ]]; then
   echo "Using repository in $LLVM_REPO"
 
-  git_maybe_clone https://github.com/llvm-mirror/llvm "$LLVM_REPO"
-  git_maybe_clone https://github.com/llvm-mirror/clang "$LLVM_REPO/tools/clang"
+  wget_copy_archive "llvm" "$LLVM_REPO" "$MIN_LLVM_SHA"
+  wget_copy_archive "clang" "$LLVM_REPO/tools/clang" "$MIN_CLANG_SHA"
   rm -rf "$LLVM_REPO/tools/clang/tools/extra"
-
-  git_checkout_sha "$LLVM_REPO" "$MIN_LLVM_SHA"
-  git_checkout_sha "$LLVM_REPO/tools/clang" "$MIN_CLANG_SHA"
 fi
 
 if [[ -z "$1" || "$1" == "--build_only" ]]; then
