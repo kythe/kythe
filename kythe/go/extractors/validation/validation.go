3481d6abb0ab640b0499dd52c3b5705016716f35
Daniel Moy <danielmoy@google.com>
2018-12-18 16:05:32 -0800

    feat: add a simple kzip validator
    Opens a local kzip, and compares it to the contents in a local directory
    repo root.  Prints out how much is missing, and isolates the top missing
    directories and prints them as well.
    
    Usage
      kzip_validator -kzip <kzip-file> \
                     -local_repo <repo-root-path> \
                     -lang <comma-separated-langs> \
                     [-missing_file <file-to-save-missing-paths]
    
    Future versions of this should support grabbing the repo down from a
    remote url instead of loading it from local disk.  Future versions of
    this might also want to work off of a blacklist for sub-parts of repos
    the user might not care about.

---
+++ kythe/go/extractors/validation/validation.go
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2018 The Kythe Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package validation contains logic for verifing the contents of a kzip.
+package validation
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"kythe.io/kythe/go/platform/kzip"
+)
+
+// Harness encapsulates data for validation of a single kzip and repo.
+type Harness struct {
+	// Path to the kzip file to validate.
+	Kzip string
+	// Path to the root of the repo to compare the kzip against.
+	Repo string
+	// Language extensions to check, e.g. java, cpp, h.
+	Langs map[string]bool
+	// Optional file to output missing paths to.
+	MissingOutput *string
+}
+
+// Result is a simple container struct explaining the results of validation.
+type Result struct {
+	// Number of files in the kzip file matching one of Langs extensions.
+	FilesInKzip int
+	// Number of files in the repo matching one of Langs extensions.
+	FilesInRepo int
+	// Number of files in the repo that were not matched in the kzip.
+	Missing int
+	// A breakdown of the top missing files, by subdirectory.
+	TopMissing Coverage
+}
+
+// Coverage describes the missing subdirectories in a repo.
+type Coverage struct {
+	// The missing paths.
+	Paths []Dir
+}
+
+// The number of missing paths to print in output.
+const topMissingPathsLimit = 20
+const topMissingPathsDepth = 5
+
+// Dir describes a single missing subdirectory and how much is missing.
+type Dir struct {
+	// Path is a subdirectory from Repo root.
+	Path string
+	// Missing is the number of files in the Repo from the corresponding
+	// subdirectory that were missing from the Kzip.
+	Missing int
+}
+
+// Len is part of sort.Interface.
+func (c *Coverage) Len() int {
+	return len(c.Paths)
+}
+
+// Swap is part of sort.Interface.
+func (c *Coverage) Swap(i, j int) {
+	c.Paths[i], c.Paths[j] = c.Paths[j], c.Paths[i]
+}
+
+// Less is part of sort.Interface.
+func (c *Coverage) Less(i, j int) bool {
+	return c.Paths[i].Missing < c.Paths[j].Missing
+}
+
+// Validate opens a kzip file and compares all of its contained files with those
+// present in the provided repo path.  It returns results explaining what
+// percentage of files are covered in the kzip.
+func (h Harness) Validate() (*Result, error) {
+	log.Printf("Gathering validation data for %s", h.Kzip)
+	fromKzip, err := h.filenamesFromKzip(h.Kzip)
+	if err != nil {
+		return nil, fmt.Errorf("reading from kzip %s: %v", h.Kzip, err)
+	}
+	fromRepo, err := h.filenamesFromPath(h.Repo)
+	if err != nil {
+		return nil, fmt.Errorf("reading from repo %s: %v", h.Repo, err)
+	}
+	missingFromKzip := map[string]bool{}
+	for f := range fromRepo {
+		if _, ok := fromKzip[f]; !ok {
+			missingFromKzip[f] = true
+		}
+	}
+
+	// Try to calculate the most missing subdirectories.
+	topMissing := Coverage{}
+	// Only try to go so many paths deep (src/java/com/domain/sub).
+	// TODO(danielmoy): be more intelligent about this.
+	for i := 1; i <= topMissingPathsDepth; i++ {
+		topMissing = getTopMissing(missingFromKzip, topMissingPathsLimit, i)
+		if len(topMissing.Paths) >= topMissingPathsLimit {
+			break
+		}
+	}
+
+	if h.MissingOutput != nil {
+		err = printMissing(missingFromKzip, *h.MissingOutput)
+	}
+
+	return &Result{
+		FilesInKzip: len(fromKzip),
+		FilesInRepo: len(fromRepo),
+		Missing:     len(missingFromKzip),
+		TopMissing:  topMissing,
+	}, err
+}
+
+func (h Harness) filenamesFromKzip(path string) (map[string]bool, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("opening kzip %s: %v", path, err)
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("getting kzip size %s: %v", path, err)
+	}
+	r, err := kzip.NewReader(f, fi.Size())
+	if err != nil {
+		return nil, fmt.Errorf("opening kzip reader %s: %v", path, err)
+	}
+
+	ret := map[string]bool{}
+
+	if err := r.Scan(func(cu *kzip.Unit) error {
+		if cu.Proto != nil {
+			for _, f := range cu.Proto.SourceFile {
+				ret[f] = true
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("scanning kzip %s: %v", path, err)
+	}
+
+	return ret, nil
+}
+
+func (h Harness) filenamesFromPath(path string) (map[string]bool, error) {
+	ret := map[string]bool{}
+	err := filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		ext := filepath.Ext(p)
+		if len(ext) > 0 && ext[0] == '.' {
+			ext = ext[1:]
+		}
+		if _, ok := h.Langs[ext]; ok {
+			// We are only interested in the repo-relative path.
+			rp, err := filepath.Rel(path, p)
+			if err != nil {
+				return err
+			}
+			ret[rp] = true
+		}
+		return nil
+	})
+	return ret, err
+}
+
+// Returns the top N most common Mth-level subdirectories.
+func getTopMissing(missing map[string]bool, num, level int) Coverage {
+	counts := map[string]int{}
+	for f := range missing {
+		dirs := strings.Split(f, string(os.PathSeparator))
+		if len(dirs) > level-1 {
+			counts[strings.Join(dirs[:level-1], string(os.PathSeparator))]++
+		} else {
+			counts[f]++
+		}
+	}
+
+	top := Coverage{}
+	for f, c := range counts {
+		top.Paths = append(top.Paths, Dir{f, c})
+	}
+	sort.Sort(sort.Reverse(&top))
+	if len(top.Paths) >= num {
+		top.Paths = top.Paths[:num]
+	}
+	return top
+}
+
+func printMissing(filenames map[string]bool, output string) (err error) {
+	f, err := os.Create(output)
+	if err != nil {
+		return fmt.Errorf("opening missing file output %s: %v", output, err)
+	}
+	defer func() {
+		cerr := f.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	for name := range filenames {
+		f.WriteString(name + "\n")
+	}
+
+	return
+}
---
+++ kythe/go/platform/tools/kzip_validator/kzip_validator.go
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2018 The Kythe Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Binary kzip_validator checks the contents of a kzip against a code repo and
+// compares file coverage.
+//
+// Example:
+//  kzip_validator -kzip <kzip-file> -repoURL <repo-url> [-version <hash>]
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"kythe.io/kythe/go/extractors/validation"
+)
+
+var (
+	kzip      = flag.String("kzip", "", "The kzip file to check")
+	repoURL   = flag.String("repo_url", "", "The repo to compare against")
+	localRepo = flag.String("local_repo", "", "The path of an optional local repo to specify instead of -repoURL")
+	lang      = flag.String("lang", "", "The comma-separated language file extensions to check, e.g. 'java' or 'cpp,h'")
+
+	missingFile = flag.String("missing_file", "", "An optional file to write all missing filepaths to")
+
+	// TODO(danielmoy): support specific version validation
+	version = flag.String("version", "", "The version of the repo to compare")
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s -kzip <kzip-file> -repoURL <url> [-version <hash>]
+
+Compare a kzip file's contents with a given repo, and print results of file coverage.
+
+Options:
+`, filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+func verifyFlags() {
+	if len(flag.Args()) > 0 {
+		log.Fatalf("Unknown arguments: %v", flag.Args())
+	}
+
+	if *kzip == "" {
+		log.Fatal("You must provide a -kzip file")
+	}
+	if *repoURL == "" && *localRepo == "" {
+		log.Fatal("You must specify either -repoURL or a -localRepo path")
+	}
+	if *repoURL != "" && *localRepo != "" {
+		log.Fatal("You must specify only one of -repoURL or -localRepo")
+	}
+	if *lang == "" {
+		log.Fatal("You must specify what -lang file extensions to examine")
+	}
+}
+
+func main() {
+	flag.Parse()
+	verifyFlags()
+
+	fmt.Printf("Validating kzip %s\n", *kzip)
+
+	repoPath := getRepo()
+
+	h := validation.Harness{
+		Kzip:  *kzip,
+		Repo:  repoPath,
+		Langs: langs(),
+	}
+	if *missingFile != "" {
+		h.MissingOutput = missingFile
+	}
+	res, err := h.Validate()
+	if err != nil {
+		log.Fatalf("validating: %v", err)
+	}
+
+	log.Printf("Result: %v", res)
+
+	fmt.Println("Kzip verification:")
+	fmt.Printf("Kzip file count: %d\n", res.FilesInKzip)
+	fmt.Printf("Repo file count: %d\n", res.FilesInRepo)
+	fmt.Printf("Percent missing: %.3f\n", float64(res.Missing)/float64(res.FilesInRepo))
+	if len(res.TopMissing.Paths) > 0 {
+		fmt.Println("Top missing repos:")
+		for _, v := range res.TopMissing.Paths {
+			fmt.Printf("%10d %s\n", v.Missing, v.Path)
+		}
+	}
+}
+
+// getRepo either just early returns an available -localRepo, or fetches it from
+// the specified remote -repoURL
+func getRepo() string {
+	if *localRepo != "" {
+		log.Printf("Comparing against local copy of repo %s", *localRepo)
+		return *localRepo
+	}
+	log.Fatal("Unsupported use of -repoURL")
+	log.Printf("Comparing against remote repo %s", *repoURL)
+	return ""
+}
+
+func langs() map[string]bool {
+	ret := map[string]bool{}
+	exts := strings.Split(*lang, ",")
+	for _, v := range exts {
+		ret[v] = true
+	}
+	return ret
+}
