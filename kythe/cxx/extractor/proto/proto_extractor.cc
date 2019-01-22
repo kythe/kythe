e37fde3e520bc8984b5efe0426067c28f517182b
Justin Buchanan <justbuchanan@gmail.com>
2019-01-22 13:33:02 -0800

    refactor: Move proto extractor logic into shared library (#32)
    This code will be shared with the textproto extractor

--- kythe/cxx/extractor/proto/proto_extractor.cc
+++ kythe/cxx/extractor/proto/proto_extractor.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 The Kythe Authors. All rights reserved.
+ * Copyright 2019 The Kythe Authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,30 +14,20 @@
  * limitations under the License.
  */
 
-// Standalone extractor for protobuf. Given a proto file, builds a kzip
-// containing the proto file(s) and all dependencies.
-//
-// Usage:
-//   export KYTHE_OUTPUT_FILE=foo.kzip
-//   proto_extractor foo.proto
-//   proto_extractor foo.proto bar.proto
-//   proto_extractor foo.proto -- --proto_path dir/with/my/deps
+#include "proto_extractor.h"
 
 #include <string>
 
-#include "absl/strings/match.h"
 #include "absl/strings/str_cat.h"
-#include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "google/protobuf/compiler/importer.h"
 #include "google/protobuf/io/zero_copy_stream_impl.h"
 #include "kythe/cxx/common/file_vname_generator.h"
-#include "kythe/cxx/common/kzip_writer.h"
 #include "kythe/cxx/common/path_utils.h"
-#include "kythe/cxx/indexer/proto/search_path.h"
 #include "kythe/proto/analysis.pb.h"
 
 namespace kythe {
+namespace lang_proto {
 namespace {
 
 // Error "collector" that just writes messages to log output.
@@ -97,92 +87,34 @@ static std::string LoadFileOrDie(const std::string& file) {
   return content;
 }
 
-// If KYTHE_VNAMES environment variable is set, configures the vname generator
-// with the json config file at that path.
-void SetVNameConfigFromEnv(FileVNameGenerator* vname_gen) {
-  std::string path;
-  if (const char* path_cstr = getenv("KYTHE_VNAMES")) {
-    path = path_cstr;
+}  // namespace
+
+proto::CompilationUnit ProtoExtractor::ExtractProtos(
+    const std::vector<std::string>& proto_filenames,
+    IndexWriter* index_writer) const {
+  proto::CompilationUnit unit;
+
+  CHECK(GetCurrentDirectory(unit.mutable_working_directory()));
+
+  for (const std::string& proto : proto_filenames) {
+    unit.add_argument(proto);
   }
-  if (path.empty()) {
-    return;
-  }
-
-  std::string json = LoadFileOrDie(path);
-
-  std::string error_text;
-  CHECK(vname_gen->LoadJsonString(json, &error_text))
-      << "Could not parse vname generator configuration: " << error_text;
-}
-
-}  // anonymous namespace
-
-int main(int argc, char* argv[]) {
-  google::InitGoogleLogging(argv[0]);
-  gflags::SetUsageMessage(R"(Standalone extractor for the Kythe Proto indexer.
-Creates a kzip containing the main proto file(s) and any dependencies.
-
-Examples:
-  export KYTHE_OUTPUT_FILE=foo.kzip
-  proto_extractor foo.proto
-  proto_extractor foo.proto bar.proto
-  proto_extractor foo.proto -- --proto_path dir/with/my/deps")");
-  gflags::ParseCommandLineFlags(&argc, &argv, /*remove_flags=*/true);
-  std::vector<std::string> final_args(argv + 1, argv + argc);
-
-  std::string output_filename;
-  if (const char* env_output_file = getenv("KYTHE_OUTPUT_FILE")) {
-    output_filename = env_output_file;
-  }
-  CHECK(!output_filename.empty())
-      << "Please specify an output kzip file with the KYTHE_OUTPUT_FILE "
-         "environment variable.";
-
-  proto::IndexedCompilation compilation;
-  CHECK(GetCurrentDirectory(
-      compilation.mutable_unit()->mutable_working_directory()));
-
-  // File paths in the output kzip should be relative to this directory.
-  std::string kythe_root_directory = ".";
-  if (const char* env_root_directory = getenv("KYTHE_ROOT_DIRECTORY")) {
-    kythe_root_directory = env_root_directory;
-  }
-
-  // Parse --proto_path and -I args into a set of path substitions (search
-  // paths).
-  std::vector<std::pair<std::string, std::string>> path_substitutions;
-  std::vector<std::string> unused_args;
-  ::kythe::lang_proto::ParsePathSubstitutions(final_args, &path_substitutions,
-                                              &unused_args);
-
-  // Look through remaining args for .proto files.
-  std::vector<std::string> proto_filenames;
-  for (const std::string& arg : unused_args) {
-    CHECK(absl::EndsWith(arg, ".proto"))
-        << "Invalid arg, expected a proto file: '" << arg << "'";
-    proto_filenames.push_back(arg);
-    compilation.mutable_unit()->add_argument(arg);
-    compilation.mutable_unit()->add_source_file(
-        RelativizePath(arg, kythe_root_directory));
-  }
-  CHECK(proto_filenames.size() >= 1) << "Expected 1+ .proto files.";
 
   // Add path substitutions to src_tree and add as args to compilation unit.
   RecordingDiskSourceTree src_tree;
   src_tree.MapPath("", "");  // Add current directory to VFS.
   if (path_substitutions.size()) {
-    compilation.mutable_unit()->add_argument("--");
+    unit.add_argument("--");
     for (const auto& sub : path_substitutions) {
       // Add search path to proto SourceTree.
       src_tree.MapPath(sub.first, sub.second);
 
       // Record in compilation unit args so indexer gets the same paths.
-      compilation.mutable_unit()->add_argument("--proto_path");
+      unit.add_argument("--proto_path");
       if (sub.first == "") {
-        compilation.mutable_unit()->add_argument(sub.second);
+        unit.add_argument(sub.second);
       } else {
-        compilation.mutable_unit()->add_argument(
-            absl::StrCat(sub.first, "=", sub.second));
+        unit.add_argument(absl::StrCat(sub.first, "=", sub.second));
       }
     }
   }
@@ -198,18 +130,9 @@ Examples:
       google::protobuf::compiler::Importer importer(&src_tree, &err_collector);
       CHECK(importer.Import(fname) != nullptr)
           << "Failed to import file: " << fname;
+
+      unit.add_source_file(RelativizePath(fname, root_directory));
     }
-  }
-
-  StatusOr<IndexWriter> kzip_writer = KzipWriter::Create(output_filename);
-  CHECK(kzip_writer.ok()) << "Error opening output kzip: "
-                          << kzip_writer.status();
-
-  FileVNameGenerator file_vnames;
-  SetVNameConfigFromEnv(&file_vnames);
-  std::string corpus;
-  if (const char* env_corpus = getenv("KYTHE_CORPUS")) {
-    corpus = env_corpus;
   }
 
   // Write each toplevel proto and its transitive dependencies into the kzip.
@@ -233,32 +156,45 @@ Examples:
     CHECK(src_tree.VirtualFileToDiskFile(filename, &full_path))
         << "Error canonicalizing path: " << filename;
     // Make path relative to KYTHE_ROOT_DIRECTORY.
-    full_path = RelativizePath(full_path, kythe_root_directory);
+    full_path = RelativizePath(full_path, root_directory);
 
-    // Write file to kzip.
-    auto digest = kzip_writer->WriteFile(file_contents);
+    // Write file to index.
+    auto digest = index_writer->WriteFile(file_contents);
     CHECK(digest.ok()) << digest.status();
 
     // Record file info to compilation unit.
-    proto::CompilationUnit::FileInput* file_input =
-        compilation.mutable_unit()->add_required_input();
-    proto::VName vname = file_vnames.LookupVName(full_path);
+    proto::CompilationUnit::FileInput* file_input = unit.add_required_input();
+    proto::VName vname = vname_gen.LookupVName(full_path);
     if (vname.corpus().empty()) {
-      vname.set_corpus(corpus);
+      vname.set_corpus(std::string(corpus));
     }
     *file_input->mutable_v_name() = std::move(vname);
     file_input->mutable_info()->set_path(full_path);
     file_input->mutable_info()->set_digest(*digest);
   }
 
-  auto digest = kzip_writer->WriteUnit(compilation);
-  CHECK(digest.ok()) << "Error writing unit to kzip: " << digest.status();
-
-  CHECK(kzip_writer->Close().ok());
-
-  return 0;
+  return unit;
 }
 
-}  // namespace kythe
+void ProtoExtractor::ConfigureFromEnv() {
+  if (const char* env_corpus = getenv("KYTHE_CORPUS")) {
+    corpus = env_corpus;
+  }
 
-int main(int argc, char* argv[]) { return kythe::main(argc, argv); }
+  // File paths in the output kzip should be relative to this directory.
+  if (const char* env_root_directory = getenv("KYTHE_ROOT_DIRECTORY")) {
+    root_directory = env_root_directory;
+  }
+
+  // Configure VName generator.
+  const char* vname_path = getenv("KYTHE_VNAMES");
+  if (vname_path && strlen(vname_path) > 0) {
+    std::string json = LoadFileOrDie(vname_path);
+    std::string error_text;
+    CHECK(vname_gen.LoadJsonString(json, &error_text))
+        << "Could not parse vname generator configuration: " << error_text;
+  }
+}
+
+}  // namespace lang_proto
+}  // namespace kythe
