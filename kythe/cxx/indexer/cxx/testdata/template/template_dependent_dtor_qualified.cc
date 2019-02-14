60d20f9ecfaebf78dc2030bfd24eb322913d14b1
Shahms King <shahms@shahms.com>
2019-02-14 13:19:42 -0800

    refactor(cxx_indexer): DependentName nodes and edges (#3500)
    * refactor(cxx_indexer): pull out emit ranges from BNIDependentName
    
    * chore: more work refactoring dependent names
    
    * chore(cxx_indexer): refactor handling of dependent names
    
    * Update kythe/cxx/indexer/cxx/IndexerASTHooks.cc
    
    Co-Authored-By: shahms <shahms@shahms.com>

--- kythe/cxx/indexer/cxx/testdata/template/template_dependent_dtor_qualified.cc
+++ kythe/cxx/indexer/cxx/testdata/template/template_dependent_dtor_qualified.cc
@@ -13,6 +13,7 @@ class C {
     //- BaseInT param.0 SomeId
     //- SomeId.node/kind lookup
     //- SomeId.text "SomeId"
+    //- @"~" ref Dtor
     t->SomeId::Base::~Base();
     // TODO(zarko): Does it make sense for SomeId param.0 TyvarT?
     // What if instead of 'SomeId' we wrote 'T' above?
