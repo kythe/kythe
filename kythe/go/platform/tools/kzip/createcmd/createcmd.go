d0255f56a0d6336469aff463ff032d40f0d30176
Shahms King <shahms@shahms.com>
2019-02-22 09:11:49 -0800

    feat(kzip_create): allow embedding details messages in kzips (#3538)
    * feat(kzip_create): allow embedding details messages in kzips
    
    * chore: address comments
    
    * chore: make golint happy

--- kythe/go/platform/tools/kzip/createcmd/createcmd.go
+++ kythe/go/platform/tools/kzip/createcmd/createcmd.go
@@ -19,15 +19,19 @@ package createcmd
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"os"
+	"strings"
 
 	"kythe.io/kythe/go/platform/kzip"
 	"kythe.io/kythe/go/util/cmdutil"
 	"kythe.io/kythe/go/util/kytheuri"
 
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/google/subcommands"
 
+	anypb "github.com/golang/protobuf/ptypes/any"
 	apb "kythe.io/kythe/proto/analysis_go_proto"
 	spb "kythe.io/kythe/proto/storage_go_proto"
 )
@@ -35,9 +39,10 @@ import (
 type createCommand struct {
 	cmdutil.Info
 
-	uri    string
-	output string
-	source string
+	uri     string
+	output  string
+	source  string
+	details string
 }
 
 // New creates a new subcommand for merging kzip files.
@@ -60,6 +65,7 @@ func (c *createCommand) SetFlags(fs *flag.FlagSet) {
 	fs.StringVar(&c.uri, "uri", "", "A Kythe URI naming the compilation unit")
 	fs.StringVar(&c.output, "output", "", "Path for output kzip file")
 	fs.StringVar(&c.source, "source", "", "Path for input source file")
+	fs.StringVar(&c.details, "details", "", "JSON-encoded Any messages to embed as compilation details.")
 }
 
 // Execute implements the subcommands interface and creates the requested file.
@@ -77,7 +83,7 @@ func (c *createCommand) Execute(ctx context.Context, fs *flag.FlagSet, _ ...inte
 	// within the specified Kythe URI.
 	uri, err := kytheuri.Parse(c.uri)
 	if err != nil {
-		return c.Fail("error parsing -uri: ", err)
+		return c.Fail("error parsing -uri: %v", err)
 	}
 	cu := &apb.CompilationUnit{
 		VName: &spb.VName{
@@ -90,21 +96,27 @@ func (c *createCommand) Execute(ctx context.Context, fs *flag.FlagSet, _ ...inte
 		SourceFile: []string{c.source},
 	}
 
+	details, err := unmarshalDetails(c.details)
+	if err != nil {
+		return c.Fail("error parsing -details: %v", err)
+	}
+	cu.Details = details
+
 	out, err := openWriter(c.output)
 	if err != nil {
-		return c.Fail("error opening -output: ", err)
+		return c.Fail("error opening -output: %v", err)
 	}
 
 	for _, path := range append([]string{c.source}, fs.Args()...) {
 		err := addFile(out, cu, path)
 		if err != nil {
-			return c.Fail("error adding file to -output: ", err)
+			return c.Fail("error adding file to -output: %v", err)
 		}
 	}
 
 	_, err = out.AddUnit(cu, nil)
 	if err != nil {
-		return c.Fail("error writing compilation to -output: ", err)
+		return c.Fail("error writing compilation to -output: %v", err)
 	}
 
 	if err := out.Close(); err != nil {
@@ -144,4 +156,17 @@ func addFile(out *kzip.Writer, cu *apb.CompilationUnit, path string) error {
 		},
 	})
 	return nil
+}
+
+func unmarshalDetails(details string) ([]*anypb.Any, error) {
+	var msg []*anypb.Any
+	dec := json.NewDecoder(strings.NewReader(details))
+	for dec.More() {
+		var detail anypb.Any
+		if err := jsonpb.UnmarshalNext(dec, &detail); err != nil {
+			return msg, err
+		}
+		msg = append(msg, &detail)
+	}
+	return msg, nil
 }
