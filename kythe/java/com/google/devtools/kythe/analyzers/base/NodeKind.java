fb0eae028b60f899e422d48ff26a5c25e90be84f
Jay Sachs <jsachs@google.com>
2019-03-18 08:55:05 -0700

    better

--- kythe/java/com/google/devtools/kythe/analyzers/base/NodeKind.java
+++ kythe/java/com/google/devtools/kythe/analyzers/base/NodeKind.java
@@ -67,7 +67,7 @@ public enum NodeKind {
     return kind;
   }
 
-  /** Returns the node's subkind Kythe GraphStore value (or {@code Optional.empty()}). */
+  /** Returns the node's subkind Kythe GraphStore value. */
   public final Optional<String> getSubkind() {
     return Optional.ofNullable(subkind);
   }
