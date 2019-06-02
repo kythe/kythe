b649a127969f374bd250ab39d5814630711a3304
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-01 20:01:09 -0700

    Add more spec definitions and tests (failing)

--- kythe/typescript/testdata/declaration_spec.ts
+++ kythe/typescript/testdata/declaration_spec.ts
@@ -1,86 +1,103 @@
 // Tests the specification of VNames for symbol declarations
 
+// SourceFile
+//- FileModule=VName("module", _, _, "testdata/declaration_spec", "typescript").node/kind record
+//- FileModuleAnchor.node/kind anchor
+//- FileModuleAnchor./kythe/loc/start 0
+//- FileModuleAnchor./kythe/loc/end 1
+//- FileModuleAnchor defines/binding FileModule
+
 // NamespaceImport
-//- @NspI defines/binding VName("NspI", _, _, "testdata/spec", "typescript")
-import * as NspI from "./declaration";
+//- @NspI defines/binding VName("NspI", _, _, "testdata/declaration_spec", "typescript")
+import * as NspI from './declaration';
 
-// ImportSpecifier
-//- @I defines/binding VName("I", _, _, "testdata/spec", "typescript")
-import { decl as I } from "./declaration";
-
-// ExportSpecifier
-//- @exp define/binding VName("exp", _, _, "testdata/spec", "typescript")
-export { E as exp };
-
-// ModuleDeclaration
-//- @N define/binding VName("N", _, _, "testdata/spec", "typescript")
-namespace N {}
+// ExportAssignment
+//- @default defines/binding VName("default", _, _, "testdata/declaration_spec", "typescript")
+export default NspI;
 
 // ClassDeclaration
-//- @C defines/binding VName("C", _, _, "testdata/spec", "typescript")
+//- @C defines/binding VName("C", _, _, "testdata/declaration_spec", "typescript")
+//- @C defines/binding VName("C#type", _, _, "testdata/declaration_spec", "typescript")
 class C {
   // PropertyDeclaration
-  //- @method defines/binding VName("C.property", _, _, "testdata/spec", "typescript")
+  //- @property defines/binding VName("C.property", _, _, "testdata/declaration_spec", "typescript")
   property = 0;
 
   // MethodDeclaration
-  //- @method defines/binding VName("C.method", _, _, "testdata/spec", "typescript")
+  //- @method defines/binding VName("C.method", _, _, "testdata/declaration_spec", "typescript")
   method() {}
+
+  // Constructor
+  //- @constructor defines/binding VName("C.constructor", _, _, "testdata/declaration_spec", "typescript")
+  constructor() {}
+
+  // GetAccessor
+  //- @prop defines/binding VName("C.prop#getter", _, _, "testdata/declaration_spec", "typescript")
+  get prop() {
+    return this.property;
+  }
+
+  // SetAccessor
+  //- @prop defines/binding VName("C.prop#setter", _, _, "testdata/declaration_spec", "typescript")
+  set prop(nProp) {
+    this.property = nProp;
+  }
 }
 
 // EnumDeclaration
-//- @D defines/binding VName("E", _, _, "testdata/spec", "typescript")
+//- @E defines/binding VName("E", _, _, "testdata/declaration_spec", "typescript")
+//- @E defines/binding VName("E#type", _, _, "testdata/declaration_spec", "typescript")
 enum E {
   // EnumMember
-  //- @EnumMember defines/binding VName("E.EnumMember", _, _, "testdata/spec", "typescript")
+  //- @EnumMember defines/binding VName("E.EnumMember", _, _, "testdata/declaration_spec", "typescript")
   EnumMember = 0
 }
 
 // FunctionDeclaration
-//- @fun defines/binding VName("fun", _, _, "testdata/spec", "typescript")
+//- @#1"fun" defines/binding VName("fun", _, _, "testdata/declaration_spec", "typescript")
 function fun(
-  // Parameter
-  //- @param defines/binding VName("fun.param", _, _, "testdata/spec", "typescript")
-  param: number
-) {}
+    // Parameter
+    //- @param defines/binding VName("fun.param", _, _, "testdata/declaration_spec", "typescript")
+    param: number) {}
 
 // InterfaceDeclaration
-//- @B defines/binding VName("B", _, _, "testdata/spec", "typescript")
+//- @B defines/binding VName("B#type", _, _, "testdata/declaration_spec", "typescript")
 interface B {
   // PropertySignature
-  //- @pSig defines/binding VName("B.pSig", _, _, "testdata/spec", "typescript")
+  //- @pSig defines/binding VName("B.pSig", _, _, "testdata/declaration_spec", "typescript")
   pSig: number;
 
   // MethodSignature
-  //- @mSig defines/binding VName("B.mSig", _, _, "testdata/spec", "typescript")
+  //- @mSig defines/binding VName("B.mSig", _, _, "testdata/declaration_spec", "typescript")
   mSig(): void;
 }
 
 // VariableDeclaration
-//- @v defines/binding VName("v", _, _, "testdata/spec", "typescript")
+//- @v defines/binding VName("v", _, _, "testdata/declaration_spec", "typescript")
 let v = {
   // PropertyAssignment
-  //- @prop defines/binding VName("v.prop", _, _, "testdata/spec", "typescript")
+  // TODO: the signature here should be something like `block0.prop`, but
+  // anonymous block names are not well-defined by the spec yet.
+  //- @prop defines/binding VName(_, _, _, "testdata/declaration_spec", "typescript")
   prop: 0
 };
 
 // TypeAliasDeclaration
-//- @AliasArray defines/binding VName("AliasArray", _, _, "testdata/spec", "typescript")
+//- @AliasArray defines/binding VName("AliasArray#type", _, _, "testdata/declaration_spec", "typescript")
 type AliasArray<
-  // TypeParameter
-  //- @T defines/binding VName("AliasArray.T", _, _, "testdata/spec", "typescript")
-  T
-> = Array<T>;
+    // TypeParameter
+    //- @#0"T" defines/binding VName("AliasArray.T#type", _, _, "testdata/declaration_spec", "typescript")
+    T> = Array<T>;
 
-//- @decl defines/binding VName("aFun", _, _, "testdata/spec", "typescript")
-const aFun = () => {
+//- @arrowFun defines/binding VName("arrowFun", _, _, "testdata/declaration_spec", "typescript")
+const arrowFun = () => {
   // Arrow function scope name is not well-defined.
-  //- @decl defines/binding VName(_, _, _, "testdata/spec", "typescript")
+  //- @anonArrowFunDecl defines/binding VName(_, _, _, "testdata/declaration_spec", "typescript")
   let anonArrowFunDecl;
 };
 
 {
   // Anonymous block scope name is not well-defined.
-  //- @decl defines/binding VName(_, _, _, "testdata/spec", "typescript")
+  //- @anonBlockDecl defines/binding VName(_, _, _, "testdata/declaration_spec", "typescript")
   let anonBlockDecl;
 }
