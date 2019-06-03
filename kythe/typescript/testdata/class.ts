07810fb2eb070bf9f4389f314abab0ace2f9400d
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-02 22:40:19 -0700

    Ensure members declared in a ctor are scoped to the class. Add tests for this.

--- kythe/typescript/testdata/class.ts
+++ kythe/typescript/testdata/class.ts
@@ -19,9 +19,6 @@ interface IExtended extends IFace {}
 
 //- @Class defines/binding Class
 //- Class.node/kind record
-//- @Class defines/binding ClassCtor
-//- ClassCtor.node/kind function
-//- ClassCtor.subkind constructor
 //- @IFace ref IFace
 class Class implements IFace {
     //- @member defines/binding Member
@@ -33,11 +30,12 @@ class Class implements IFace {
     //- StaticMember.tag/static _
     static staticMember: number;
 
-    // TODO: ClassCtor should really point at this constructor, not at the
-    // top-level class declaration.
     // This ctor declares a new member var named 'otherMember', and also
     // declares an ordinary parameter named 'member' (to ensure we don't get
     // confused about params to the ctor vs true member variables).
+    //- @constructor defines/binding ClassCtor
+    //- ClassCtor.node/kind function
+    //- ClassCtor.subkind constructor
     //- @otherMember defines/binding OtherMember
     //- OtherMember.node/kind variable
     //- @member defines/binding FakeMember
@@ -59,11 +57,21 @@ class Class implements IFace {
     ifaceMethod(): void {}
 }
 
+// A class without a constructor binds the constructor to its declaration.
 //- @Class ref ClassCtor
+//- @Subclass defines/binding SubClassCtor
+//- SubClassCtor.node/kind function
+//- SubClassCtor.subkind constructor
 class Subclass extends Class {
     method() {
         //- @member ref Member
         this.member;
+
+        //- @Class ref ClassCtor
+        new Class(0, '');
+
+        //- @Class ref Class
+        type CC = Class;
     }
 }
 
