65e686ce63dd8bda4a1ab6824f06da5fe680461e
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-05 09:42:57 -0700

    Adds tests for plugin model

---
+++ kythe/typescript/testdata/plugin.ts
@@ -0,0 +1,5 @@
+// Tests that a plugin emits entries on top of the base indexer.
+
+// From the TypeScript indexer
+//- _Mod=vname("module", _, _, "testdata/plugin", "typescript").node/kind record
+//- _PluginMod=vname("plugin-module", _, _, "testdata/plugin", "plugin-language").node/kind record
