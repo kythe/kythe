3bcb68780557826c33ac34aa4473c29f14a5b886
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-19 13:57:47 -0700

    fix(typescript_indexer): point class type and values at the class name
    Revert "feat(typescript_indexer): VName schema compliance for constructors (#3785)"
    Also add a 'ref/call' edge to class constructors in a `new` expression.
    
    This reverts commit cdee8bf8a813ddd0309dbab00110f0e972282996.

--- kythe/typescript/testdata/class.ts
+++ kythe/typescript/testdata/class.ts
@@ -17,47 +17,45 @@ interface IFace {
 //- @IFace ref IFace
 interface IExtended extends IFace {}
 
-//- @Class defines/binding Class
+//- @Class defines/binding Class=vname("Class#type", _, _, _, _)
 //- Class.node/kind record
+//- @Class defines/binding ClassValue=vname("Class", _, _, _, _)
+//- ClassValue.node/kind function
 //- @IFace ref IFace
 class Class implements IFace {
     //- @member defines/binding Member
     //- Member.node/kind variable
-    //- Member childof Class
     //- !{ Member.tag/static _ }
     member: number;
 
     //- @staticMember defines/binding StaticMember
     //- StaticMember.tag/static _
-    //- StaticMember childof Class
     static staticMember: number;
 
     // This ctor declares a new member var named 'otherMember', and also
     // declares an ordinary parameter named 'member' (to ensure we don't get
     // confused about params to the ctor vs true member variables).
-    //- @constructor defines/binding ClassCtor
+    //- @constructor defines/binding ClassCtor=vname("Class.constructor", _, _, _, _)
     //- ClassCtor.node/kind function
     //- ClassCtor.subkind constructor
     //- @otherMember defines/binding OtherMember
     //- OtherMember.node/kind variable
-    //- OtherMember childof Class
-    //- !{ OtherMember childof ClassCtor }
     //- @member defines/binding FakeMember
     //- FakeMember.node/kind variable
-    //- FakeMember childof ClassCtor
-    //- !{ FakeMember childof Class }
-    constructor(public otherMember: number, member: string) {}
+    constructor(public otherMember: number, member: string) {
+      //- @Class ref ClassValue
+      //- @Class ref/call ClassCtor
+      new Class(0, 'a');
+    }
 
     //- @method defines/binding Method
     //- Method.node/kind function
     //- Method childof Class
-    //- @param defines/binding MethodParam
-    //- MethodParam childof Method
-    method(param: number) {
+    method() {
         //- @member ref Member
         this.member;
         //- @method ref Method
-        this.method(param);
+        this.method();
     }
 
     // TODO: ensure the method is linked to the interface too.
@@ -65,25 +63,15 @@ class Class implements IFace {
     ifaceMethod(): void {}
 }
 
-// A class without a constructor binds the constructor to its declaration.
-//- @Class ref ClassCtor
-//- @Subclass defines/binding SubClassCtor
-//- SubClassCtor.node/kind function
-//- SubClassCtor.subkind constructor
+//- @Class ref ClassValue
 class Subclass extends Class {
     method() {
         //- @member ref Member
         this.member;
-
-        //- @Class ref ClassCtor
-        new Class(0, '');
-
-        //- @Class ref Class
-        type CC = Class;
     }
 }
 
-//- @Class ref ClassCtor
+//- @Class ref ClassValue
 let instance = new Class(3, 'a');
 //- @otherMember ref OtherMember
 instance.otherMember;
