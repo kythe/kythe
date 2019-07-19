2139771cc23b12f93f8d439d349ca6dbc9d1e5ec
Shahms King <shahms@google.com>
2019-07-19 11:04:45 -0700

    feat(java_indexer): add basic tests for java-9-10-11 features (#3898)

---
+++ kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/DiamondOperations.java
@@ -0,0 +1,36 @@
+package pkg;
+
+final class DiamondOperations {
+  private static void testAnonymousInnerDiamond() {
+    //- @"Generic<String>" ref GenericStringType
+    //- GenericStringType.node/kind tapp
+    //- GenericStringType param.0 GenericType
+    Generic<String> iter =
+        //- @"Generic<>" ref DiamondType
+        //- DiamondType.node/kind tapp
+        //- DiamondType param.0 GenericType
+        new Generic<>() {
+          @Override
+          public String method() {
+            return "";
+          }
+        };
+  }
+
+  private static void testTrivialDiamondOperator() {
+    // TODO(#3745): Change this test so that GenericIntType and DiamondIntType are the same.
+    //- @"Generic<Integer>" ref GenericIntType
+    //- GenericIntType.node/kind tapp
+    //- GenericIntType param.0 GenericType
+    //- @"Generic<>" ref DiamondIntType
+    //- DiamondIntType.node/kind tapp
+    //- DiamondIntType param.0 GenericType
+    Generic<Integer> n = new Generic<>();
+  }
+
+  private static class Generic<T> {
+    public T method() {
+      return null;
+    }
+  }
+}
