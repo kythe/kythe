a11e9b0468f6c6030d1b788e35dbc7863fd3dc66
Justin Buchanan <justinbuchanan@google.com>
2019-07-29 11:54:39 -0700

    fix(bazel extractor): better handling of bazel exit code
    We use the --keep_going flag so that we can extract all buildable targets even if some targets fail.
    If bazel fails to build some packages, but otherwise succeeds in extracting, the exit code is 1. Other
    errors like "bazel server died unexpectedly" give other exit codes and will now cause the
    extractor script to fail.

--- kythe/extractors/bazel/extract.sh
+++ kythe/extractors/bazel/extract.sh
@@ -66,6 +66,13 @@ fi
 # care if some targets fail, but bazel will return a failure code if any
 # targets fail.
 /kythe/bazelisk --bazelrc=/kythe/bazelrc "$@" || true
+retval=$?
+if [ $retval -eq 1 ]; then
+    echo "Not all bazel targets built successfully, but continuing anyways."
+else if [ $retval -ne 0 ];
+    echo "Bazel build failed with exit code: $retval"
+    exit 1
+fi
 
 # Collect any extracted compilations.
 mkdir -p "$KYTHE_OUTPUT_DIRECTORY"
