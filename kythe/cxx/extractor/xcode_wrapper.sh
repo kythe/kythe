430b58293680c1df7ac20b16a1ca9cd19f16a8c7
Shahms King <shahms@google.com>
2019-08-26 08:39:48 -0700

    fix(shell): silence shellcheck warning on non-constant source

--- kythe/cxx/extractor/xcode_wrapper.sh
+++ kythe/cxx/extractor/xcode_wrapper.sh
@@ -25,8 +25,10 @@ if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/
     fi
 fi
 if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  # shellcheck disable=SC1090
   source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
 elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  # shellcheck disable=SC1090
   source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
             "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
 else
