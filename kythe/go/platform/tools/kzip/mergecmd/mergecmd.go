db2398723b0fb71171d13f2cf8d475426997f0a6
Cody Schroeder <schroederc@google.com>
2019-10-01 08:51:32 -0700

    fix(tooling): check error before result

--- kythe/go/platform/tools/kzip/mergecmd/mergecmd.go
+++ kythe/go/platform/tools/kzip/mergecmd/mergecmd.go
@@ -69,6 +69,9 @@ func (c *mergeCommand) Execute(ctx context.Context, fs *flag.FlagSet, _ ...inter
 		dir = "."
 	}
 	tmpOut, err := vfs.CreateTempFile(ctx, dir, file)
+	if err != nil {
+		return c.Fail("Error creating temp output: %v", err)
+	}
 	tmpName := tmpOut.Name()
 	defer func() {
 		if tmpOut != nil {
@@ -76,9 +79,6 @@ func (c *mergeCommand) Execute(ctx context.Context, fs *flag.FlagSet, _ ...inter
 			vfs.Remove(ctx, tmpName)
 		}
 	}()
-	if err != nil {
-		return c.Fail("Error creating temp output: %v", err)
-	}
 	archives := fs.Args()
 	if c.append {
 		orig, err := vfs.Open(ctx, c.output)
