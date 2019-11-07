c7a1b8392c139eb54afaccd15c86a98879e60323
Justin Buchanan <justinbuchanan@google.com>
2019-11-07 15:17:33 -0800

    comment

--- kythe/go/platform/kzip/info/info.go
+++ kythe/go/platform/kzip/info/info.go
@@ -53,6 +53,7 @@ func KzipInfo(f kzip.File, fileSize int64, scanOpts ...kzip.ScanOption) (*apb.Kz
 		for _, ri := range u.Proto.RequiredInput {
 			riCorpus := requiredInputCorpus(u, ri)
 			if riCorpus == "" {
+				// Trim spaces to work around the fact that log("%v", proto) is inconsistent about trailing spaces in google3 vs open-source go.
 				msg := strings.TrimSpace(fmt.Sprintf("unable to determine corpus for required_input %q in CU %v", ri.Info.Path, u.Proto.GetVName()))
 				kzipInfo.CriticalKzipErrors = append(kzipInfo.CriticalKzipErrors, msg)
 				return nil
