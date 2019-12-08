f922155dc984ab8daf6559d675e58d5b1b5a5f9c
Mikita Belahlazau <nbeloglazov@google.com>
2019-12-07 18:59:45 -0800

    fix(typescript_indexer): emit refs to TYPE nodes in export statements
    Some symbols like interfaces don't have VALUE nodes and only have TYPE
    nodes. This commit fixes refs for such symbols.
    
    Tested:
      unit test

---
+++ kythe/typescript/testdata/reexport/main.ts
@@ -0,0 +1,19 @@
+/** @fileoverview See description in reexport.ts */
+
+//- @Interface defines/binding Interface
+export interface Interface {}
+
+//- @Class defines/binding Class
+export class Class {}
+
+//- @someFunction defines/binding SomeFunction
+export function someFunction() {}
+
+//- @CONSTANT defines/binding Constant
+export const CONSTANT = 1;
+
+//- @Enum defines/binding Enum
+export enum Enum {}
+
+//- @Type defines/binding Type
+export type Type = string;
