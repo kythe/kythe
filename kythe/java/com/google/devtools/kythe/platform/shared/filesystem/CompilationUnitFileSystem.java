9c13d569aea80229520d393ccdcf19a00dd06899
Shahms King <shahms@google.com>
2019-12-12 13:24:36 -0800

    chore(java_common): run google-java-format

--- kythe/java/com/google/devtools/kythe/platform/shared/filesystem/CompilationUnitFileSystem.java
+++ kythe/java/com/google/devtools/kythe/platform/shared/filesystem/CompilationUnitFileSystem.java
@@ -37,7 +37,6 @@ import java.nio.file.attribute.UserPrincipalLookupService;
 import java.nio.file.spi.FileSystemProvider;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 /**
