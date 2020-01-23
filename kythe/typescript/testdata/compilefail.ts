c0d9436aba00a1904c617751888896a85ebf6ada
Evan Martin <evan.martin@gmail.com>
2020-01-23 23:04:45 +0000

    typescript: handle the case where symbol has no name
    In some cases we encounter a symbol that doesn't have an associated
    VName.  Rather than crashing, change the calling code to move on to
    subsequent code by adding a million return statements.
    
    The associated test adds more badly formed code that reproduces this
    problem.  The test will crash without the corresponding indexer change.

--- kythe/typescript/testdata/compilefail.ts
+++ kythe/typescript/testdata/compilefail.ts
@@ -1,6 +1,11 @@
 // This file exercises what happens when you have a compilation error.
 // It's also imported by compilefail_import.ts.
 
+// This is an error because 'no-such-module' doesn't exist.
+import * as bad from 'no-such-module';
+let badType: bad.Type;  // use a type
+let badValue = bad.value;  // use a value
+
 // This is an error because 'UndefinedSymbol' is never defined.
 export type Bad = UndefinedSymbol;
 
