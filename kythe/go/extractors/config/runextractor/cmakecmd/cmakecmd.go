ab26719181d52244a81f00c4fac918d87c4c9403
Sal Guarnieri <salguarnieri@google.com>
2020-01-24 13:05:41 -0800

    fixup

--- kythe/go/extractors/config/runextractor/cmakecmd/cmakecmd.go
+++ kythe/go/extractors/config/runextractor/cmakecmd/cmakecmd.go
@@ -107,9 +107,9 @@ func (c *cmakeCommand) Execute(ctx context.Context, fs *flag.FlagSet, args ...in
 		return c.Fail("Unable to create build directory: %v", err)
 	}
 
-	// The Kythe extractor is clang based, so tell cmake that we are Clang so the right
+	// The Kythe extractor is clang based, so tell cmake to use clang so the right
 	// commands are generated.
-	if err := runIn(exec.CommandContext(ctx, "cmake", "-DCMAKE_CXX_COMPILER_ID=Clang", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", sourceDir), buildDir); err != nil {
+	if err := runIn(exec.CommandContext(ctx, "cmake", "-DCMAKE_CXX_COMPILER=clang++", "-DCMAKE_C_COMPILER=clang", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", sourceDir), buildDir); err != nil {
 		return c.Fail("Error configuring cmake: %v", err)
 	}
 
