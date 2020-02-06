5d6f63eb0051c96699691bfe23a7ea0e654fd205
Shahms King <shahms@google.com>
2020-02-06 13:03:17 -0800

    fix(kzip_merge): add a test that globwalker behaves like filepath.Walk

---
+++ kythe/go/platform/vfs/vfs_test.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 The Kythe Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vfs
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"kythe.io/kythe/go/test/testutil"
+)
+
+const testDataDir = "testdata"
+
+func TestGlobWalker(t *testing.T) {
+	byglob, err := collectFiles(&globWalker{LocalFS{}}, testutil.TestFilePath(t, testDataDir))
+	testutil.FatalOnErrT(t, "unable to collect files from glob", err)
+	bylocal, err := collectFiles(LocalFS{}, testutil.TestFilePath(t, testDataDir))
+	testutil.FatalOnErrT(t, "unable to collect local files", err)
+	testutil.FatalOnErrT(t, "local and glob walks differ", testutil.DeepEqual(bylocal, byglob))
+}
+
+func collectFiles(walk Walker, root string) ([]string, error) {
+	var files []string
+	return files, walk.Walk(context.Background(), root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		files = append(files, path)
+		return nil
+	})
+}
