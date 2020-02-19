255331cb1e861f1f9741c7afce56988c04a28658
Justin Buchanan <justinbuchanan@google.com>
2020-02-19 15:13:09 -0800

    feat(go indexer): add option to only emit doc/uri facts for stdlib pkgs (#4383)

--- kythe/go/indexer/cmd/go_indexer/go_indexer.go
+++ kythe/go/indexer/cmd/go_indexer/go_indexer.go
@@ -40,14 +40,15 @@ import (
 )
 
 var (
-	doJSON         = flag.Bool("json", false, "Write output as JSON")
-	doLibNodes     = flag.Bool("libnodes", false, "Emit nodes for standard library packages")
-	doCodeFacts    = flag.Bool("code", false, "Emit code facts containing MarkedSource markup")
-	doAnchorScopes = flag.Bool("anchor_scopes", false, "Emit childof edges to an anchor's semantic scope")
-	metaSuffix     = flag.String("meta", "", "If set, treat files with this suffix as JSON linkage metadata")
-	docBase        = flag.String("docbase", "http://godoc.org", "If set, use as the base URL for godoc links")
-	verbose        = flag.Bool("verbose", false, "Emit verbose log information")
-	contOnErr      = flag.Bool("continue", false, "Log errors encountered during analysis but do not exit unsuccessfully")
+	doJSON                         = flag.Bool("json", false, "Write output as JSON")
+	doLibNodes                     = flag.Bool("libnodes", false, "Emit nodes for standard library packages")
+	doCodeFacts                    = flag.Bool("code", false, "Emit code facts containing MarkedSource markup")
+	doAnchorScopes                 = flag.Bool("anchor_scopes", false, "Emit childof edges to an anchor's semantic scope")
+	metaSuffix                     = flag.String("meta", "", "If set, treat files with this suffix as JSON linkage metadata")
+	docBase                        = flag.String("docbase", "http://godoc.org", "If set, use as the base URL for godoc links")
+	onlyEmitDocURIsForStandardLibs = flag.Bool("only_emit_doc_uris_for_standard_libs", false, "If true, the doc/uri fact is only emitted for go std library packages")
+	verbose                        = flag.Bool("verbose", false, "Emit verbose log information")
+	contOnErr                      = flag.Bool("continue", false, "Log errors encountered during analysis but do not exit unsuccessfully")
 
 	writeEntry func(context.Context, *spb.Entry) error
 	docURL     *url.URL
@@ -144,11 +145,12 @@ func indexGo(ctx context.Context, unit *apb.CompilationUnit, f indexer.Fetcher) 
 		log.Printf("Finished resolving compilation: %s", pi.String())
 	}
 	return pi.Emit(ctx, writeEntry, &indexer.EmitOptions{
-		EmitStandardLibs: *doLibNodes,
-		EmitMarkedSource: *doCodeFacts,
-		EmitAnchorScopes: *doAnchorScopes,
-		EmitLinkages:     *metaSuffix != "",
-		DocBase:          docURL,
+		EmitStandardLibs:               *doLibNodes,
+		EmitMarkedSource:               *doCodeFacts,
+		EmitAnchorScopes:               *doAnchorScopes,
+		EmitLinkages:                   *metaSuffix != "",
+		DocBase:                        docURL,
+		OnlyEmitDocURIsForStandardLibs: *onlyEmitDocURIsForStandardLibs,
 	})
 }
 
