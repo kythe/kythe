4140adfca08c894828d0eb91c6c8d3b74fa1886e
Wyatt Calandro <wcalandro@google.com>
2020-06-02 23:37:54 +0000

    feat(rust): Remove deprecated Rust indexer
    This PR removes the old Rust indexer to make way for the new one.

--- kythe/rust/indexer/testdata/basic/file_content.rs
+++
@@ -1,7 +0,0 @@
-// Checks that the indexer emits file nodes with content.
-mod a;
-//- vname("", "file_content", "", "file_content.rs",
-//-   "").node/kind file
-//- AFile = vname("", "file_content", "", "a.rs", "")
-//-   .node/kind file
-//- AFile.text "fn main(){}\n"
