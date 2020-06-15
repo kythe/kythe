becfc18bf98a4ee0ed8d55ce019645ac262ffdd0
Justin Buchanan <justinbuchanan@google.com>
2020-06-15 15:13:40 -0700

    feat(extraction): generate build metadata kzip in bazel-extractor docker image

--- kythe/extractors/bazel/extract.sh
+++ kythe/extractors/bazel/extract.sh
@@ -86,5 +86,14 @@ fi
 mkdir -p "$KYTHE_OUTPUT_DIRECTORY"
 find bazel-out/*/extra_actions/external/kythe_release -name '*.kzip' | \
   xargs -r /kythe/tools/kzip merge --append --encoding "$KYTHE_KZIP_ENCODING" --output "$KYTHE_OUTPUT_DIRECTORY/compilations.kzip"
+
+# Record the timestamp of the git commit in a metadata kzip.
+METADATA_KZIP="buildmetadata.kzip"
+/kythe/tools/kzip create_metadata \
+  --output "$METADATA_KZIP" \
+  --corpus "$KYTHE_CORPUS" \
+  --commit_timestamp "$(git log --pretty='%ad' -n 1 HEAD)"
+/kythe/tools/kzip merge --append --encoding "$KYTHE_KZIP_ENCODING" --output "$KYTHE_OUTPUT_DIRECTORY/compilations.kzip" "$METADATA_KZIP"
+
 /kythe/fix_permissions.sh "$KYTHE_OUTPUT_DIRECTORY"
 test -f "$KYTHE_OUTPUT_DIRECTORY/compilations.kzip"
