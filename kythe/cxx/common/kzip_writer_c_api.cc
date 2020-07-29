beee4f6f438ca8018f94bd48179401dd57f0b887
Filip Filmar <fmil@google.com>
2020-07-29 10:01:44 -0700

    KZIP Writer C API (and Rust FFI) (#4608)
    * feat(rust): Adds a C API for the kzip writer
    
    This will allow me to reuse the C++ kzip writing code for
    the rust indexer instead of redoing that work in rust.
    
    Issue #4606
    
    * Renames the 'status' library
    
    Somewhat surprisingly -- if there are two libraries in the global namespace
    that have the same name, bazel does not disambiguate them, and an attempt
    to link them will fail.  Not sure why, but that's what happens.
    
    * fixup: handled review comments
    
    Simplified and cleared up issues raised during
    code review.
    
    * Works around the -fPIC issue for rust
    
    Rust binaries are compiled with -fPIC by default, and this fights with
    bazel's default of -fno-pic when using `-c opt` mode.  This has caused
    the compilation to fail.
    
    After some running around with hair on fire, I found
    https://github.com/bazelbuild/rules_rust/issues/118, which seems to
    work around the issue.
    
    I think in the long run, rules_rust should handle this automagically for
    the users.
    
    * Document no NUL byte at the end of results

---
+++ kythe/cxx/common/kzip_writer_c_api.cc
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2018 The Kythe Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kythe/cxx/common/kzip_writer_c_api.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "kythe/cxx/common/index_writer.h"
+#include "kythe/cxx/common/kzip_writer.h"
+#include "kythe/cxx/common/status_or.h"
+#include "kythe/proto/analysis.pb.h"
+
+using kythe::IndexWriter;
+using kythe::KzipEncoding;
+using kythe::StatusOr;
+
+namespace {
+
+KzipEncoding ToKzipEncoding(int32_t encoding) {
+  KzipEncoding kzip_encoding = KzipEncoding::kJson;
+  switch (encoding) {
+    case KZIP_WRITER_ENCODING_JSON:
+      kzip_encoding = KzipEncoding::kJson;
+      break;
+    case KZIP_WRITER_ENCODING_PROTO:
+      kzip_encoding = KzipEncoding::kProto;
+      break;
+    default:
+      // fallthrough
+    case KZIP_WRITER_ENCODING_ALL:
+      kzip_encoding = kythe::KzipEncoding::kAll;
+      break;
+  }
+  return kzip_encoding;
+}
+
+int32_t CodeFromStatus(const absl::Status& s) {
+  const absl::StatusCode code = s.code();
+  LOG(ERROR) << "Status: " << s;
+  return static_cast<int32_t>(code);
+}
+
+}  // namespace
+
+KzipWriter* KzipWriter_Create(const char* path, const size_t path_len,
+                              int32_t encoding, int32_t* create_status) {
+  CHECK(path != nullptr);
+  *create_status = 0;
+  KzipEncoding kzip_encoding = ToKzipEncoding(encoding);
+  const absl::string_view path_view(path, path_len);
+
+  StatusOr<IndexWriter> writer =
+      kythe::KzipWriter::Create(path_view, kzip_encoding);
+  if (!writer.ok()) {
+    *create_status = CodeFromStatus(writer.status());
+    return nullptr;
+  }
+  return reinterpret_cast<KzipWriter*>(
+      new kythe::IndexWriter(*std::move(writer)));
+}
+
+void KzipWriter_Delete(KzipWriter* writer) {
+  // We assume that `writer` was created by `KzipWriter_Create`.
+  delete reinterpret_cast<IndexWriter*>(writer);
+}
+
+int32_t KzipWriter_Close(KzipWriter* writer) {
+  const absl::Status status = reinterpret_cast<IndexWriter*>(writer)->Close();
+  return static_cast<int32_t>(status.code());
+}
+
+int32_t KzipWriter_WriteFile(KzipWriter* writer, const char* content,
+                             const size_t content_length, char* digest_buffer,
+                             size_t buffer_length,
+                             size_t* resulting_digest_size) {
+  // We assume that `writer` was created by `KzipWriter_Create`.
+  IndexWriter* w = reinterpret_cast<IndexWriter*>(writer);
+  auto digest_or = w->WriteFile({content, content_length});
+  if (!digest_or.ok()) {
+    return CodeFromStatus(digest_or.status());
+  }
+  const std::string& digest = *digest_or;
+  if (digest.length() > buffer_length) {
+    return KZIP_WRITER_BUFFER_TOO_SMALL_ERROR;
+  }
+  memcpy(digest_buffer, digest.c_str(), digest.length());
+  *resulting_digest_size = digest.length();
+  return 0;
+}
+
+int32_t KzipWriter_WriteUnit(KzipWriter* writer, const char* proto,
+                             size_t proto_length, char* digest_buffer,
+                             size_t buffer_length,
+                             size_t* resulting_digest_size) {
+  // We assume that `writer` was created by `KzipWriter_Create`.
+  IndexWriter* w = reinterpret_cast<IndexWriter*>(writer);
+  kythe::proto::IndexedCompilation unit;
+  const bool success = unit.ParseFromArray(proto, proto_length);
+  if (!success) {
+    return KZIP_WRITER_PROTO_PARSING_ERROR;
+  }
+  StatusOr<std::string> digest_or = w->WriteUnit(unit);
+  if (!digest_or.ok()) {
+    return CodeFromStatus(digest_or.status());
+  }
+  const auto& digest = *digest_or;
+  if (digest.length() > buffer_length) {
+    return KZIP_WRITER_BUFFER_TOO_SMALL_ERROR;
+  }
+  memcpy(digest_buffer, digest.c_str(), digest.length());
+  *resulting_digest_size = digest.length();
+  return 0;
+}
---
+++ kythe/cxx/common/kzip_writer_c_api_test.cc
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2018 The Kythe Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kythe/cxx/common/kzip_writer_c_api.h"
+
+#include <zip.h>
+
+#include <cstdlib>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_replace.h"
+#include "absl/strings/string_view.h"
+#include "absl/strings/strip.h"
+#include "glog/logging.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "kythe/cxx/common/kzip_reader.h"
+#include "kythe/cxx/common/libzip/error.h"
+#include "kythe/proto/go.pb.h"
+
+namespace kythe {
+namespace {
+using ::testing::ElementsAre;
+using ::testing::Values;
+
+absl::string_view TestTmpdir() {
+  return absl::StripSuffix(std::getenv("TEST_TMPDIR"), "/");
+}
+
+std::string TestOutputFile(absl::string_view basename) {
+  const auto* test_info = testing::UnitTest::GetInstance()->current_test_info();
+  const auto filename =
+      absl::StrReplaceAll(absl::StrCat(test_info->test_case_name(), "_",
+                                       test_info->name(), "_", basename),
+                          {{"/", "-"}});
+  return absl::StrCat(TestTmpdir(), "/", filename);
+}
+
+TEST(KzipWriterCApiTest, CApiTest) {
+  std::string dummy_file = TestOutputFile("dummy.kzip");
+  int32_t status;
+  std::unique_ptr<::KzipWriter, std::function<void(::KzipWriter*)>> w(
+      KzipWriter_Create(dummy_file.c_str(), dummy_file.length(),
+                        KZIP_WRITER_ENCODING_PROTO, &status),
+      [](::KzipWriter* w) {
+        if (w != nullptr) {
+          KzipWriter_Delete(w);
+        }
+      });
+  ASSERT_EQ(0, status);
+  {
+    std::string contents("contents");
+    char digest[200];
+    size_t length;
+    int32_t status =
+        KzipWriter_WriteFile(w.get(), contents.c_str(), contents.length(),
+                             digest, sizeof(digest), &length);
+    ASSERT_EQ(0, status);
+  }
+  {
+    std::string proto_bytes;
+    kythe::proto::IndexedCompilation unit;
+    ASSERT_TRUE(unit.SerializeToString(&proto_bytes));
+    char digest[800];
+    size_t length;
+    int32_t status =
+        KzipWriter_WriteUnit(w.get(), proto_bytes.c_str(), proto_bytes.length(),
+                             digest, sizeof(digest), &length);
+    ASSERT_EQ(0, status);
+  }
+  {
+    int32_t status = KzipWriter_Close(w.get());
+    ASSERT_EQ(0, status);
+  }
+
+  std::vector<std::string> contents;
+  {
+    auto* archive = zip_open(dummy_file.c_str(), ZIP_RDONLY, nullptr);
+    ASSERT_NE(archive, nullptr);
+    struct Closer {
+      ~Closer() { zip_discard(a); }
+      zip_t* a;
+    } closer{archive};
+    for (int i = 0; i < zip_get_num_entries(archive, 0); ++i) {
+      const char* name = zip_get_name(archive, i, 0);
+      ASSERT_NE(archive, nullptr) << libzip::ToStatus(zip_get_error(archive));
+      contents.push_back(name);
+    }
+  }
+  EXPECT_THAT(
+      contents,
+      // Order matters here as "root/" must come first.
+      // We don't really care about the rest of the entries, but it's easy
+      // enough to fix the order of the subdirectories and minimally harmful.
+      ElementsAre(
+          "root/", "root/files/", "root/pbunits/",
+          "root/files/"
+          "d1b2a59fbea7e20077af9f91b27e95e865061b270be03ff539ab3b73587882e8",
+          "root/pbunits/"
+          "44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a"));
+}
+
+}  // namespace
+}  // namespace kythe
