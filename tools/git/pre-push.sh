5876788cde25f94447884e2c4c3eaf8ec20096af
Shahms King <shahms@google.com>
2020-07-30 12:05:35 -0700

    chore: never attmept to apply patches in pre-push

--- tools/git/pre-push.sh
+++ tools/git/pre-push.sh
@@ -49,8 +49,6 @@ while read -r local_ref local_sha remote_ref remote_sha; do
     git checkout -q "$branch"
   fi
   # TODO(#3173): remove need for arcanist
-  if [[ "$KYTHE_SKIP_ARC_LINT" -ne 0 ]]; then
-    arc lint --rev "$remote/master"
-  fi
+  arc lint --never-apply-patches --rev "$remote/master" < /dev/null
   arc unit --rev "$remote/master"
 done
