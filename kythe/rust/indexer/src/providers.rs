58698e7047c6e8a21415cffe068c99f909676901
Wyatt Calandro <wcalandro@google.com>
2020-07-31 18:18:01 +0000

    Always remove forward-slash from root folder name
    There is an inconsistency where sometimes the root folder has an ending forward-slash and sometimes it doesn't. This necessitates removing forward-slashes from the root name and adding them to the format string when we generate a file path.

--- kythe/rust/indexer/src/providers.rs
+++ kythe/rust/indexer/src/providers.rs
@@ -80,7 +80,7 @@ impl KzipFileProvider {
             }
             // Safe to unwrap because kzip read would have failed if the internal paths
             // weren't UTF-8
-            path.to_str().unwrap().to_owned()
+            path.to_str().unwrap().to_owned().replace("/", "")
         };
 
         Ok(Self { zip_archive, root_name })
