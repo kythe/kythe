784e8ee7da2c5fe8eb8c246393f9d700261fd344
Shahms King <shahms@google.com>
2020-08-17 11:50:36 -0700

    fix(cxx): normalize release config, include assert stacktraces (#4655)
    * fix(cxx): normalize release config, include assert stacktraces
    
    * chore: conditionalize the assert override
    
    * chore: use local_defines
    
    * chore: add comment including Abseil bug

--- kythe/cxx/common/init.cc
+++ kythe/cxx/common/init.cc
@@ -15,11 +15,38 @@
  */
 #include "kythe/cxx/common/init.h"
 
+#include <cassert>
+#include <cstring>
+
 #include "absl/debugging/failure_signal_handler.h"
 #include "absl/debugging/symbolize.h"
 #include "glog/logging.h"
 
 namespace kythe {
+
+#if defined(KYTHE_OVERRIDE_ASSERT_FAIL)
+extern "C" {
+// Make sure we get a legible stack trace when assert from <assert.h> fails.
+// This works around https://github.com/abseil/abseil-cpp/issues/769.
+//
+// We are replacing implementation-defined function (defined in glibc)
+// used by the expansion of system defined assert() macro.
+void __assert_fail(const char* assertion, const char* file, unsigned int line,
+                   const char* function) __THROW {
+  LOG(FATAL) << "assert.h assertion failed at " << file << ":" << line
+             << (function ? " in " : "") << (function ? function : "") << ": "
+             << assertion;
+}
+
+// Same as above, but for assert_perror.
+void __assert_perror_fail(int errnum, const char* file, unsigned int line,
+                          const char* function) __THROW {
+  LOG(FATAL) << "assert.h assert_perror failed at " << file << ":" << line
+             << " " << (function ? function : "") << ": "
+             << std::strerror(errnum) << " [" << errnum << "]";
+}
+}  // extern "C"
+#endif
 
 void InitializeProgram(const char* argv0) {
   google::InitGoogleLogging(argv0);
