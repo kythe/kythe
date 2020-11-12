0d455662e33ef14e3f63b6bfd286ae1ef0bef84d
Shahms King <shahms@google.com>
2020-11-11 16:48:38 -0800

    feat(java_indexer): tag abstract methods/classes as such (#4746)

---
+++ kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/AbstractClass.java
@@ -0,0 +1,9 @@
+package pkg;
+
+//- @AbstractClass defines/binding ClassDef
+//- ClassDef.tag/abstract _
+abstract class AbstractClass {
+  //- @method defines/binding MethodDef
+  //- MethodDef.tag/abstract _
+  public abstract void method();
+}
