aea21669ddeba949840c9a4a5cba738611c4fecf
Jay Sachs <jsachs@google.com>
2021-01-20 19:00:14 -0800

    fix: disable TS function deprecation tagging

--- kythe/typescript/testdata/jsdoc.ts
+++ kythe/typescript/testdata/jsdoc.ts
@@ -1,13 +1,13 @@
 export {}
 
-//- @:8"singleLine" defines/binding VarSingleLine
+//- @+5"singleLine" defines/binding VarSingleLine
 //- VarSingleLineDoc documents VarSingleLine
 //- VarSingleLineDoc.node/kind doc
 //- VarSingleLineDoc.text "Single-line doc."
 /** Single-line doc. */
 var singleLine;
 
-//- @:19"multiLine" defines/binding VarMultiLine
+//- @+9"multiLine" defines/binding VarMultiLine
 //- VarMultiLineDoc documents VarMultiLine
 //- VarMultiLineDoc.node/kind doc
 //- VarMultiLineDoc.text "Multi-line doc.\n  spaced in.\nEOT"
@@ -18,27 +18,27 @@ var singleLine;
  */
 const multiLine = 3;
 
-//- @:26"Class" defines/binding Class
+//- @+5"Class" defines/binding Class
 //- ClassDoc documents Class
 //- ClassDoc.node/kind doc
 //- ClassDoc.text "Class doc."
 /** Class doc. */
 class Class {
-//- @:32"property" defines/binding Property
+//- @+5"property" defines/binding Property
 //- PropertyDoc documents Property
 //- PropertyDoc.node/kind doc
 //- PropertyDoc.text "Property doc."
   /** Property doc. */
   private property: string;
 
-//- @:39"method" defines/binding Method
+//- @+5"method" defines/binding Method
 //- MethodDoc documents Method
 //- MethodDoc.node/kind doc
 //- MethodDoc.text "Method doc."
   /** Method doc. */
   method() {}
 
-//- @:49"method2" defines/binding Method2
+//- @+8"method2" defines/binding Method2
 //- Method2Doc documents Method2
 //- Method2Doc.node/kind doc
 //- Method2Doc.text "Multi line doc.\n  Indented."
@@ -49,7 +49,7 @@ class Class {
   method2() {}
 }
 
-//- @:57"myFunc" defines/binding Function
+//- @+5"myFunc" defines/binding Function
 //- FunctionDoc documents Function
 //- FunctionDoc.node/kind doc
 //- FunctionDoc.text "Function doc."
@@ -57,25 +57,30 @@ class Class {
 function myFunc() {}
 
 interface String {
-//- @:66"fontsize" defines/binding FontSize
+//- @+6"fontsize" defines/binding FontSize
 //- FontSize.tag/deprecated "The <font> element has been removed in HTML5.\nPrefer using CSS properties."
   /**
    * @deprecated The <font> element has been removed in HTML5.
    *             Prefer using CSS properties.
    */
   fontsize: (size: number) => string;
-//- @:70"big" defines/binding Big
+//- @+3"big" defines/binding Big
 //- Big.tag/deprecated ""
   /** @deprecated */
   big: () => string;
 }
 
-//- @:76"PromiseAlias" defines/binding PromiseAlias
+//- @+3"PromiseAlias" defines/binding PromiseAlias
 //- PromiseAlias.tag/deprecated "dont use me"
 /** @deprecated dont use me */
 type PromiseAlias<T> = Promise<T>;
 
-//- @:81"Foo" defines/binding IFoo
+//- @+3"Foo" defines/binding IFoo
 //- IFoo.tag/deprecated "use Bar"
 /** @deprecated use Bar */
 interface Foo {}
+
+//- @+3"depFunc" defines/binding DepFunc
+//- !{ DepFunc.tag/deprecated "is deprecated" }
+/**  @deprecated is deprecated */
+function depFunc() {}
