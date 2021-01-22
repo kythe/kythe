215857ae23988955d24ca1df354242583c7ae3c0
Shahms King <shahms@google.com>
2021-01-22 11:13:34 -0800

    chore: update the rebuild script

--- tools/platforms/configs/rebuild.sh
+++ tools/platforms/configs/rebuild.sh
@@ -5,6 +5,20 @@
 cp "$(dirname "$0")/empty-versions.bzl" "$(dirname "$0")/versions.bzl"
 RBE_AUTOCONF_ROOT="$(bazel info workspace)"
 export RBE_AUTOCONF_ROOT
-for config in min max; do
-  bazel --bazelrc=/dev/null build "@rbe_bazel_${config}version//..."
+
+VERSIONS=(
+  "$(cat "$RBE_AUTOCONF_ROOT/.bazelversion")"
+  "$(cat "$RBE_AUTOCONF_ROOT/.bazelminversion")"
+)
+
+TARGETS=(
+  "@rbe_default//..."
+  "@rbe_bazel_minversion//..."
+  "@rbe_bazel_maxversion//..."
+)
+
+for version in "${VERSIONS[@]}"; do
+  for target in "${TARGETS[@]}"; do
+    USE_BAZEL_VERSION="$version" bazel --bazelrc=/dev/null query "$target"
+  done
 done
