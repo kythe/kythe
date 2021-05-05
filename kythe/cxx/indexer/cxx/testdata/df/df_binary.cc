7049dad435ad9d6bcfed302c575afec0797e3bd5
Luke Zarko <zarko@google.com>
2021-05-05 15:42:55 -0700

    fix(cxx_indexer): address comments

--- kythe/cxx/indexer/cxx/testdata/df/df_binary.cc
+++ kythe/cxx/indexer/cxx/testdata/df/df_binary.cc
@@ -1,18 +1,26 @@
 // Accumulator-style operators are in the influence graph.
 
-void f(int x, int y, int z) {
-    //- @x ref VarX
-    //- @x ref/writes VarX
-    //- @y ref VarY
-    //- !{ @y ref/writes VarY }
-    x += y;
-    //- @z ref VarZ
-    //- @z ref/writes VarZ
-    //- @x ref VarX
-    //- !{ @x ref/writes VarX }
-    z -= x;
-    //- VarX influences VarX
-    //- VarZ influences VarZ
-    //- VarY influences VarX
-    //- VarX influences VarZ
+void f(int x, int y, int z, int j, int k) {
+  //- @x ref VarX
+  //- @x ref/writes VarX
+  //- @y ref VarY
+  //- !{ @y ref/writes VarY }
+  x += y;
+  //- @z ref VarZ
+  //- @z ref/writes VarZ
+  //- @x ref VarX
+  //- !{ @x ref/writes VarX }
+  z -= x;
+  //- @j ref VarJ
+  //- @j ref/writes VarJ
+  //- @k ref VarK
+  //- @k ref/writes VarK
+  j -= k++;
+  //- VarX influences VarX
+  //- VarZ influences VarZ
+  //- VarY influences VarX
+  //- VarX influences VarZ
+  //- VarK influences VarK
+  //- VarJ influences VarJ
+  //- VarK influences VarJ
 }
