bf8a05cad593bf52ed90816795e4b13442d3d52e
Disha Jain <dishajain@google.com>
2021-09-14 15:30:55 +0000

    Update filepaths in inline metadata test goals

--- kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java
+++ kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java
@@ -14,7 +14,7 @@ package pkg;
 
 @javax.annotation.Generated(
   value = "//experiments/mendel/extensions/flag_codegen:codegen",
-  comments = "kythe-inline-metadata:Mendel Codegen"
+  comments = "kythe-inline-metadata:Inline Codegen"
 )
 
 public final class InlineMetadata {
@@ -59,7 +59,7 @@ public final class InlineMetadata {
 //- EA.loc/start 416
 //- EA.loc/end 423
 //- EA defines/binding InlineMetadata
-//- vname("IDENTIFIER:Generic", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates vname("9685fe12644670a48786e99ea2284102d5bebdb91672385b4efa2226d1777ad7", kythe, "", "third_party/kythe/kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java", "java")
+//- vname("IDENTIFIER:Generic", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates vname("9685fe12644670a48786e99ea2284102d5bebdb91672385b4efa2226d1777ad7", kythe, "", "kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java", "java")
 
 //- _Alt=vname("IDENTIFIER:Generic", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates InlineMetadata
 //- InlineMetadata.node/kind record
@@ -68,9 +68,9 @@ public final class InlineMetadata {
 //- EB.loc/start 940
 //- EB.loc/end 975
 //- EB defines/binding FALL_ASLEEP_WHILE_YOURE_ASHORE_FLAG
-//- vname("IDENTIFIER:Generic.fall_asleep_while_youre_ashore", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates vname("a0d784f4607cc6c9b38185145eb466fefcd7378ed6b54cb0041b1bec23bd20f3", kythe, "", "third_party/kythe/kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java", "java")
+//- vname("IDENTIFIER:Generic.fall_asleep_while_youre_ashore", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates vname("a0d784f4607cc6c9b38185145eb466fefcd7378ed6b54cb0041b1bec23bd20f3", kythe, "", "kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/InlineMetadata.java", "java")
 //- _Alt2=vname("IDENTIFIER:Generic.fall_asleep_while_youre_ashore", google3, "", "googledata/experiments/fakefe/features/generic_feature.gcl", gcl) generates FALL_ASLEEP_WHILE_YOURE_ASHORE_FLAG
 //- FALL_ASLEEP_WHILE_YOURE_ASHORE_FLAG.node/kind variable
 
 
- // Mendel Codegen:CAESfwgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMaXgoSSURFTlRJRklFUjpHZW5lcmljEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgoAMopwMSmQEIAhIVL2t5dGhlL2VkZ2UvZ2VuZXJhdGVzGngKLElERU5USUZJRVI6R2VuZXJpYy5hbm90aGVyX2JyaWNrX2luX3RoZV93YWxsEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgoQUovwUSngEIAhIVL2t5dGhlL2VkZ2UvZ2VuZXJhdGVzGn0KMUlERU5USUZJRVI6R2VuZXJpYy5mYWxsX2FzbGVlcF93aGlsZV95b3VyZV9hc2hvcmUSB2dvb2dsZTMiOmdvb2dsZWRhdGEvZXhwZXJpbWVudHMvZmFrZWZlL2ZlYXR1cmVzL2dlbmVyaWNfZmVhdHVyZS5nY2wqA2djbCCsByjPBxKYAQgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMadworSURFTlRJRklFUjpHZW5lcmljLm1lbl93aXRoX2FsbF90aGVfYW5zd2VycxIHZ29vZ2xlMyI6Z29vZ2xlZGF0YS9leHBlcmltZW50cy9mYWtlZmUvZmVhdHVyZXMvZ2VuZXJpY19mZWF0dXJlLmdjbCoDZ2NsILQJKNEJEosBCAISFS9reXRoZS9lZGdlL2dlbmVyYXRlcxpqCh5JREVOVElGSUVSOkdlbmVyaWMuc29tZV9idXR0b24SB2dvb2dsZTMiOmdvb2dsZWRhdGEvZXhwZXJpbWVudHMvZmFrZWZlL2ZlYXR1cmVzL2dlbmVyaWNfZmVhdHVyZS5nY2wqA2djbCCuCyi+CxKUAQgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMacwonSURFTlRJRklFUjpHZW5lcmljLnNvbWVfYnV0dG9uX3JvbGxiYWNrEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgnw0ouA0=
+ // Inline Codegen:CAESfwgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMaXgoSSURFTlRJRklFUjpHZW5lcmljEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgoAMopwMSmQEIAhIVL2t5dGhlL2VkZ2UvZ2VuZXJhdGVzGngKLElERU5USUZJRVI6R2VuZXJpYy5hbm90aGVyX2JyaWNrX2luX3RoZV93YWxsEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgoQUovwUSngEIAhIVL2t5dGhlL2VkZ2UvZ2VuZXJhdGVzGn0KMUlERU5USUZJRVI6R2VuZXJpYy5mYWxsX2FzbGVlcF93aGlsZV95b3VyZV9hc2hvcmUSB2dvb2dsZTMiOmdvb2dsZWRhdGEvZXhwZXJpbWVudHMvZmFrZWZlL2ZlYXR1cmVzL2dlbmVyaWNfZmVhdHVyZS5nY2wqA2djbCCsByjPBxKYAQgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMadworSURFTlRJRklFUjpHZW5lcmljLm1lbl93aXRoX2FsbF90aGVfYW5zd2VycxIHZ29vZ2xlMyI6Z29vZ2xlZGF0YS9leHBlcmltZW50cy9mYWtlZmUvZmVhdHVyZXMvZ2VuZXJpY19mZWF0dXJlLmdjbCoDZ2NsILQJKNEJEosBCAISFS9reXRoZS9lZGdlL2dlbmVyYXRlcxpqCh5JREVOVElGSUVSOkdlbmVyaWMuc29tZV9idXR0b24SB2dvb2dsZTMiOmdvb2dsZWRhdGEvZXhwZXJpbWVudHMvZmFrZWZlL2ZlYXR1cmVzL2dlbmVyaWNfZmVhdHVyZS5nY2wqA2djbCCuCyi+CxKUAQgCEhUva3l0aGUvZWRnZS9nZW5lcmF0ZXMacwonSURFTlRJRklFUjpHZW5lcmljLnNvbWVfYnV0dG9uX3JvbGxiYWNrEgdnb29nbGUzIjpnb29nbGVkYXRhL2V4cGVyaW1lbnRzL2Zha2VmZS9mZWF0dXJlcy9nZW5lcmljX2ZlYXR1cmUuZ2NsKgNnY2wgnw0ouA0=
