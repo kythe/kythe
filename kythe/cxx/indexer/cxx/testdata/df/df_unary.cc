c710ad4c5b8bb17d49850bfb97fed4543bf6827b
Luke Zarko <zarko@google.com>
2021-10-07 22:04:13 -0700

    fix: test update for transitional change

--- kythe/cxx/indexer/cxx/testdata/df/df_unary.cc
+++ kythe/cxx/indexer/cxx/testdata/df/df_unary.cc
@@ -16,7 +16,7 @@ void f() {
     //- VarX influences VarX
     //- !{ @x ref/writes VarX }
     //- !{ @z ref/writes VarZ }
-    //- !{ @y ref VarY }
+    //- // !{ @y ref VarY } todo(zarko): transitional change.
     //- @z ref VarZ
     //- @x ref VarX
     //- @y ref/writes VarY
