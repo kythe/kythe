28df40df975ffa71fb809ba2f289842cb7925b1f
Jay Sachs <jsachs@google.com>
2021-10-20 17:44:20 -0700

    feat: include edge in output of `kythe xrefs` for definitions and references

--- kythe/go/services/cli/commands_xrefs.go
+++ kythe/go/services/cli/commands_xrefs.go
@@ -217,10 +217,10 @@ func displayRelatedAnchors(kind string, anchors []*xpb.CrossReferencesReply_Rela
 					return err
 				}
 			}
-			if _, err := fmt.Fprintf(out, "    [%d:%d-%d:%d)\n      %q\n",
+			if _, err := fmt.Fprintf(out, "    [%d:%d-%d:%d %s)\n      %q\n",
 				a.Anchor.Span.Start.LineNumber, a.Anchor.Span.Start.ColumnOffset,
 				a.Anchor.Span.End.LineNumber, a.Anchor.Span.End.ColumnOffset,
-				string(a.Anchor.Snippet)); err != nil {
+				a.Anchor.GetKind(), string(a.Anchor.Snippet)); err != nil {
 				return err
 			}
 			for _, site := range a.Site {
