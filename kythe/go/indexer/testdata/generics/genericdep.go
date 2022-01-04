140f6d8c473c9fb4be9b5daa25168adca0b27293
Cody Schroeder <schroederc@google.com>
2022-01-04 12:40:43 -0800

    fix(go_indexer): ensure generic references work across packages

---
+++ kythe/go/indexer/testdata/generics/genericdep.go
@@ -0,0 +1,24 @@
+package genericdep
+
+import (
+	genericinterface "kythe/go/indexer/genericinterface_test"
+)
+
+func main() {
+	c := &genericinterface.Container[string]{"element"}
+	//- @Accept ref ContainerAccept
+	c.Accept("yup")
+	//- @Element ref Element
+	c.Element = ""
+}
+
+//- @Number defines/binding Number
+//- Number satisfies Interface
+type Number struct{ I int }
+
+//- @Accept defines/binding NumberAccept
+//- NumberAccept overrides Accept
+func (n *Number) Accept(i int) { n.I = i }
+
+//- @Interface ref Interface
+var _ genericinterface.Interface[int] = &Number{42}
