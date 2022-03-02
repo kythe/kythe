cce98f1b2fade4d787c8155484a1b628f0ce2b90
Shahms King <shahms@google.com>
2022-03-02 10:48:19 -0800

    chore: remove Java 8 workarounds and resulting dead code (#5230)
    * chore: remove Java 8 workarounds
    
    * chore: remove dead JavaFileManager code
    
    * chore: remove jdk8 extractor and support files
    
    * chore: fix a missed java9_extractor reference

--- kythe/java/com/google/devtools/kythe/platform/java/filemanager/ForwardingStandardJavaFileManager.java
+++ kythe/java/com/google/devtools/kythe/platform/java/filemanager/ForwardingStandardJavaFileManager.java
@@ -15,14 +15,8 @@
  */
 package com.google.devtools.kythe.platform.java.filemanager;
 
-import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
-import com.google.common.flogger.FluentLogger;
-import com.google.common.reflect.Reflection;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.ServiceLoader;
@@ -41,109 +35,41 @@ import javax.tools.StandardJavaFileManager;
 public class ForwardingStandardJavaFileManager
     extends ForwardingJavaFileManager<StandardJavaFileManager> implements StandardJavaFileManager {
 
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-
-  // TODO(shahms): Remove these when we've moved to JDK9 and can invoke the methods directly.
-  //  Until then, cache the lookup of these extended StandardJavaFileManager methods.
-  private static final Method getLocationForModuleNameMethod =
-      getMethodOrNull("getLocationForModule", Location.class, String.class);
-  private static final Method getLocationForModuleFileMethod =
-      getMethodOrNull("getLocationForModule", Location.class, JavaFileObject.class);
-  private static final Method getServiceLoaderMethod =
-      getMethodOrNull("getServiceLoader", Location.class, Class.class);
-  private static final Method inferModuleNameMethod =
-      getMethodOrNull("inferModuleName", Location.class);
-  private static final Method listLocationsForModulesMethod =
-      getMethodOrNull("listLocationsForModules", Location.class);
-  private static final Method containsMethod =
-      getMethodOrNull("contains", Location.class, FileObject.class);
-  private static final Method getJavaFileObjectsFromPathsMethod =
-      getMethodOrNull("getJavaFileObjectsFromPaths", Iterable.class);
-  private static final Method getJavaFileObjectsMethod =
-      getMethodOrNull("getJavaFileObjects", Path[].class);
-  private static final Method setLocationFromPathsMethod =
-      getMethodOrNull("setLocationFromPaths", Location.class, Collection.class);
-  private static final Method setLocationForModuleMethod =
-      getMethodOrNull("setLocationForModule", Location.class, String.class, Collection.class);
-  private static final Method getLocationAsPathsMethod =
-      getMethodOrNull("getLocationAsPaths", Location.class);
-  private static final Method asPathMethod = getMethodOrNull("asPath", FileObject.class);
-  private static final Class<?> pathFactoryInterface = getClassOrNull("PathFactory");
-  private static final Method setPathFactoryMethod =
-      getMethodOrNull("setPathFactory", pathFactoryInterface);
-
-  /** Equivalent interface for JDK9's StandardJavaFileManager.PathFactory */
-  @FunctionalInterface
-  public static interface PathFactory {
-    Path getPath(String path, String... more);
-  }
-
   protected ForwardingStandardJavaFileManager(StandardJavaFileManager fileManager) {
     super(fileManager);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public Location getLocationForModule(Location location, String moduleName) throws IOException {
-    // TODO(shahms): return fileManager.getLocationForModule(location, fo);
-    try {
-      return (Location) getLocationForModuleNameMethod.invoke(fileManager, location, moduleName);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getLocationForModule", e, IOException.class);
-    }
+    return fileManager.getLocationForModule(location, moduleName);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public Location getLocationForModule(Location location, JavaFileObject fo) throws IOException {
-    // TODO(shahms): return fileManager.getLocationForModule(location, fo);
-    try {
-      return (Location) getLocationForModuleFileMethod.invoke(fileManager, location, fo);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getLocationForModule", e, IOException.class);
-    }
+    return fileManager.getLocationForModule(location, fo);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   @SuppressWarnings({"unchecked"}) // safe by specification.
   public <S> ServiceLoader<S> getServiceLoader(Location location, Class<S> service)
       throws IOException {
-    // TODO(shahms): return fileManager.getServiceLoader(location, service);
-    try {
-      return (ServiceLoader<S>) getServiceLoaderMethod.invoke(location, service);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getServiceLoader", e, IOException.class);
-    }
+    return fileManager.getServiceLoader(location, service);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public String inferModuleName(Location location) throws IOException {
-    // TODO(shahms): return fileManager.inferModuleName(location);
-    try {
-      return (String) inferModuleNameMethod.invoke(fileManager, location);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("inferModuleName", e, IOException.class);
-    }
+    return fileManager.inferModuleName(location);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   @SuppressWarnings({"unchecked"}) // safe by specification.
   public Iterable<Set<Location>> listLocationsForModules(Location location) throws IOException {
-    // TODO(shahms): return fileManager.listLocationsForModules(location);
-    try {
-      return (Iterable<Set<Location>>) listLocationsForModulesMethod.invoke(fileManager, location);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible(
-          "listLocationsForModules", e, IOException.class);
-    }
+    return fileManager.listLocationsForModules(location);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public boolean contains(Location location, FileObject fo) throws IOException {
-    // TODO(shahms): return fileManager.contains(location, fo);
-    try {
-      return (Boolean) containsMethod.invoke(fileManager, location, fo);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("contains", e, IOException.class);
-    }
+    return fileManager.contains(location, fo);
   }
 
   @Override
@@ -152,17 +78,11 @@ public class ForwardingStandardJavaFileManager
     return fileManager.getJavaFileObjectsFromFiles(files);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   @SuppressWarnings({"unchecked", "IterablePathParameter"}) // safe by specification.
   public Iterable<? extends JavaFileObject> getJavaFileObjectsFromPaths(
       Iterable<? extends Path> paths) {
-    //  TODO(shahms): fileManager.getJavaFileObjectsFromPaths(paths);
-    try {
-      return (Iterable<? extends JavaFileObject>)
-          getJavaFileObjectsFromPathsMethod.invoke(fileManager, paths);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getJavaFileObjectsFromPaths", e);
-    }
+    return fileManager.getJavaFileObjectsFromPaths(paths);
   }
 
   @Override
@@ -180,38 +100,22 @@ public class ForwardingStandardJavaFileManager
     return fileManager.getJavaFileObjects(names);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   @SuppressWarnings({"unchecked"}) // safe by specification.
   public Iterable<? extends JavaFileObject> getJavaFileObjects(Path... paths) {
-    //  TODO(shahms): fileManager.getJavaFileObjects(paths);
-    try {
-      return (Iterable<? extends JavaFileObject>)
-          getJavaFileObjectsMethod.invoke(fileManager, (Object) paths);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getJavaFileObjects", e);
-    }
+    return fileManager.getJavaFileObjects(paths);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public void setLocationFromPaths(Location location, Collection<? extends Path> paths)
       throws IOException {
-    // TODO(shahms): fileManager.setLocationFromPaths(location, paths);
-    try {
-      setLocationFromPathsMethod.invoke(fileManager, location, paths);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("setLocationFromPaths", e, IOException.class);
-    }
+    fileManager.setLocationFromPaths(location, paths);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public void setLocationForModule(
       Location location, String moduleName, Collection<? extends Path> paths) throws IOException {
-    // TODO(shahms): fileManager.setLocationForModule(location, moduleName, paths);
-    try {
-      setLocationForModuleMethod.invoke(fileManager, location, moduleName, paths);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("setLocationForModule", e, IOException.class);
-    }
+    fileManager.setLocationForModule(location, moduleName, paths);
   }
 
   @Override
@@ -219,18 +123,10 @@ public class ForwardingStandardJavaFileManager
     return fileManager.getLocation(location);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   @SuppressWarnings({"unchecked"}) // Safe by specification.
   public Iterable<? extends Path> getLocationAsPaths(Location location) {
-    // TODO(shahms): return fileManager.getLocationAsPaths(location);
-    try {
-      return (Iterable<? extends Path>) getLocationAsPathsMethod.invoke(fileManager, location);
-    } catch (NullPointerException npe) {
-      // Fall back to using getLocation from the underlying fileManager.
-      return Iterables.transform(fileManager.getLocation(location), File::toPath);
-    } catch (ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("getLocationAsPaths", e);
-    }
+    return fileManager.getLocationAsPaths(location);
   }
 
   @Override
@@ -243,82 +139,13 @@ public class ForwardingStandardJavaFileManager
     fileManager.setLocation(location, files);
   }
 
-  // TODO(shahms): @Override; added in JDK9
+  @Override
   public Path asPath(FileObject fo) {
-    // TODO(shahms): return fileManager.asPath(fo);
-    try {
-      return (Path) asPathMethod.invoke(fileManager, fo);
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("asPath", e);
-    }
+    return fileManager.asPath(fo);
   }
 
-  // TODO(shahms): @Override; added in JDK9
-  // We've intentionally mirrored this functional interface
-  // as this is a compatibility shim which will be deleted when
-  // we can stop requiring that this be buildable on Java 8 but run
-  // on JDK11.
-  @SuppressWarnings("FunctionalInterfaceClash")
-  public void setPathFactory(PathFactory factory) {
-    // TODO(shahms): fileManager.setPathFactory(factory);
-    try {
-      setPathFactoryMethod.invoke(
-          fileManager,
-          Reflection.newProxy(
-              pathFactoryInterface,
-              (proxy, method, args) -> {
-                return factory.getPath((String) args[0], (String[]) args[1]);
-              }));
-    } catch (NullPointerException | ReflectiveOperationException e) {
-      throw propagateInvocationTargetErrorIfPossible("setPathFactory", e);
-    }
-  }
-
-  private static Method getMethodOrNull(String name, Class<?>... parameterTypes) {
-    try {
-      return StandardJavaFileManager.class.getMethod(name, parameterTypes);
-    } catch (NoSuchMethodException e) {
-      logger.atInfo().withCause(e).log("Failed to find extended StandardJavaFileManager method");
-    }
-    return null;
-  }
-
-  private static Class<?> getClassOrNull(String name) {
-    try {
-      return Class.forName(
-          StandardJavaFileManager.class.getCanonicalName() + "$" + name,
-          true,
-          StandardJavaFileManager.class.getClassLoader());
-    } catch (ClassNotFoundException e) {
-      logger.atInfo().withCause(e).log("Failed to find StandardJavaFileManager class");
-    }
-    return null;
-  }
-
-  private static UnsupportedOperationException propagateInvocationTargetErrorIfPossible(
-      String methodName, Throwable error) {
-    if (error instanceof InvocationTargetException) {
-      Throwables.throwIfUnchecked(((InvocationTargetException) error).getCause());
-    }
-    return unsupportedVersionError(methodName, error);
-  }
-
-  private static UnsupportedOperationException propagateInvocationTargetErrorIfPossible(
-      String methodName, Throwable error, Class<IOException> declaredType) throws IOException {
-    if (error instanceof InvocationTargetException) {
-      // Log the exception because the propagated destination may not provide a nice error log.
-      Throwable t = ((InvocationTargetException) error).getCause();
-      logger.atWarning().withCause(t).log(
-          "Error in underlying filemanager. A more detailed message may have been output to"
-              + " stderr.");
-      Throwables.propagateIfPossible(t, declaredType);
-    }
-    return unsupportedVersionError(methodName, error);
-  }
-
-  private static UnsupportedOperationException unsupportedVersionError(
-      String methodName, Throwable cause) {
-    return new UnsupportedOperationException(
-        methodName + " called by unsupported Java version", cause);
+  @Override
+  public void setPathFactory(StandardJavaFileManager.PathFactory factory) {
+    fileManager.setPathFactory(factory);
   }
 }
