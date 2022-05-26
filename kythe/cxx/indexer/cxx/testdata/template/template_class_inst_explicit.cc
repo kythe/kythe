ab54544d41d426f1206ca3174c05577973c4377d
Shahms King <shahms@google.com>
2022-05-26 16:03:36 -0700

    fix(cxx_indexer): emit non-implicit refs to explicit template specializations

--- kythe/cxx/indexer/cxx/testdata/template/template_class_inst_explicit.cc
+++ kythe/cxx/indexer/cxx/testdata/template/template_class_inst_explicit.cc
@@ -4,6 +4,8 @@ using ExternalDef = int;
 template <typename T> struct C {
   using X = ExternalDef;
 };
+//- @C defines/binding CInstantiation
+//- @C ref CInstantiation
 template struct C<int>;
 //- ExplicitC.node/kind record
 //- XAnchor childof/context ExplicitC
