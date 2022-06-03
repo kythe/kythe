60c0e854e860314e9201dd008b53f1d20ad99c60
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-06-03 09:18:36 -0700

    feat(typescript_indexer): use ref/id for destructuring variables (#5292)
    It also affects object literal properties that reference inteface fields.
    Those should use `override` instead but it requires extra work and will
    be done as follow up.
    
    Co-authored-by: Shahms King <shahms@google.com>

--- kythe/typescript/testdata/interface.ts
+++ kythe/typescript/testdata/interface.ts
@@ -10,16 +10,16 @@ interface Person {
 }
 
 const p: Person = {
-  //- @name ref Name
+  //- @name ref/id Name
   name: 'Alice',
-  //- @getAge ref GetAge
+  //- @getAge ref/id GetAge
   getAge() {}
 };
 
 const p2 = {
-  //- @name ref Name
+  //- @name ref/id Name
   name: 'Alice',
-  //- @getAge ref GetAge
+  //- @getAge ref/id GetAge
   getAge() {}
 } as Person;
 
@@ -29,28 +29,28 @@ p.name;
 //- @getAge ref GetAge
 p.getAge();
 
-//- @getAge ref GetAge
+//- @getAge ref/id GetAge
 const {getAge} = p;
 
-//- @name ref Name
-//- @getAge ref GetAge
+//- @name ref/id Name
+//- @getAge ref/id GetAge
 function takesPerson({name, getAge: newGetAge}: Person) {}
 
-//- @name ref Name
-//- @getAge ref GetAge
+//- @name ref/id Name
+//- @getAge ref/id GetAge
 takesPerson({name: 'Alice', getAge() {}});
 
 class PersonTaker {
   constructor(p: Person) {}
 }
 
-//- @name ref Name
-//- @getAge ref GetAge
+//- @name ref/id Name
+//- @getAge ref/id GetAge
 new PersonTaker({name: 'Alice', getAge() {}});
 
 function returnPerson(): Person {
-  //- @name ref Name
-  //- @getAge ref GetAge
+  //- @name ref/id Name
+  //- @getAge ref/id GetAge
   return {name: 'Alice', getAge() {}};
 }
 
@@ -58,7 +58,7 @@ function returnPerson(): Person {
 {
   const name = 'Alice';
   const getAge = () = {};
-  //- @name ref Name
-  //- @getAge ref GetAge
+  //- @name ref/id Name
+  //- @getAge ref/id GetAge
   const p3: Person = {name, getAge};
 }
