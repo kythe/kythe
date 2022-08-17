41a68a937a584b43d1e35c08e0c70c9e304486fe
Wyatt Calandro <64923869+wcalandro@users.noreply.github.com>
2022-08-17 11:27:05 -0700

    chore(rust_common): use the rustfmt from rules_rust

--- tools/rust/rustfmt.sh
+++ tools/rust/rustfmt.sh
@@ -14,9 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find kythe/rust/ tools/rust/ -name '*.rs' -not -wholename "*target/*" -not -wholename "*rust/*/testdata/*" -print0 | while read -r -d $'\0' f
-do
-    echo "Formatting $f";
-    rustfmt --config-path "$(dirname "${BASH_SOURCE[0]}")" "$@" "$f" &
-done
-wait
+# Find the rustfmt binary that was distributed with rules_rust so that we
+# don't have to rely on the system binary, which may be out of date.
+RUSTFMT=$(find -L bazel-kythe/external -not \( -path "bazel-kythe/external/io_kythe" -prune \) -wholename "bazel-kythe/external/rust_*_x86_64*_tools/bin/rustfmt")
+
+# If no arguments are provided, format all Rust files. Otherwise, format the
+# specified file.
+if [[ $# -eq 0 ]] ; then
+    find kythe/rust/ tools/rust/ -name '*.rs' -not -wholename "*target/*" -not -wholename "*rust/*/testdata/*" -print0 | while read -r -d $'\0' f
+    do
+        echo "Formatting $f";
+        $RUSTFMT --config-path "$(dirname "${BASH_SOURCE[0]}")" "$@" "$f" &
+    done
+    wait
+else
+    $RUSTFMT --config-path "$(dirname "${BASH_SOURCE[0]}")" "$1"
+fi
