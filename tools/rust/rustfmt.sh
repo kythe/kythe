c00d2b42cb340f3ca6011dda5a7988948b3dfc5a
Wyatt Calandro <64923869+wcalandro@users.noreply.github.com>
2022-08-18 18:15:41 +0000

    chore(rust_common): rustfmt improvements (#5357)
    * chore(rust_common): enable rustfmt tests using aspect
    
    * chore(rust_common): use the rustfmt from rules_rust
    
    * fix: fallback to system rustfmt for cloudbuild
    
    * fix: use golang 1.18 specifically
    
    * fix: bazelrc changes

--- tools/rust/rustfmt.sh
+++ tools/rust/rustfmt.sh
@@ -14,9 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find kythe/rust/ tools/rust/ -name '*.rs' -not -wholename "*target/*" -not -wholename "*rust/*/testdata/*" -print0 | while read -r -d $'\0' f
-do
-    echo "Formatting $f";
-    rustfmt --config-path "$(dirname "${BASH_SOURCE[0]}")" "$@" "$f" &
-done
-wait
+# Find the rustfmt binary that was distributed with rules_rust so that we don't
+# have to rely on the system binary, which may be out of date. If rules_rust
+# is not present (which usually occurs if we are running in Cloud Build), we
+# use the version of rustfmt installed on the system.
+if [[ -d "bazel-kythe/external/rules_rust" ]]; then
+    RUSTFMT=$(find -L bazel-kythe/external -not \( -path "bazel-kythe/external/io_kythe" -prune \) -wholename "bazel-kythe/external/rust_*_x86_64*_tools/bin/rustfmt")
+else
+    RUSTFMT="rustfmt"
+fi
+
+# If no arguments are provided, format all Rust files. Otherwise, format the
+# specified file.
+if [[ $# -eq 0 ]] ; then
+    find kythe/rust/ tools/rust/ -name '*.rs' -not -wholename "*target/*" -not -wholename "*rust/*/testdata/*" -print0 | while read -r -d $'\0' f
+    do
+        echo "Formatting $f";
+        $RUSTFMT --config-path "$(dirname "${BASH_SOURCE[0]}")" "$@" "$f" &
+    done
+    wait
+else
+    $RUSTFMT --config-path "$(dirname "${BASH_SOURCE[0]}")" "$1"
+fi
