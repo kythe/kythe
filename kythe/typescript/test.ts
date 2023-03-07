ff8bbd15e5a34bedfc7a234bd846afe63bcc9018
Mikita Belahlazau <nikelandjelo@gmail.com>
2023-03-07 14:07:17 -0800

    feat(typescript_indexer): implement ref inlining for imported symbols (#5527)

--- kythe/typescript/test.ts
+++ kythe/typescript/test.ts
@@ -109,6 +109,7 @@ function verify(
   for (const file of testFiles) {
     fileVNames.set(file, {...rootVName, path: file});
   }
+  const enableEdgeReassignment = testCase.name.includes('enable_edge_reassignment');
 
   try {
     const compilationUnit: indexer.CompilationUnit = {
@@ -124,6 +125,7 @@ function verify(
         verifier.stdin.write(JSON.stringify(obj) + '\n');
       },
       plugins,
+      enableImportsEdgeReassignment: enableEdgeReassignment,
     });
   } finally {
     // Ensure we close stdin on the verifier even on crashes, or otherwise
