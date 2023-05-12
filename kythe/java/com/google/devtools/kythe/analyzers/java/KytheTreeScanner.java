42fe43241be5934b96f3b9a242d636331577a817
Justin Buchanan <justbuchanan@gmail.com>
2023-05-12 18:42:42 +0000

    simplify

--- kythe/java/com/google/devtools/kythe/analyzers/java/KytheTreeScanner.java
+++ kythe/java/com/google/devtools/kythe/analyzers/java/KytheTreeScanner.java
@@ -1423,11 +1423,9 @@ public class KytheTreeScanner extends JCTreeScanner<JavaNode, TreeContext> {
             entrySets.emitEdge(node, rule.edgeOut, rule.vname);
           }
 
-          if (rule.semantic != Metadata.Semantic.NONE) {
-            if (rule.semantic == Metadata.Semantic.WRITE) {
-              VName genFuncVName = rule.reverseEdge ? node : rule.vname;
-              entrySets.getEmitter().emitFact(genFuncVName, "/kythe/semantic/generated", "set");
-            }
+          if (rule.semantic == Metadata.Semantic.WRITE) {
+            VName genFuncVName = rule.reverseEdge ? node : rule.vname;
+            entrySets.getEmitter().emitFact(genFuncVName, "/kythe/semantic/generated", "set");
           }
         }
       }
