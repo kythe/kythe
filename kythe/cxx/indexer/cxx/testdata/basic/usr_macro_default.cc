7b36818c557b9d8a3db22aae2f1a3127cfacbaad
Luke Zarko <zarko@google.com>
2023-06-16 20:39:09 +0000

    fix: new files

---
+++ kythe/cxx/indexer/cxx/testdata/basic/usr_macro_default.cc
@@ -0,0 +1,6 @@
+// We index USRs for macros.
+
+//- @USR defines/binding UsrMacro
+//- Usr=vname(_,"unit",_,_,_) /clang/usr UsrMacro
+//- Usr.node/kind clang/usr
+#define USR 1
---
+++ kythe/cxx/indexer/cxx/testdata/basic/usr_var_default.cc
@@ -0,0 +1,16 @@
+// We index USRs for variables.
+
+//- @global defines/binding Global
+//- GlobalUSR=vname(_,"unit",_,_,_) /clang/usr Global
+//- GlobalUSR.node/kind clang/usr
+int global;
+
+//- @param defines/binding Param
+//- !{_ /clang/usr Param}
+void f(int param) { }
+
+void g() {
+//- @local defines/binding Local
+//- !{_ /clang/usr Local} 
+  int local;
+}
