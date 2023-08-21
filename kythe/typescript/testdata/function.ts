265213942771f05ef36b13b3cafae242edae23b8
Mikita Belahlazau <nbeloglazov@google.com>
2023-08-21 10:58:21 -0700

    fix(typescript_indexer): improve handling of anonymous functions (#5795)
    This PR improves function that returns TS Symbol for Node. Previously it handled only named Nodes such as functions with names. Now we can get Symbols of anonymous functions as well. This fixes certain aspects of anonymous functions such as adds missing `childof` edges. And additionally it brings close handling of named and anonymous entities.
    
    Unfortunately it relies on using private Node.symbol property. I considered alternative options such as:
    
    1. Support two different branches: named nodes with Symbol and anonymous nodes without Symbols. That would require re-implementing a SymbolVNameStore cache to deduplicate vnames for anonymous nodes. Keeping both in sync would be challenging and confusing.
    
    2. For anonymous nodes, where we can't get Symbols using public API, create our own Symbol instance. Symbol is public interface so we can create objects that implement it. Would also need to introduce Node=>FakeSymbol cache.
    
    Both approaches seem more complicated. So I decided to use private Node.symbol API. If future TS versions remove it - we'll have to switch to one of the alternative approaches.

--- kythe/typescript/testdata/function.ts
+++ kythe/typescript/testdata/function.ts
@@ -86,7 +86,7 @@ function bindingTest({aa, bb: {cc, dd}}, [ee, [ff]]: NestedArray) {
   dd = ff;
 }
 
-// Test function expressions introducing an anonymous scope
+// Test function expressions introducing an Anon scope
 //- @ev defines/binding Ev
 //- @an defines/binding An
 let ev, an;
@@ -95,4 +95,18 @@ let ev, an;
 let ev;
 //- @an ref An
 an;
-})()
+})();
+
+//- AnonArrowDef defines _
+//- AnonArrowDef.node/kind anchor
+//- AnonArrowDef.loc/start @^"arg"
+//- AnonArrowDef.loc/end @$"42"
+(arg => 42)();
+
+//- AnonFunctionDef defines _
+//- AnonFunctionDef.node/kind anchor
+//- AnonFunctionDef.loc/start @^"function"
+(function() {
+  return 42;
+//- AnonFunctionDef.loc/end @$"}"
+})();
