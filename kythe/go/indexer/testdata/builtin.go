c26972aac9d00407cd210e69c6543bfa2280a76d
Cody Schroeder <schroederc@google.com>
2023-08-25 09:33:56 -0700

    test: add some more checks on Go builtins

--- kythe/go/indexer/testdata/builtin.go
+++ kythe/go/indexer/testdata/builtin.go
@@ -1,6 +1,9 @@
 package builtin
 
-//- _BoolBuiltin=vname("bool#builtin", "golang.org", "", "", "go").node/kind _?
-//- _IntBuiltin=vname("int#builtin", "golang.org", "", "", "go").node/kind _?
+//- BoolBuiltin=vname("bool#builtin", "golang.org", "", "", "go").node/kind tbuiltin
+//- BoolBuiltin.code/rendered/callsite_signature "bool"
+//- IntBuiltin=vname("int#builtin", "golang.org", "", "", "go").node/kind tbuiltin
+//- IntBuiltin.code/rendered/callsite_signature "int"
 
-//- _ErrorBuiltin=vname("error#builtin", "golang.org", "", "", "go").node/kind _?
+//- ErrorBuiltin=vname("error#builtin", "golang.org", "", "", "go").node/kind tbuiltin
+//- ErrorBuiltin.code/rendered/callsite_signature "error"
