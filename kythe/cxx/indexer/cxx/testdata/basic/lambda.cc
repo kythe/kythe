d1a1cfa69bbd47950264a0954092da85c428f896
Justin Buchanan <justbuchanan@gmail.com>
2023-10-11 13:52:19 -0700

    improve TODO comment

--- kythe/cxx/indexer/cxx/testdata/basic/lambda.cc
+++ kythe/cxx/indexer/cxx/testdata/basic/lambda.cc
@@ -1,4 +1,4 @@
-// TODO(justinbuchanan): fix indexer to include leading '(' in lambda anchor
+// TODO(justinbuchanan): fix indexer to include leading '[](' in lambda anchor
 //- @") {}" defines Lambda
 //- Lambda.node/kind function
 auto fn = []() {};
