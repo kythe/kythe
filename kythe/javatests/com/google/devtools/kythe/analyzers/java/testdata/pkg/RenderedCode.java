83561c2e548ded48cb3eabaa163659d506ac06e8
Cody Schroeder <schroederc@google.com>
2023-10-13 12:56:01 -0700

    fix(java_indexer): add basic MarkedSource for wildcards

--- kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/RenderedCode.java
+++ kythe/javatests/com/google/devtools/kythe/analyzers/java/testdata/pkg/RenderedCode.java
@@ -2,6 +2,8 @@
 // - Package.code/rendered/signature "package pkg"
 package pkg;
 
+import java.util.List;
+
 // - @RenderedCode defines/binding RenderedCode
 // - RenderedCode.code/rendered/signature "public final class RenderedCode"
 // - RenderedCode.code/rendered/qualified_name "pkg.RenderedCode"
@@ -33,11 +35,34 @@ public final class RenderedCode {
     public static <A, B> Inner<A, B> create() {
       return null;
     }
+
+    // - @genericList defines/binding GenericList
+    // - GenericList.code/rendered/signature "List<T> genericList"
+    List<T> genericList;
   }
 
   // - @CONSTANT defines/binding Constant
   // - Constant.code/rendered/signature "public static final String CONSTANT"
   public static final String CONSTANT = "blah";
+
+  // - @arry defines/binding Arry
+  // - Arry.code/rendered/signature "private int[] arry"
+  private int[] arry;
+
+  // - @wildcardList defines/binding WildcardList
+  // - WildcardList.code/rendered/signature "List<?> wildcardList"
+  // - WildcardList typed WildcardListType
+  // - WildcardListType param.1 Wildcard
+  // - Wildcard.code/rendered/signature "?"
+  List<?> wildcardList;
+
+  // - @extendsBoundedList defines/binding ExtendsBoundedList
+  // - ExtendsBoundedList.code/rendered/signature "List<? extends RenderedCode> extendsBoundedList"
+  List<? extends RenderedCode> extendsBoundedList;
+
+  // - @superBoundedList defines/binding SuperBoundedList
+  // - SuperBoundedList.code/rendered/signature "List<? super Inner<?, ?>> superBoundedList"
+  List<? super Inner<?, ?>> superBoundedList;
 
   // - @E defines/binding E
   // - E.code/rendered/signature "protected enum E"
