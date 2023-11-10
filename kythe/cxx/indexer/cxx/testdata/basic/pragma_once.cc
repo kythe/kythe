5c50736cef2dc99b9e6315ca3a565533d560e045
Shahms King <shahms@google.com>
2023-11-10 09:38:49 -0800

    chore: adjust the pragma once testcase

--- kythe/cxx/indexer/cxx/testdata/basic/pragma_once.cc
+++ kythe/cxx/indexer/cxx/testdata/basic/pragma_once.cc
@@ -1,4 +1,9 @@
-//- @"\"kythe/cxx/indexer/cxx/testdata/basic/pragma_once.h\"" ref/includes PragmaOnceHeaderB?
+//- NativeAnchor=@"\"kythe/cxx/indexer/cxx/testdata/basic/pragma_once.h\"" ref/includes NativeHeader
 #include "kythe/cxx/indexer/cxx/testdata/basic/pragma_once.h"
-//- @"\"kythe_indexer_test/pragma_once.h\"" ref/includes PragmaOnceHeaderA?
+//- MappedAnchor=@"\"kythe_indexer_test/pragma_once.h\"" ref/includes MappedHeader
 #include "kythe_indexer_test/pragma_once.h"
+
+// Ideally, these files would have the same VName, but since they don't
+// ensure that they are different, as expected, for now.
+//- !{ NativeAnchor ref/includes MappedHeader }
+//- !{ MappedAnchor ref/includes NativeHeader }
