e811fae0b577c146d1f532b77b145863d5a5d570
Shahms King <shahms@google.com>
2023-11-10 09:53:43 -0800

    chore: command on the pragma once test

--- kythe/cxx/indexer/cxx/testdata/basic/pragma_once.h
+++ kythe/cxx/indexer/cxx/testdata/basic/pragma_once.h
@@ -2,6 +2,8 @@
 
 namespace kythe_indexer_test {
 
+// If pragma once breaks, this struct will be defined multiple times
+// and trigger an error, failing the test.
 struct Empty{};
 
 }  // namespace kythe_indexer_test
