4e8da94dd1960dbd6eae9458a82fb0ddf5d6b498
Cody Schroeder <schroederc@google.com>
2024-02-14 11:47:39 -0800

    feat(go_indexer): add experimental flag support

---
+++ kythe/go/indexer/testdata/flags.go
@@ -0,0 +1,31 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	//- @flagVar defines/binding FlagVar
+	//- FlagVar.node/kind variable
+	//- @"\"flag_name\"" defines/binding Flag=vname("flag flag_name", _, _, _, _)
+	//- Flag.node/kind google/gflag
+	//- Flag generates FlagVar
+	flagVar = flag.String("flag_name", "default_value", "Flag description")
+
+	//- FlagDoc documents Flag
+	//- FlagDoc.node/kind doc
+	//- FlagDoc.text "Flag description"
+)
+
+func init() {
+	//- @localFlag defines/binding LocalFlag
+	//- LocalFlag.node/kind variable
+	//- @"\"bool_flag\"" defines/binding BoolFlag=vname("flag bool_flag", _, _, _, _)
+	//- BoolFlag.node/kind google/gflag
+	//- BoolFlag generates LocalFlag
+	localFlag := flag.Bool("bool_flag", false, "Bool flag")
+
+	args := flag.Args()
+	fmt.Println(*flagVar, *localFlag, args)
+}
