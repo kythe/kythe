138dc1a27aca3f83649a70a9c2211c8a2036c497
Cody Schroeder <schroederc@google.com>
2024-03-19 11:41:48 -0700

    run targets

--- update_generated_sources.sh
+++ update_generated_sources.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -e
-bazel build $(bazel query 'kind("_write_source_file", //...)')
+# Runs all Bazel targets that write generated files.
+#
+# Usage:
+#   chronic ./update_generated_sources.sh
+
+cd "$(dirname "$0")"
+
+for target in $(bazel query 'kind("_write_source_file", //...)'); do
+  bazel build $target
+done
