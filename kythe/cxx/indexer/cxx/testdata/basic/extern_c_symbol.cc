1a5bd228e1265787bfd41b1b12db510fd79880d5
zrlk <zrlk@users.noreply.github.com>
2024-04-18 17:05:59 -0400

    feat(cxx_indexer): emit symbols for extern C function defns (#6104)
    * feat(cxx_indexer): emit symbols for extern C function defns
    
    Symbols for extern "C" function definitions are emitted
    as name nodes in the symbol language. A function definition
    node is `named` its name node. The name node uses the corpus
    of the function it names. (The node kind, language, edge kind,
    and edge direction are all up for discussion.)
    
    * fix: symbol -> csymbol; condition csymbols on a flag

---
+++ kythe/cxx/indexer/cxx/testdata/basic/extern_c_symbol.cc
@@ -0,0 +1,10 @@
+// extern "C" function definitions are given names.
+//- @f defines/binding FnF
+//- !{ FnF named _ }
+void f();
+//- @g defines/binding FnG
+//- FnG named vname("g","file","","","csymbol")
+extern "C" void g() {}
+//- @h defines/binding FnH
+//- !{ FnH named _ }
+extern "C" void h();
